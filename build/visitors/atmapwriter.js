8354eec7e6318ad4380ee75017939b39395761c7
Patrick Brosset <patrick.brosset@amadeus.com>
2012-06-12 15:35:19 +0200

    md5 versioning of packages + release versioning of AT

--- build/visitors/atmapwriter.js
+++ build/visitors/atmapwriter.js
@@ -1,4 +1,23 @@
-module.exports.onFileEnd = function(callback, config, packageFileObject) {
-	packageFileObject.content += "\n\naria.core.DownloadMgr.updateUrlMap(" + JSON.stringify(config.urlmap) + ");";
-	callback();
+var fs = require("fs");
+
+function getATLoaderPackage(config) {
+    var loader = null;
+    Object.keys(config.packages).forEach(function(value, index, packages) {
+        var packageConfig = config.packages[value];
+        if(packageConfig.appendUrlMap) {
+            loader = value;
+        }
+    });
+    return loader;
+};
+
+module.exports.onEnd = function(callback, config) {
+    // Find which is the ariatemplates's loader based on the fact that it has the atmapwriter's visitor
+    var atLoader = getATLoaderPackage(config);
+    if(atLoader) {
+        var fd = fs.openSync(atLoader, "a");
+        fs.writeSync(fd, "\n\naria.core.DownloadMgr.updateUrlMap(" + JSON.stringify(config.urlmap) + ");");
+    } else {
+        logger.logError("Could not find any package configured with the atmapwriter.js visitor, so could not add the urlMap anywhere.");
+    }
 };
