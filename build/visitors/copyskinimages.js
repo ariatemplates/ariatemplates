a603c0e6f169c4269f26f39cd1b2b22df3beb4ae
Patrick Brosset <patrick.brosset@amadeus.com>
2012-06-19 12:15:00 +0200

    copying all images to the release

--- build/visitors/copyskinimages.js
+++ build/visitors/copyskinimages.js
@@ -3,6 +3,11 @@ var wrench = require("wrench");
 module.exports.onPackageStart = function(callback, config, packageFileObject) {
     var skinFilePath = packageFileObject.path;
     var imgFolderPath = skinFilePath.substring(0, skinFilePath.lastIndexOf("."));
+
+    logger.logInfo("Copying all images from " + config.source + "/" + imgFolderPath + " to " + config.destination + "/" + imgFolderPath);
+
+    wrench.mkdirSyncRecursive(config.destination + "/" + imgFolderPath, 0777);
     wrench.copyDirSyncRecursive(config.source + "/" + imgFolderPath, config.destination + "/" + imgFolderPath);
+
     callback();
 };
