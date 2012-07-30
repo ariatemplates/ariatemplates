ariatemplates/ariatemplates

commit cd6c2e050a5e642a527a8b475bfbeaf0792d4e9d

This commit fixes the issue #82

src/aria/widgets/errorlist/ErrorList.js
@@ -71,16 +71,17 @@ Aria.classDefinition({
             p.bindableProperties = p.bindableProperties.concat(["messages"]);
         },
 
-        _onBoundPropertyChange : function (propertyName, newValue, oldValue) {
-            this._inOnBoundPropertyChange = true;
-            try {
-                if (propertyName == "messages") {
-                    this._subTplModuleCtrl.setMessages(newValue);
-                }
-                this._cfg[propertyName] = newValue
-            } finally {
-                this._inOnBoundPropertyChange = false;
-            }
-        }
-    }
+		_onBoundPropertyChange : function (propertyName, newValue, oldValue) {
+			this._inOnBoundPropertyChange = true;
+			try {
+				if (propertyName == "messages") {
+					var domId = this._domElt;
+					this._subTplModuleCtrl.setMessages(newValue, domId);
+				}
+				this._cfg[propertyName] = newValue
+			} finally {
+				this._inOnBoundPropertyChange = false;
+			}
+		}
+	}
 });

src/aria/widgets/errorlist/ErrorListController.js
@@ -46,20 +46,25 @@ Aria.classDefinition({
             this.$callback(cb);
         },
 
-        /**
-         * Method to be called when changing the messages data structure.
-         * @param {aria.utils.validators.CfgBeans.MessagesList} messages list of messages
-         */
-        setMessages : function (messages) {
-            if (messages == null) {
-                messages = [];
+		/**
+		 * Method to be called when changing the messages data structure.
+		 * @param {aria.utils.validators.CfgBeans.MessagesList} messages list of messages
+		 */
+		setMessages : function (messages, domId) {
+			if (messages == null) {
+				messages = [];
+			}
+			var map = {};
+			var filteredMessages = this._processMessages(messages, map, this._data.filterTypes);
+			this.json.setValue(this._data, "messageTypes", map);
+			this.json.setValue(this._data, "messages", filteredMessages);
+			if(filteredMessages.length > 0){
+               this.$raiseEvent({
+                name : "messagesChanged",
+                domRef : domId
+				});
             }
-            var map = {};
-            var filteredMessages = this._processMessages(messages, map, this._data.filterTypes);
-            this.json.setValue(this._data, "messageTypes", map);
-            this.json.setValue(this._data, "messages", filteredMessages);
-            this.$raiseEvent("messagesChanged");
-        },
+		},
 
         /**
          * Set the focus on the field which corresponds to the specified message.

src/aria/widgets/errorlist/ErrorListTemplateScript.js
@@ -18,16 +18,16 @@
  * @class aria.widgets.errorlist.ErrorListTemplateScript
  */
 Aria.tplScriptDefinition({
-    $classpath : "aria.widgets.errorlist.ErrorListTemplateScript",
-    $dependencies : ["aria.utils.Data"],
-    $statics : {
-        /**
-         * Links each type of message with an icon. The order in the array is important as the first entry for which
-         * messages of that type exist in the messages list is used. Since it depends on aria.utils.Data wait for it to
-         * be loaded by the dependency manager
-         * @type Array
-         */
-        ICONS : [],
+	$classpath : "aria.widgets.errorlist.ErrorListTemplateScript",
+	$dependencies : ["aria.utils.Data","aria.utils.Dom"],
+	$statics : {
+		/**
+		 * Links each type of message with an icon. The order in the array is important as the first entry for which
+		 * messages of that type exist in the messages list is used. Since it depends on aria.utils.Data wait for it to
+		 * be loaded by the dependency manager
+		 * @type Array
+		 */
+		ICONS : [],
 
         /**
          * Icon used if there is no matching icon in ICONS.
@@ -68,24 +68,28 @@ Aria.tplScriptDefinition({
             });
         },
 
-        /**
-         * React to module events
-         * @param {Event} evt
-         */
-        onModuleEvent : function (evt) {
-            if (evt.name == "messagesChanged") {
-                this.$refresh();
-            }
-        },
-
-        /**
-         * Click on an error message
-         * @param {Event} evt Not used
-         * @param {Object} msg Error message
-         */
-        clickOnMessage : function (evt, msg) {
-            this.moduleCtrl.focusField(msg);
-        },
+		/**
+		 * React to module events
+		 * @param {Event} evt
+		 */
+		onModuleEvent : function (evt) {
+			
+			if (evt.name == "messagesChanged") {
+				if(evt.domRef){
+                    var domConfig = aria.utils.Dom.scrollIntoView(evt.domRef);
+                }
+				this.$refresh();
+			}
+		},
+		
+		/**
+		 * Click on an error message
+		 * @param {Event} evt Not used
+		 * @param {Object} msg Error message
+		 */
+		clickOnMessage : function (evt, msg) {
+			this.moduleCtrl.focusField(msg);
+		},
 
         /**
          * Get the icon name for the current message type

commit 347527530c9474a9ec1becaf47a7ead9986d0a36

Fix a regression on the ErrorList widget (introduced by commit cd6c2e050a5e642a527a8b475bfbeaf0792d4e9d)

src/aria/widgets/errorlist/ErrorList.js
@@ -71,17 +71,17 @@ Aria.classDefinition({
             p.bindableProperties = p.bindableProperties.concat(["messages"]);
         },
 
-		_onBoundPropertyChange : function (propertyName, newValue, oldValue) {
-			this._inOnBoundPropertyChange = true;
-			try {
-				if (propertyName == "messages") {
-					var domId = this._domElt;
-					this._subTplModuleCtrl.setMessages(newValue, domId);
-				}
-				this._cfg[propertyName] = newValue
-			} finally {
-				this._inOnBoundPropertyChange = false;
-			}
-		}
-	}
+        _onBoundPropertyChange : function (propertyName, newValue, oldValue) {
+            this._inOnBoundPropertyChange = true;
+            try {
+                if (propertyName == "messages") {
+                    var domId = this._domElt;
+                    this._subTplModuleCtrl.setMessages(newValue, domId);
+                }
+                this._cfg[propertyName] = newValue
+            } finally {
+                this._inOnBoundPropertyChange = false;
+            }
+        }
+    }
 });

src/aria/widgets/errorlist/ErrorListController.js
@@ -46,26 +46,25 @@ Aria.classDefinition({
             this.$callback(cb);
         },
 
-		/**
-		 * Method to be called when changing the messages data structure.
-		 * @param {aria.utils.validators.CfgBeans.MessagesList} messages list of messages
+        /**
+         * Method to be called when changing the messages data structure.
+         * @param {aria.utils.validators.CfgBeans.MessagesList} messages list of messages
          * @param {HTMLElement} element to which is to be made visible
-		 */
-		setMessages : function (messages, element) {
-			if (messages == null) {
-				messages = [];
-			}
-			var map = {};
-			var filteredMessages = this._processMessages(messages, map, this._data.filterTypes);
-			this.json.setValue(this._data, "messageTypes", map);
-			this.json.setValue(this._data, "messages", filteredMessages);
-			if(filteredMessages.length > 0){
-               this.$raiseEvent({
-                name : "messagesChanged",
-                domRef : element
-				});
+         */
+        setMessages : function (messages, element) {
+            if (messages == null) {
+                messages = [];
             }
-		},
+            var map = {};
+            var filteredMessages = this._processMessages(messages, map, this._data.filterTypes);
+            this.json.setValue(this._data, "messageTypes", map);
+            this.json.setValue(this._data, "messages", filteredMessages);
+            var domRef = (filteredMessages.length > 0) ? element : null;
+            this.$raiseEvent({
+                name : "messagesChanged",
+                domRef : domRef
+            });
+        },
 
         /**
          * Set the focus on the field which corresponds to the specified message.

src/aria/widgets/errorlist/ErrorListTemplateScript.js
@@ -18,16 +18,16 @@
  * @class aria.widgets.errorlist.ErrorListTemplateScript
  */
 Aria.tplScriptDefinition({
-	$classpath : "aria.widgets.errorlist.ErrorListTemplateScript",
-	$dependencies : ["aria.utils.Data","aria.utils.Dom"],
-	$statics : {
-		/**
-		 * Links each type of message with an icon. The order in the array is important as the first entry for which
-		 * messages of that type exist in the messages list is used. Since it depends on aria.utils.Data wait for it to
-		 * be loaded by the dependency manager
-		 * @type Array
-		 */
-		ICONS : [],
+    $classpath : "aria.widgets.errorlist.ErrorListTemplateScript",
+    $dependencies : ["aria.utils.Data", "aria.utils.Dom"],
+    $statics : {
+        /**
+         * Links each type of message with an icon. The order in the array is important as the first entry for which
+         * messages of that type exist in the messages list is used. Since it depends on aria.utils.Data wait for it to
+         * be loaded by the dependency manager
+         * @type Array
+         */
+        ICONS : [],
 
         /**
          * Icon used if there is no matching icon in ICONS.
@@ -68,28 +68,28 @@ Aria.tplScriptDefinition({
             });
         },
 
-		/**
-		 * React to module events
-		 * @param {Event} evt
-		 */
-		onModuleEvent : function (evt) {
-			
-			if (evt.name == "messagesChanged") {
-				if(evt.domRef){
+        /**
+         * React to module events
+         * @param {Event} evt
+         */
+        onModuleEvent : function (evt) {
+
+            if (evt.name == "messagesChanged") {
+                if (evt.domRef) {
                     var domConfig = aria.utils.Dom.scrollIntoView(evt.domRef);
                 }
-				this.$refresh();
-			}
-		},
-		
-		/**
-		 * Click on an error message
-		 * @param {Event} evt Not used
-		 * @param {Object} msg Error message
-		 */
-		clickOnMessage : function (evt, msg) {
-			this.moduleCtrl.focusField(msg);
-		},
+                this.$refresh();
+            }
+        },
+
+        /**
+         * Click on an error message
+         * @param {Event} evt Not used
+         * @param {Object} msg Error message
+         */
+        clickOnMessage : function (evt, msg) {
+            this.moduleCtrl.focusField(msg);
+        },
 
         /**
          * Get the icon name for the current message type