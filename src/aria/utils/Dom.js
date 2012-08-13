ariatemplates/ariatemplates

commit bc4711c968b69eddc08d9b0d8511649435ffaaf8

maintains fix for issue 97 and resolves issue with IE8/IE9/IE10 test failure.

src/aria/utils/Dom.js
@@ -973,12 +973,6 @@ Aria.classDefinition({
          * not specified, does "minimal job".
          */
         scrollIntoView : function (element, alignTop) {
-            var hasVerticalScrollbar = element.scrollHeight > element.clientHeight;
-            var hasHorizontalScrollbar = element.scrollWidth > element.clientWidth;
-            if (!hasVerticalScrollbar && !hasHorizontalScrollbar) {
-                return;
-            }
-
             var document = element.ownerDocument;
             var origin = element, originRect = origin.getBoundingClientRect();
             var parent, hasScroll = false, docElementOk = false;
@@ -993,6 +987,10 @@ Aria.classDefinition({
                 }
 
                 if (element) {
+                    var hasScrollbar = (!element.clientHeight) ? false : element.scrollHeight > element.clientHeight;
+                    if (!hasScrollbar) {
+                        continue;
+                    }
 
                     var rects;
                     if (element == documentScroll) {

commit 939f2b039a732b84c3225e277d8b0db4cee9a884

Fixes infinite loop on Chrome introduced by commit bc4711c968b69eddc08d9b0d8511649435ffaaf8

src/aria/utils/Dom.js
@@ -1032,6 +1032,9 @@ Aria.classDefinition({
                 if (element) {
                     var hasScrollbar = (!element.clientHeight) ? false : element.scrollHeight > element.clientHeight;
                     if (!hasScrollbar) {
+                        if (element == documentScroll) {
+                            element = null;
+                        }
                         continue;
                     }
 