872d0b1fe283e3d70c38ab55abc168e6e3f8a3be
fbernardo <fabiano.bernardo@amadeus.com>
2012-09-26 15:40:10 +0200

    method hasCORS removed

--- src/aria/core/transport/XHR.js
+++ src/aria/core/transport/XHR.js
@@ -22,17 +22,5 @@
 Aria.classDefinition({
     $classpath : "aria.core.transport.XHR",
     $extends : "aria.core.transport.BaseXHR",
-    $singleton : true,
-    $constructor : function () {
-        this.$BaseXHR.constructor.call(this);
-    },
-    $prototype : {
-        /**
-         * Tells if the browser implementation of XMLHttpRequest supports CORS
-         * @return {Boolean}
-         */
-        hasCORS : function () {
-            return ("withCredentials" in this._standardXHR());
-        }
-    }
+    $singleton : true
 });
