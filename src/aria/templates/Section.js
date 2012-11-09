ariatemplates/ariatemplates

commit c0df1561c13b852bfa102dc522ee47c8836ceb34

The ids can now be incrementally generated when the test mode is turned on
Ids can now ends with a '+', an id will be automatically generated in test mode,
based on the template id and the string before the + sign.

src/aria/templates/Section.js
@@ -14,9 +14,12 @@
  */
 
 (function () {
+    var idMgr = null;
 
     var TYPE_SECTION = 0;
     var TYPE_BEHAVIOR = 1;
+
+    var regexpForId = /^[\w\-:\.]+\+?$/;
 
     /**
      * Represents a section in a template.
@@ -25,8 +28,15 @@
         $classpath : "aria.templates.Section",
         $dependencies : ["aria.utils.Array", "aria.utils.Json", "aria.utils.Delegate",
                 "aria.templates.NavigationManager", "aria.templates.CfgBeans", "aria.utils.Dom", "aria.utils.String",
-                "aria.templates.DomElementWrapper", "aria.utils.Html", "aria.templates.DomEventWrapper"],
-        /**
+                "aria.templates.DomElementWrapper", "aria.utils.Html", "aria.templates.DomEventWrapper", "aria.utils.IdManager"],
+        $onload : function () {
+            idMgr = new aria.utils.IdManager("s");
+        },
+        $onunload : function () {
+            idMgr.$dispose();
+            idMgr = null;
+        },
+       /**
          * Constructor
          * @param {aria.templates.TemplateCtxt} tplCtxt
          * @param {aria.templates.CfgBeans.SectionCfg} configuration of this section (id, type, ...).
@@ -160,10 +170,24 @@
             bindings = cfg.bindRefreshTo;
             id = cfg.id;
 
-            if (!/^[\w\-:\.]+$/.test(id)) {
-                this.$logError(this.INVALID_SECTION_ID, [this.tplCtxt.tplClasspath, id]);
-                this.cfgOk = false;
-                return;
+            var domId;
+            if (id) {
+                if (!regexpForId.test(id)) {
+                    this.$logError(this.INVALID_SECTION_ID, [this.tplCtxt.tplClasspath, id]);
+                    this.cfgOk = false;
+                    return;
+                }
+
+                if (id.indexOf('+') > -1) {
+                    if (Aria.testMode) {
+                        domId = this.tplCtxt.$getAutoId(id);
+                    }
+                    // From the application's point of view, an id with a '+' inside it is equivalent to no id at all.
+                    // We only use the id with a '+' inside it to generate the id used in the DOM
+                    delete cfg.id;
+                    id = null;
+                }
+
             }
 
             /**
@@ -173,10 +197,17 @@
             this.domType = type;
 
             /**
+             * Specify the DomId for the section
+             * @type String
+             */
+            this._domId = domId || (id ? this.tplCtxt.$getId(id) : this._createDynamicId());
+
+             /**
              * Id of the section
              * @type String
              */
-            this.id = id;
+            this.id = id || "_gen_" + this._domId;
+
 
             /**
              * CSS class for the section
@@ -245,6 +276,7 @@
         $destructor : function () {
 
             this.removeDelegateIdsAndCallbacks();
+            this._releaseAllDynamicIds();
 
             // remove delegation for this section
             if (this.delegateId) {
@@ -752,10 +784,9 @@
                     // (used in the tooltip)
                     var cssClass = this.cssClass ? ' class="' + this.cssClass + '"' : '';
                     var attributeList = this.attributes ? aria.utils.Html.buildAttributeList(this.attributes) : '';
-                    var h = ['<', this.domType, attributeList, cssClass, ' id="', this.tplCtxt.$getId(this.id), '" ',
+                    var h = ['<', this.domType, attributeList, cssClass, ' id="', this._domId, '" ',
                             aria.utils.Delegate.getMarkup(this.delegateId), '>'];
                     out.write(h.join(''));// opening the section
-                    return;
                 }
             },
             /**
@@ -906,9 +937,37 @@
              */
             getDom : function () {
                 if (!this._domElt) {
-                    this._domElt = aria.utils.Dom.getElementById(this.tplCtxt.$getId(this.id));
+                    this._domElt = aria.utils.Dom.getElementById(this._domId);
                 }
                 return this._domElt;
+            },
+
+            /**
+             * Create a dynamic id to be used in the markup generated by the widget in order to get a reference to the
+             * corresponding DOM element later (for example in initWidget).
+             * @return {String}
+             */
+            _createDynamicId : function () {
+                var res = idMgr.getId();
+                if (this._dynamicIds) {
+                    this._dynamicIds.push(res);
+                } else {
+                    this._dynamicIds = [res];
+                }
+                return res;
+            },
+
+            /**
+             * Release all dynamic ids created with _createDynamicId, so that they can be reused for other widgets.
+             */
+            _releaseAllDynamicIds : function () {
+                var dynamicIds = this._dynamicIds;
+                if (dynamicIds) {
+                    for (var i = dynamicIds.length - 1; i >= 0; i--) {
+                        idMgr.releaseId(dynamicIds[i]);
+                    }
+                    this._dynamicIds = null;
+                }
             },
 
             /**

src/aria/templates/TemplateCtxt.js
@@ -768,7 +768,8 @@
                 var differed;
                 var params = this._cfg;
                 var tpl = this._tpl;
-                var domElt = !section.id ? params.tplDiv : aria.utils.Dom.getElementById(this.$getId(section.id));
+                var domElt = section.id ? section.getDom() : params.tplDiv;
+
                 if (domElt) {
                     if (!skipInsertHTML) {
                         // replaceHTML may change domElt (especially on IE)
@@ -958,6 +959,10 @@
                 } else {
                     this._id = "tpl" + idCount;
                     idCount++;
+                }
+
+                if (Aria.testMode) {
+                    this._prefixIds = {};
                 }
 
                 // if the template has been customized, the widget returns already the actual (customized) classpath
@@ -1280,6 +1285,20 @@
             },
 
             /**
+             * Generate an automatic id computed from a base id, with an incremental counter
+             * @param {String} baseId specified in the template
+             * @return {String} The global id to be used in the dom
+             */
+            $getAutoId : function (baseId) {
+                baseId = this._id + "_" + baseId.replace(/\+/g, "");
+
+                // Calculate and return the new id
+                var prefixIds = this._prefixIds;
+                var id = prefixIds[baseId] = (prefixIds[baseId] || 0) + 1;
+                return baseId + "_auto_" + id;
+            },
+
+            /**
              * Write generated ID to DOM Element. This method is intended to be called only from the generated code of
              * templates (created in aria.templates.ClassGenerator) and never directly from developper code. A call to
              * this method is generated for the {id ...} statement
@@ -1290,8 +1309,24 @@
             __$writeId : function (id) {
                 // the id must come from the real template context (for a macro lib today, this is not the real
                 // templateCtxt)
-                var genId = this._out.tplCtxt.$getId(id);
-                this._out.write('id="' + genId + '"');
+                var genId = this._out.tplCtxt.getDomId(id);
+                if (genId) {
+                    this._out.write('id="' + genId + '"');
+                }
+            },
+
+            /**
+             * Return the generated domId for specified id.
+             * @return {String}
+             */
+            getDomId : function (id) {
+                if (id && id.indexOf("+") != -1) {
+                    if (Aria.testMode) {
+                        return this.$getAutoId(id);
+                    }
+                    return null;
+                }
+                return this.$getId(id);
             },
 
             /**

src/aria/jsunit/Assert.js
@@ -175,10 +175,7 @@
              * @param {String} optMsg optional message to add to the failure description
              */
             assertFalse : function (value, optMsg) {
-                if (!optMsg) {
-                    "Expected false. Got : " + value;
-                }
-                this.assertTrue((value === false), optMsg);
+                this.assertTrue((value === false), optMsg || ("Expected false. Got : " + value));
             },
 
             /**
@@ -188,10 +185,7 @@
              * @param {String} optMsg optional message to add to the failure description
              */
             assertEquals : function (value1, value2, optMsg) {
-                if (!optMsg) {
-                    "Expected : " + value1 + ". Got : " + value2;
-                }
-                this.assertTrue((value1 === value2), optMsg);
+                this.assertTrue((value1 === value2), optMsg || ("Expected : " + value1 + ". Got : " + value2));
             },
 
             /**
@@ -201,10 +195,7 @@
              * @param {String} optMsg optional message to add to the failure description
              */
             assertNotEquals : function (value1, value2, optMsg) {
-                if (!optMsg) {
-                    "Expected : " + value1 + ". Got : " + value2;
-                }
-                this.assertTrue((value1 !== value2), optMsg);
+                this.assertTrue((value1 !== value2), optMsg || ("Expected : " + value1 + ". Got : " + value2));
             },
 
             /**

src/aria/jsunit/TemplateTestCase.js
@@ -281,6 +281,29 @@ Aria.classDefinition({
         },
 
         /**
+         * Useful method to emulate the getElementsByClassName on browser which doesn't support this feature
+         * @param {HTMLElement} dom The source dom element
+         * @param {String} classname The class name to look for
+         * @return {Array} Array of Html elements
+         */
+       getElementsByClassName : function (dom, classname) {
+           if (dom.getElementsByClassName) {
+               return dom.getElementsByClassName(classname);
+           } else {
+               var els = dom.getElementsByTagName("*");
+               var found = [];
+               var regexp = new RegExp("\\b" + classname + "\\b");
+               for(var i = 0, ii = els.length; i < ii; i++) {
+                   var el = els[i];
+                   if (regexp.test(el.className)) {
+                       found.push(el);
+                   }
+               }
+               return found;
+           }
+        },
+
+        /**
          * @param {aria.templates.TemplateCtxt||aria.templates.Section} obj
          * @param {Array} output contains the sub-template contexts
          */

src/aria/templates/CfgBeans.js
@@ -727,8 +727,7 @@ Aria.beanDefinitions({
             $properties : {
                 "id" : {
                     $type : "json:String",
-                    $description : "Id of the section.",
-                    $mandatory : true
+                    $description : "Id of the section."
                 },
                 "type" : {
                     $type : "json:String",

src/aria/widgets/Widget.js
@@ -189,15 +189,18 @@
             }
 
             // check id
-            if (!cfg.id) {
-                // create a dynamic id
-                // for dynamic ids, it is better not to add a template context suffix,
-                // because id unicity is already ensured by construction and because adding a suffix
-                // makes ids less reusable (and IE has leaks when ids are not reused)
-                this._domId = this._createDynamicId();
-            } else {
-                this._domId = this._context.$getId(cfg.id);
+            var id = cfg.id;
+            var domId;
+            if (id && id.indexOf('+') > -1) {
+                if (Aria.testMode) {
+                    domId = this._context.$getAutoId(id);
+                }
+                // From the application's point of view, an id with a '+' inside it is equivalent to no id at all.
+                // We only use the id with a '+' inside it to generate the id used in the DOM
+                delete cfg.id;
+                id = null;
             }
+            this._domId = domId || (id ? this._context.$getId(id) : this._createDynamicId());
         },
         $destructor : function () {
             this.removeDelegation();

test/aria/templates/TemplatesTestSuite.js
@@ -48,5 +48,7 @@ Aria.classDefinition({
         this.addTests("test.aria.templates.TxtTemplateTest");
         this.addTests("test.aria.templates.ViewTest");
 
+        this.addTests("test.aria.templates.generatedId.IncrementalElementIdTestCase");
+
     }
 });

test/aria/templates/generatedId/IncrementalElementIdTestCase.js
@@ -1,0 +1,173 @@
+/**
+ * @class test.aria.templates.generatedId.IncrementalElementIdTest",
+ */
+Aria.classDefinition({
+    $classpath : "test.aria.templates.generatedId.IncrementalElementIdTestCase",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $constructor : function () {
+        this.$TemplateTestCase.constructor.call(this);
+        Aria.testMode = true;
+
+        this.setTestEnv({
+            template : "test.aria.templates.generatedId.TemplateWithSectionId",
+            data : {
+                sectionrefresh : "value1",
+                firstTime:true
+            }
+        });
+
+    },
+
+    $prototype : {
+        tearDown : function () {
+            aria.core.IO.$unregisterListeners(this);
+        },
+        _genId : function (id, index) {
+            return aria.utils.Dom.getElementById([this.templateCtxt._id, id, 'auto', index].join("_"));
+        },
+        _genTplId : function (id) {
+            return this.templateCtxt._id + id;
+        },
+
+        _checkDom : function(expectedCount) {
+            var isTestMode = Aria.testMode;
+
+            var tpl = this.testDiv.getElementsByTagName("div")[0];
+
+            // Artificially put the "bibi" class on the button, to easily target it
+            var container = this.getElementsByClassName(tpl, "button")[0];
+            var button = container.getElementsByTagName("span")[0];
+            if (button.className.indexOf("bibi") == -1) {
+                button.className += " button bibi";
+            }
+
+            for(var baseId in expectedCount) {
+                var count = expectedCount[baseId];
+                var elements = this.getElementsByClassName(tpl, baseId);
+
+                // Check element numbers
+                this.assertEquals(elements.length, count.length, "The number of elements '" + baseId + "' is incorrect");
+
+                for(var i = 0, ii = count.length; i < ii; i++) {
+                    if (elements[i]) {
+                        if (isTestMode) {
+                            this.assertEquals(elements[i].id, this._genTplId('_' + baseId + '_auto_' + count[i]));
+                        } else {
+                            var id = elements[i].id;
+                            var expected = count[i];
+                            if (!expected) {
+                                this.assertEquals(id, expected);
+                            } else {
+                                this.assertTrue(count[i].test(id), "The dom id is not the expected one (should match " + expected + ")");
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        runTemplateTest : function () {
+            this._whenTestModeOn();
+        },
+        _whenTestModeOn : function () {
+
+
+            this._checkDom({
+                "bibi" : [1, 2],
+                "toto" : [1, 2, 3, 4]
+            });
+
+            // Section refresh;
+            aria.utils.Json.setValue(this.templateCtxt.data, "firstTime", false);
+            aria.utils.Json.setValue(this.templateCtxt.data, "sectionrefresh", "_whenTestModeOn");
+
+            this._checkDom({
+                "bibi" : [3, 2],
+                "toto" : [1, 5, 4]
+            });
+
+            // Chain
+            Aria.testMode = false;
+            this._refreshTestTemplate();
+            this._whenTestModeOff();
+
+        },
+        _whenTestModeOff : function () {
+
+            this._checkDom({
+                "bibi" : [/^w.*/, ''],
+                "toto" : [/^s.*/, '', '']
+            });
+
+            // Change firstTime and refresh
+            aria.utils.Json.setValue(this.templateCtxt.data, "firstTime", false);
+            aria.utils.Json.setValue(this.templateCtxt.data, "sectionrefresh", "_whenTestModeOff");
+
+            this._checkDom({
+                "bibi" : [/^w.*/, ''],
+                "toto" : [/^s.*/, '', '']
+            });
+
+            // Chain
+            Aria.testMode = true;
+            this._replaceTestTemplate({
+                template : "test.aria.templates.generatedId.TemplateWithOutSectionId",
+                data : {
+                    sectionrefresh : "value1",
+                    firstTime : true
+                }
+            }, {
+                fn : this._whenTestModeOnWithOutSectionId,
+                scope : this
+            });
+
+        },
+        _whenTestModeOnWithOutSectionId : function () {
+
+            this._checkDom({
+                "bibi" : [1, 2],
+                "toto" : [1, 2, 3]
+            });
+
+            // Change firstTime and refresh
+            aria.utils.Json.setValue(this.templateCtxt.data, "firstTime", false);
+            aria.utils.Json.setValue(this.templateCtxt.data, "sectionrefresh", "_whenTestModeOnWithOutSectionId");
+
+            this._checkDom({
+                "bibi" : [3, 2],
+                "toto" : [4, 3]
+            });
+
+            Aria.testMode = false;
+            this._refreshTestTemplate();
+
+            this._whenTestModeOffWithOutSectionId();
+        },
+        _whenTestModeOffWithOutSectionId : function () {
+
+            this._checkDom({
+                "bibi" : [/^w.*/, ''],
+                "toto" : ['', ''],
+                "section" : [/^s.*/]
+            });
+
+            // Change firstTime and refresh
+            aria.utils.Json.setValue(this.templateCtxt.data, "firstTime", false);
+            aria.utils.Json.setValue(this.templateCtxt.data, "sectionrefresh", "_whenTestModeOnWithOutSectionId");
+
+            this._checkDom({
+                "bibi" : [/^w.*/, ''],
+                "toto" : ['', ''],
+                "section" : [/^s.*/]
+            });
+
+            Aria.testMode = false;
+
+            this._end();
+        },
+       _end : function () {
+            this.notifyTemplateTestEnd();
+        }
+
+    }
+});

commit 45fc2baf628ad885a5bf82f192e9eb5c523fe308

Fix issue introduced in commit c0df1561c13b852bfa102dc522ee47c8836ceb34
In some cases, the dom section was wrongly referenced on IE

src/aria/templates/Section.js
@@ -943,6 +943,14 @@
             },
 
             /**
+             * Set the dom element corresponding to this section.
+             * @param {HTMLElement} dom
+             */
+            setDom : function (dom) {
+                this._domElt = dom;
+            },
+
+            /**
              * Create a dynamic id to be used in the markup generated by the widget in order to get a reference to the
              * corresponding DOM element later (for example in initWidget).
              * @return {String}

src/aria/templates/TemplateCtxt.js
@@ -773,7 +773,7 @@
                 if (domElt) {
                     if (!skipInsertHTML) {
                         // replaceHTML may change domElt (especially on IE)
-                        domElt = aria.utils.Dom.replaceHTML(domElt, section.html);
+                        section.setDom(aria.utils.Dom.replaceHTML(domElt, section.html));
                     }
                     if (!section.id) {
                         // the whole template is being refreshed; let's apply the correct size