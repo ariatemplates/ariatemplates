e6969ecc117c9eb6383ffee1933b219c366036ad
Fabio Crisci <fabio.crisci@amadeus.com>
2012-12-20 17:09:59 +0100

    feat #305 remobe useless code
    Ages ago we removed .bindableProperties from widgets. Unfortunately we forgot to remove it from all classes.
    This does some cleaning of dead code.

--- src/aria/widgets/container/Tab.js
+++ src/aria/widgets/container/Tab.js
@@ -79,18 +79,6 @@ Aria.classDefinition({
         this.$Container.$destructor.call(this);
     },
     $prototype : {
-        /**
-         * Prototype init method called at prototype creation time Allows to store class-level objects that are shared
-         * by all instances
-         * @param {Object} p the prototype object being built
-         * @param {Object} def the class definition
-         * @param {Object} sdef the superclass class definition
-         */
-        $init : function (p, def, sdef) {
-            // prototype initialization function
-            // we add the bindable properties to the Widget prototype
-            p.bindableProperties = p.bindableProperties.concat(["selectedTab"]);
-        },
 
         /**
          * Called when a new instance is initialized
