ariatemplates/ariatemplates

commit b1eb1323439bc3f3203ef5b0c3880410105e8715

Autoselect on $focus
Autoselect the value of a TextInput when the field is focused
programmatically

src/aria/widgets/form/TextInput.js
@@ -814,17 +814,7 @@ Aria.classDefinition({
          * @protected
          */
         _dom_onclick : function () {
-            if (this._firstFocus && this._cfg.autoselect) {
-                // this allow to click again and put the cursor at a given
-                // position
-                this._firstFocus = false;
-                var field = this.getTextInputField();
-                var start = 0;
-                var end = (field.value.length) ? field.value.length : 0;
-                if (end) {
-                    this.setCaretPosition(start, end);
-                }
-            }
+            this._autoselect();
         },
 
         /**
@@ -1166,10 +1156,29 @@ Aria.classDefinition({
             }
             var textInputField = this.getTextInputField();
             textInputField.focus();
-            // IE FIX: requires the value to be reset for the cursor to be
-            // positioned
+            // IE FIX: requires the value to be reset for the cursor to be positioned
             // and focused at the end of the textinput.value string
             textInputField.value = textInputField.value;
+
+            this._autoselect();
+        },
+
+        /**
+         * If enabled, autoselect the widget text setting the caret position to the whole input value.
+         * @protected
+         */
+        _autoselect : function () {
+            if (this._firstFocus && this._cfg.autoselect) {
+                // this allow to click again and put the cursor at a given
+                // position
+                this._firstFocus = false;
+                var field = this.getTextInputField();
+                var start = 0;
+                var end = (field.value.length) ? field.value.length : 0;
+                if (end) {
+                    this.setCaretPosition(start, end);
+                }
+            }
         }
 
     }

commit b419220d24bb363a141985650eb2d9a80fa83ff5

fix #315 AutoComplete JS error when clicking the dropdown button
Fixing a regression after b1eb1323439bc3f3203ef5b0c3880410105e8715 using
the similar solution as in 1c8d106816f790fa65465660ee9a6b9ab375af50.

Pull request #316.

src/aria/jsunit/TemplateTestCase.js
@@ -350,13 +350,13 @@ Aria.classDefinition({
         },
 
         /**
-         * Get the icon of a Multi-Select Widget
+         * Get the expand button of a Multi-Select, Autocomplete or any other dropdown-based widget
          * @param {String} templateWidgetID
          * @return {HTMLElement} Returns directly the input element from the DOM, or null if the ID was not found or
          * didn't correspond to an input field
          */
-        getMultiSelectIcon : function (msWidgetID) {
-            var widget = this.getWidgetInstance(msWidgetID);
+        getExpandButton : function (templateWidgetID) {
+            var widget = this.getWidgetInstance(templateWidgetID);
 
             // force widget initialization
             widget.getDom();
@@ -366,6 +366,16 @@ Aria.classDefinition({
             } else {
                 return null;
             }
+        },
+
+        /**
+         * Get the icon of a Multi-Select Widget (alias to getExpandButton for backward compat)
+         * @param {String} templateWidgetID
+         * @return {HTMLElement} Returns directly the input element from the DOM, or null if the ID was not found or
+         * didn't correspond to an input field
+         */
+        getMultiSelectIcon : function (msWidgetID) {
+            return this.getExpandButton(msWidgetID);
         },
 
         /**

src/aria/widgets/form/DropDownListTrait.js
@@ -105,7 +105,7 @@ Aria.classDefinition({
          */
         _keyPressed : function (evt) {
             if (!this._hasFocus) {
-                this.focus();
+                this.focus(null, true);
                 this._handleKey({
                     charCode : evt.charCode,
                     keyCode : evt.keyCode

src/aria/widgets/form/DropDownTextInput.js
@@ -62,7 +62,7 @@ Aria.classDefinition({
          */
         _toggleDropdown : function () {
             if (!this._hasFocus) {
-                this.focus();
+                this.focus(null, true);
             }
 
             var report = this.controller.toggleDropdown(this.getTextInputField().value, this._dropdownPopup != null);
@@ -70,7 +70,7 @@ Aria.classDefinition({
                 hasFocus : true
             });
 
-            this.focus();
+            this.focus(null, true);
         },
 
         /**

src/aria/widgets/form/TextInput.js
@@ -1189,13 +1189,14 @@ Aria.classDefinition({
         },
 
         /**
-         * If enabled, autoselect the widget text setting the caret position to the whole input value.
+         * If enabled, autoselect the widget text, setting the caret position to the whole input value.
          * @protected
          */
         _autoselect : function () {
-            if (this._firstFocus && this._cfg.autoselect) {
-                // this allow to click again and put the cursor at a given
-                // position
+            // this._cfg can be null due to the following execution chain: native focus -> native blur -> AutoComplete
+            // bound value update in the data model -> bindRefreshTo section refresh
+            if (this._firstFocus && this._cfg && this._cfg.autoselect) {
+                // this allow to click again and put the cursor at a given position
                 this._firstFocus = false;
                 var field = this.getTextInputField();
                 var start = 0;

test/aria/widgets/WidgetsTestSuite.js
@@ -42,6 +42,7 @@ Aria.classDefinition({
         this.addTests("test.aria.widgets.form.TextInputTest");
         this.addTests("test.aria.widgets.form.multiselect.issue223.MultiSelect");
         this.addTests("test.aria.widgets.form.multiselect.issue312.Issue312TestSuite");
+        this.addTests("test.aria.widgets.form.autocomplete.issue315.OpenDropDownFromButtonTest");
         this.addTests("test.aria.widgets.form.datepicker.DatePickerTestSuite");
     }
 });

test/aria/widgets/form/autocomplete/issue315/Handler.js
@@ -1,0 +1,24 @@
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.form.autocomplete.issue315.Handler",
+    $implements : ["aria.resources.handlers.IResourcesHandler"],
+    $templates : ["aria.widgets.form.list.templates.LCTemplate", "aria.widgets.form.list.templates.ListTemplate"],
+    $dependencies : ["aria.widgets.form.list.templates.ListTemplateScript"],
+    $prototype : {
+
+        getSuggestions : function (text, cb) {
+            this.$callback(cb, [text, text]);
+        },
+
+        getDefaultTemplate : function () {
+            return "aria.widgets.form.list.templates.LCTemplate";
+        },
+
+        suggestionToLabel : function (text) {
+            return text;
+        },
+
+        getAllSuggestions : function (cb) {
+            this.$callback(cb, []); // the point of this test: handler returns empty suggestion list
+        }
+    }
+});

test/aria/widgets/form/autocomplete/issue315/OpenDropDownFromButtonTest.js
@@ -1,0 +1,33 @@
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.form.autocomplete.issue315.OpenDropDownFromButtonTest",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $dependencies : ["aria.utils.FireDomEvent"],
+    $constructor : function () {
+        this.$TemplateTestCase.constructor.call(this);
+        this.setTestEnv({
+            template : "test.aria.widgets.form.autocomplete.issue315.OpenDropDownFromButtonTestTpl",
+            data : {
+                email : "" // must be an empty string && res handler must return empty array in this test
+            }
+        });
+    },
+    $prototype : {
+        tearDown : function () {
+            aria.core.IO.$unregisterListeners(this);
+        },
+
+        runTemplateTest : function () {
+            aria.core.Log.getAppenders()[0].setLogs([]);
+            var expandButton = this.getExpandButton("ac1");
+            this.synEvent.click(expandButton, {
+                fn : this._openAc,
+                scope : this
+            });
+        },
+
+        _openAc : function (evt, args) {
+            this.assertLogsEmpty();
+            this.notifyTemplateTestEnd();
+        }
+    }
+})

test/aria/widgets/form/autocomplete/issue315/OpenDropDownFromButtonTestTpl.tpl
@@ -1,0 +1,32 @@
+{Template {
+   $classpath : "test.aria.widgets.form.autocomplete.issue315.OpenDropDownFromButtonTestTpl"
+}}
+
+  {macro main()}
+    {section {
+        id : "foo",
+        macro: "acMacro",
+        bindRefreshTo : [{
+        inside : this.data, // bindRefreshTo here and bind in AC refer to the same data model entry in this edge case
+        to : "email"
+        }]
+   }/}
+  {/macro}
+
+  {macro acMacro()}
+    {@aria:AutoComplete {
+        id : "ac1",
+        label : "Autoselect",
+        expandButton : true,
+        autoselect : true,
+        resourcesHandler : "test.aria.widgets.form.autocomplete.issue315.Handler",
+        bind: {
+          value: {
+            inside: this.data,
+            to: "email"
+          }
+        }
+    }/}
+  {/macro}
+
+{/Template}