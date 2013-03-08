1a5d2a1c78ad68afbaef23626c2b9b6ddee1d626
divdavem <david-emmanuel.divernois@amadeus.com>
2013-03-08 19:24:14 +0100

    feat #400 Using atpackager
    The packager used during the open-source build of Aria Templates is now separated
    in its own project called atpackager. Templates are now pre-compiled to javascript
    classes during the build. Grunt version is updated from 0.3 to 0.4.
    
    Closes #400

--- build/grunt-tasks/task-gzipstats.js
+++ build/grunt-tasks/task-gzipstats.js
@@ -1,23 +1,25 @@
 /**
  * Display file sizes of minified packages and gzipped packages.
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     var gzip = require('gzip-js');
     var fs = require('fs');
 
-    grunt.registerMultiTask('gzipStats', 'GZips the files and displays stats', function() {
+    grunt.registerMultiTask('gzipStats', 'GZips the files and displays stats', function () {
         var srcFolder = this.data.src;
         grunt.log.writeln('Calculating GZip stats for the folder: '.cyan + srcFolder.yellow);
 
-        var files = grunt.file.expandFiles([srcFolder, '**/*.js'].join('/'));
+        var files = grunt.file.expand({
+            filter : "isFile"
+        }, srcFolder + '/**/*.js');
         grunt.log.writeln('\n      min      gzip   gain  file');
         grunt.log.writeln('--------------------------------------------------------------------------------');
 
         var sizeTotal = 0, sizeGzipTotal = 0;
-        files.forEach(function(filePath){
+        files.forEach(function (filePath) {
             var sizeKbOrig = (fs.statSync(filePath).size / 1024);
-            var gzipped = grunt.helper('gzip', grunt.file.read(filePath));
+            var gzipped = callGzip(grunt.file.read(filePath));
             var sizeKbGzip = (gzipped.length / 1024);
 
             var filePathShort = filePath.replace(srcFolder, '');
@@ -29,33 +31,35 @@ module.exports = function(grunt) {
         grunt.log.writeln(size(sizeTotal) + size(sizeGzipTotal) + gain(sizeTotal, sizeGzipTotal));
 
         // Fail task if errors were logged.
-        if (this.errorCount) { return false; }
+        if (this.errorCount) {
+            return false;
+        }
         // Otherwise, print a success message
         grunt.log.ok();
     });
 
     // Return gzipped source.
-    grunt.registerHelper('gzip', function(src) {
+    function callGzip (src) {
         return src ? gzip.zip(src, {}) : '';
-    });
+    }
 
     /**
      * @param {Float} sizeKb
      */
-    function size(sizeKb) {
-        sizeKb = sizeKb.toFixed(1) ; // should have length 3..5 (e.g. '186.0' or '7.3')
+    function size (sizeKb) {
+        sizeKb = sizeKb.toFixed(1); // should have length 3..5 (e.g. '186.0' or '7.3')
         var prependLen = 6 - sizeKb.length;
-        return (new Array(prependLen+1).join(' ')) + sizeKb + ' kB ';
+        return (new Array(prependLen + 1).join(' ')) + sizeKb + ' kB ';
     }
 
     /**
      * @param {Float} sizeKbOrig
      * @param {Float} sizeKbGzip
      */
-    function gain(sizeKbOrig, sizeKbGzip){
+    function gain (sizeKbOrig, sizeKbGzip) {
         var gain = ((1 - (sizeKbGzip / sizeKbOrig)) * 100).toFixed(0);
         var prependLen = 5 - gain.length;
-        return (new Array(prependLen+1).join(' ')) + gain + '%  ';
+        return (new Array(prependLen + 1).join(' ')) + gain + '%  ';
     }
 
 };
