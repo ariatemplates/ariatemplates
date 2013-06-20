542ac192634905f6fb99128ca22990fe6230c204
divdavem <david-emmanuel.divernois@amadeus.com>
2013-06-20 10:44:04 +0200

    fix #572 Exception when accessing history.state in IE 10
    Accessing history.state sometimes raises an exception in IE10,
    instead of returning null.
    This fix surrounds accesses to history.state with try / catch
    blocks.

--- test/aria/utils/History.js
+++ test/aria/utils/History.js
@@ -38,7 +38,7 @@
 
             var browser = aria.core.Browser;
             var version = parseInt(browser.majorVersion, 10);
-            this._testInIframe = !((browser.isIE && version < 10) || (browser.isSafari && browser.environment == "Windows"));
+            this._testInIframe = !(browser.isIE || (browser.isSafari && browser.environment == "Windows"));
             this._stopTestBeforeNavigation = browser.isIE7 || browser.isPhantomJS;
             this._delayAfterNavigation = 100;
         },
