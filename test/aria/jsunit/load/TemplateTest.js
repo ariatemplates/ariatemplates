92c2f1262a1e96518a50184c57c3808c78345541
Fabio Crisci <fabio.crisci@amadeus.com>
2013-07-01 13:39:54 +0200

    fix #595 Wait Aria.load in test
    There might be cases when a test starts an Aria.load but ends before the callback is called.
    This causes undisposed objects and failures.
    
    The commit modifies the test suite to wait for completion of Aria.load before actually ending the test and starting a new one.

---
+++ test/aria/jsunit/load/TemplateTest.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.classDefinition({
+    $classpath : "test.aria.jsunit.load.TemplateTest",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $prototype : {
+        runTemplateTest : function () {
+            // Expect the complete callback to be called before actually finishing the test
+            this._expectedErrorList = ["oncomplete called"];
+
+            Aria.load({
+                classes : ["test.aria.jsunit.load.dependencies.Missed"],
+                oncomplete : {
+                    fn : this.logSomething,
+                    scope : this,
+                    args : "oncomplete called"
+                }
+            });
+
+            this.end();
+        },
+
+        logSomething : function (message) {
+            // Log something so that the tester can assert that we called the callback from checkExpectedEventListEnd
+            this.$logError(message);
+        }
+    }
+});
