ariatemplates/ariatemplates

commit 6f4e5e3586e1f1ed7f721bb16fd8ff79fa9a2c86

fix #566 test test.aria.widgets.container.tooltip.TooltipTestCase fails on IE

src/aria/jsunit/RobotTestCase.js
@@ -37,21 +37,30 @@ Aria.classDefinition({
     },
     $prototype : {
         run : function () {
-            if (this.skipTest) {
-                this.$TemplateTestCase.run.call(this);
+            var robot = aria.jsunit.Robot;
+            if (!this.skipTest && !robot.isUsable()) {
+                this._startTest();
+                this.raiseFailure('The robot is not usable');
+                this._endTest();
             } else {
-                var robot = aria.jsunit.Robot;
-                if (robot.isUsable()) {
-                    robot.initRobot({
-                        fn : this.$TemplateTestCase.run,
-                        scope : this
-                    });
-                } else {
-                    this._startTest();
-                    this.raiseFailure('Robot test cases require Java to be enabled');
-                    this._endTest();
-                }
+                this.$TemplateTestCase.run.call(this);
             }
+        },
+        _startTests : function () {
+            this.testWindow.Aria.load({
+                classes : ["aria.jsunit.SynEvents", "aria.jsunit.Robot"],
+                oncomplete : {
+                    scope: this,
+                    fn: function() {
+                        this.synEvent = this.testWindow.aria.jsunit.SynEvents;
+                        var robot = this.testWindow.aria.jsunit.Robot;
+                        robot.initRobot({
+                            fn : this.$TemplateTestCase._startTests,
+                            scope : this
+                        });
+                    }
+                }
+            });
         }
     }
 });

src/aria/jsunit/RobotJavaApplet.js
@@ -230,7 +230,8 @@ Aria.classDefinition({
             VK_X : 88,
             VK_Y : 89,
             VK_Z : 90
-        }
+        },
+        absoluteUrlRegExp : /^https?:\/\//
     },
     $prototype : {
 
@@ -255,6 +256,14 @@ Aria.classDefinition({
             if (this.applet == null) {
                 var document = Aria.$window.document;
                 var jnlp = aria.core.DownloadMgr.resolveURL('aria/jsunit/robot-applet.jnlp');
+
+                // Resolve the url depending on the base tag
+                var baseTags = document.head.getElementsByTagName("base");
+                if (baseTags && baseTags[0] && !this.absoluteUrlRegExp.test(jnlp)) {
+                    var baseUrl = baseTags[0].getAttribute("href").replace(/[^\/.]+\.[^\/.]+$/, "").replace(/\/$/, "") + "/";
+                    jnlp = baseUrl + jnlp;
+                }
+
                 var applet = document.createElement("applet");
 
                 var jnlpHrefParam = document.createElement("param");

src/aria/jsunit/TemplateTestCase.js
@@ -787,10 +787,13 @@ Aria.classDefinition({
          */
         _iframeDepLoad : function (args) {
             var document = Aria.$window.document;
+            var containerDiv = document.createElement("div");
+            containerDiv.style.cssText = "background-color:#F7F6F1;position:absolute;";
             var iframe = document.createElement("iframe");
             iframe.id = "testIframe_" + this.$class;
             iframe.style.cssText = args.def.cssText;
-            document.body.appendChild(iframe);
+            containerDiv.appendChild(iframe);
+            document.body.appendChild(containerDiv);
             args.iframe = iframe;
 
             aria.utils.FrameATLoader.loadAriaTemplatesInFrame(iframe, {

src/aria/utils/FrameATLoader.js
@@ -100,10 +100,17 @@ Aria.classDefinition({
                 scope : this,
                 args : args
             });
+
+
+            var href = Aria.$frameworkWindow.location.href.replace(/(\?|\#).*$/, "").
+                replace(/[^\/.]+\.[^\/.]+$/, "").
+                replace(/\/$/, "") +
+                "/";
             var docUrl = [aria.core.DownloadMgr.resolveURL("aria/utils/FrameATLoaderHTML.html"), '?',
-                    encodeURIComponent(Aria.$frameworkWindow.location.href.replace(/(\?|\#).*$/, "")), '#', callbackId].join('');
+                    encodeURIComponent(href), '#', callbackId].join('');
             // args.frame.contentWindow is defined only if the framework is loaded in an iframe. In the case of a new
             // window, args.frame is already the correct window object
+
             var window = args.frame.contentWindow || args.frame;
             window.location = docUrl;
         },

test/aria/widgets/container/tooltip/InnerTemplateScript.js
@@ -15,16 +15,8 @@
 
 Aria.tplScriptDefinition({
     $classpath : "test.aria.widgets.container.tooltip.InnerTemplateScript",
-    $dependencies : ["aria.jsunit.SynEvents", "aria.jsunit.Robot"],
     $prototype : {
         $viewReady : function () {
-            aria.jsunit.Robot.initRobot({
-                fn : function () {
-                    this.__IFRAME_READY__ = true;
-                },
-                scope : Aria.$window
-            });
-
             Aria.$window.scrollTo(0, 1000);
         }
     }

commit a128884ff9792c815c30c385099ab7b9d8b3997a

fix #636 test.aria.widgets.container.dialog.closeOutside.Issue389TestCase
This commit fixes a failing test on Firefox:
 test.aria.widgets.container.dialog.closeOutside.Issue389TestCase

The regression was caused by 6f4e5e3586e1f1ed7f721bb16fd8ff79fa9a2c86
along with the following bug in Firefox:
when inserting the java robot applet while a field is focused,
the field stays focused but no longer reacts when typing.

src/aria/core/JsObject.js
@@ -392,7 +392,8 @@
              * @param {aria.core.CfgBeans:Callback} cb callback description
              * @param {MultiTypes} res first result argument to pass to cb.fn (second argument will be cb.args)
              * @param {String} errorId error raised if an exception occurs in the callback
-             * @return {MultiTypes} the value returned by the callback, or undefined if the callback could not be called.
+             * @return {MultiTypes} the value returned by the callback, or undefined if the callback could not be
+             * called.
              */
             $callback : function (cb, res, errorId) {
                 if (!cb) {
@@ -649,7 +650,9 @@
                             fn : lsn.fn,
                             scope : lsn.scope,
                             args : lsn.args,
-                            once : lstCfg[evt].listenOnce
+                            once : lstCfg[evt].listenOnce,
+                            apply : lsn.apply,
+                            resIndex : lsn.resIndex
                             // we keep track of listeners which are meant to be called just once
                         };
                         // lsn is an object as in 'start' or 'end' samples set default scope

src/aria/jsunit/RobotTestCase.js
@@ -36,6 +36,12 @@ Aria.classDefinition({
         this.$TemplateTestCase.$destructor.call(this);
     },
     $prototype : {
+        /**
+         * Main method that will run all the test methods attached to the object. This method checks whether the robot
+         * is usable before calling the same method from the parent class.
+         * @see aria.jsunit.Test
+         * @override
+         */
         run : function () {
             var robot = aria.jsunit.Robot;
             if (!this.skipTest && !robot.isUsable()) {
@@ -46,21 +52,60 @@ Aria.classDefinition({
                 this.$TemplateTestCase.run.call(this);
             }
         },
-        _startTests : function () {
-            this.testWindow.Aria.load({
+
+        /**
+         * Helper function, loads the template into the div. Executes the parameter callback once the template has been
+         * successfully rendered. This method initializes the robot before calling the same method from the parent
+         * class.
+         * @param {aria.core.CfgBeans:Callback} cb Callback
+         * @protected
+         * @override
+         */
+        _loadTestTemplate : function (cb) {
+            var robot = aria.jsunit.Robot;
+            robot.initRobot({
+                fn : this.$TemplateTestCase._loadTestTemplate,
+                scope : this,
+                resIndex : -1,
+                args : cb
+            });
+        },
+
+        /**
+         * Called when the iframe and its dependencies are loaded. This method initializes the robot before calling the
+         * same method from the parent class.
+         * @param {Object} args
+         * @protected
+         * @override
+         */
+        _iframeLoad : function (args) {
+            var window = args.iframe.contentWindow;
+            window.Aria.load({
                 classes : ["aria.jsunit.SynEvents", "aria.jsunit.Robot"],
                 oncomplete : {
-                    scope: this,
-                    fn: function() {
-                        this.synEvent = this.testWindow.aria.jsunit.SynEvents;
-                        var robot = this.testWindow.aria.jsunit.Robot;
+                    scope : this,
+                    fn : function () {
+                        var robot = window.aria.jsunit.Robot;
                         robot.initRobot({
-                            fn : this.$TemplateTestCase._startTests,
-                            scope : this
+                            fn : this.$TemplateTestCase._iframeLoad,
+                            scope : this,
+                            resIndex : -1,
+                            args : args
                         });
                     }
                 }
             });
+        },
+
+        /**
+         * Callback executed when the template/data/moduleCtrl is loaded. This method makes sure the synEvent shortcut
+         * is using aria.jsunit.SynEvents from the correct frame, before calling the same method from the parent class.
+         * @protected
+         * @override
+         */
+        _templateLoadCB : function () {
+            this.synEvent = this.testWindow.aria.jsunit.SynEvents;
+            return this.$TemplateTestCase._templateLoadCB.apply(this, arguments);
         }
     }
 });