ce12be20d726de8c50f78526f39e542e4da17202
jakub-g <jakub.gieryluk@amadeus.com>
2013-07-25 16:19:37 +0200

    fix #626 test case for: pageReady event not raised
    The fix for the issue has already been integrated in 1.4.7 via
    e533d6c2b29f6fa3809c5973de3e5473638855a9. This is a missing test case.
    
    Close #643.

---
+++ test/aria/pageEngine/pageEngine/issue626/PageProvider626.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.classDefinition({
+    $classpath : "test.aria.pageEngine.pageEngine.issue626.PageProvider626",
+    $implements : ["aria.pageEngine.pageProviders.PageProviderInterface"],
+    $constructor : function () {
+
+        // These are the only important things in this test provider, the rest is a boilerplate to have page engine
+        // working. We need such an animation config which would trigger `this._isSequential()` return true in
+        // utils/css/Animations and have `animateOut` enabled.
+        this._animations = true;
+        this._animationCfg = {
+            animateIn : "flip",
+            animateOut : "flip",
+            type : 1
+        };
+    },
+
+    $prototype : {
+
+        /**
+         * @override
+         */
+        loadSiteConfig : function (callback) {
+            var siteConfig = {
+                appData : {},
+                containerId : "at-main",
+                animations : this._animations
+            };
+
+            this.$callback(callback.onsuccess, siteConfig);
+        },
+
+        /**
+         * @override
+         */
+        loadPageDefinition : function (pageRequest, callback) {
+            this.$callback(callback.onsuccess, {
+                pageId : "aaa",
+                url : "/pageEngine/aaa",
+                animation : this._animationCfg,
+                contents : {},
+                pageComposition : {
+                    template : "test.aria.pageEngine.pageEngine.site.templates.MainLayout",
+                    placeholders : {}
+                }
+            });
+        }
+    }
+});
