ariatemplates/ariatemplates

commit e6405a1b79c707f2b4090875ea2e2673f29d5c71

feat #405 Error tool tip enhancement
Currently the logic for the error tool tip is contained within
aria.widgets.form.Input, the aim of this enhancement is to share code
efficiently between multiple types of widgets. Specifically for the
requirement of this feature, this means making the code for the error tool
tip not only available for "Input" type widgets, but also for "Action"
type widgets.

In addition, it must also be possible from within a template to have a new
widget property called errorTipPosition that will be used as the preferred
position for the error tool tip, if there is not enough available space
the framework will automatically calculate the best position for the error
tool tip. The property will be added for both "Input" type widgets and
also "Action" type widgets.

Close #405

src/aria/popups/Popup.js
@@ -46,9 +46,9 @@ Aria.classDefinition({
         onBeforeOpen : "",
         onAfterOpen : "",
         onPositioned : {
-            description : "Triggered when position of the popup is choosen, according to prefered position provided",
+            description : "Triggered when position of the popup is chosen, according to prefered position provided",
             properties : {
-                position : "Position choosed if any. If empty, no position in viewset was found."
+                position : "Position chosen if any. If empty, no position in viewset was found."
             }
         }
     },
@@ -469,7 +469,6 @@ Aria.classDefinition({
          * @protected
          */
         _getPositionForAnchor : function (preferredPosition, size) {
-            var base = this.reference;
             var referenceAnchor = preferredPosition.reference;
             var popupAnchor = preferredPosition.popup;
 
@@ -632,13 +631,17 @@ Aria.classDefinition({
             } else {
                 this.computedStyle = this._getComputedStyle();
             }
-
-            this.domElement.style.cssText = ['top:', this.computedStyle.top, 'px;', 'left:', this.computedStyle.left,
-                    'px;', 'z-index:', this.computedStyle.zIndex, ';', 'position:absolute;display:inline-block;'].join('');
-            if (aria.core.Browser.isIE7 && !this.isOpen) {
-                // Without the following line, the autocomplete does not
-                // initially display its content on IE7:
-                this._document.body.appendChild(this.domElement);
+            // Need to check that the reference point is still completely visible after a scroll
+            var referenceIsInViewSet = aria.utils.Dom.isInViewport(this.referencePosition, this.referenceSize, this.domElement);
+            if (referenceIsInViewSet) {
+                this.domElement.style.cssText = ['top:', this.computedStyle.top, 'px;', 'left:',
+                        this.computedStyle.left, 'px;', 'z-index:', this.computedStyle.zIndex, ';',
+                        'position:absolute;display:inline-block;'].join('');
+                if (aria.core.Browser.isIE7 && !this.isOpen) {
+                    // Without the following line, the autocomplete does not
+                    // initially display its content on IE7:
+                    this._document.body.appendChild(this.domElement);
+                }
             }
         },
 

src/aria/widgets/CfgBeans.js
@@ -180,6 +180,9 @@ Aria.beanDefinitions({
                         },
                         "name" : {
                             $type : "common:BindingRef"
+                        },
+                        "errorTipPosition" : {
+                            $type : "common:BindingRef"
                         }
                     }
                 },
@@ -307,6 +310,11 @@ Aria.beanDefinitions({
                     $type : "json:Boolean",
                     $description : "Indicates if the label must be displayed or not (if true the label is hidden)",
                     $default : false
+                },
+                "errorTipPosition" : {
+                    $type : "json:String",
+                    $description : "Possible values are: 'bottom left', 'bottom right', 'top left', 'top right'.",
+                    $default : "top right"
                 }
             }
         },
@@ -857,6 +865,20 @@ Aria.beanDefinitions({
             $type : "WidgetCfg",
             $description : "The base configuration for the button widget",
             $properties : {
+                "bind" : {
+                    $type : "WidgetCfg.bind",
+                    $properties : {
+                        "error" : {
+                            $type : "common:BindingRef"
+                        },
+                        "errorMessages" : {
+                            $type : "common:BindingRef"
+                        },
+                        "errorTipPosition" : {
+                            $type : "common:BindingRef"
+                        }
+                    }
+                },
                 "label" : {
                     $type : "json:String",
                     $description : "Text to put inside the label in the button",
@@ -865,6 +887,25 @@ Aria.beanDefinitions({
                 "onclick" : {
                     $type : "common:Callback",
                     $description : "Function to be called when the user clicks on the button."
+                },
+                "error" : {
+                    $type : "json:Boolean",
+                    $description : "Highlights the widget to notify an error to the user, defined in a template script.",
+                    $default : false
+                },
+                "errorMessages" : {
+                    $type : "json:Array",
+                    $description : "List of error messages associated to the widget, defined in a template script - these messages will be automatically displayed in a contextual error tooltip. If this array is not empty the error property is automatically set to true",
+                    $contentType : {
+                        $type : "json:String",
+                        $description : "Error message associated to the widget"
+                    },
+                    $default : []
+                },
+                "errorTipPosition" : {
+                    $type : "json:String",
+                    $description : "Possible values are: 'bottom left', 'bottom right', 'top left', 'top right'.",
+                    $default : "top right"
                 }
             }
         },
@@ -874,7 +915,7 @@ Aria.beanDefinitions({
             $description : "The base configuration for the button widget",
             $properties : {
                 "bind" : {
-                    $type : "WidgetCfg.bind",
+                    $type : "ActionWidgetCfg.bind",
                     $properties : {
                         "disabled" : {
                             $type : "common:BindingRef"

src/aria/widgets/WidgetTrait.js
@@ -1,0 +1,51 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WidgetTrait is a class to share code between input widgets and action widgets, although this can be extended to
+ * include other widget types in the future. The purpose of this class is not to be created directly, but to allow its
+ * prototype to be imported.
+ */
+Aria.classDefinition({
+    $classpath : "aria.widgets.WidgetTrait",
+    $dependencies : ["aria.widgets.form.InputValidationHandler"],
+    $constructor : function () {
+        // The purpose of this class is to provide a prototype to be imported, not to be created directly.
+        this.$assert(11, false);
+    },
+    $prototype : {
+        /**
+         * Method used when a validation popup is needed for an input field
+         * @protected
+         */
+        _validationPopupShow : function () {
+            // check validation popup isn't already displayed
+            if (!this._onValidatePopup) {
+                this._onValidatePopup = new aria.widgets.form.InputValidationHandler(this);
+            }
+            this._onValidatePopup.show();
+        },
+
+        /**
+         * Method used to close the validation popup of an input field
+         * @protected
+         */
+        _validationPopupHide : function () {
+            if (this._onValidatePopup) {
+                this._onValidatePopup.hide();
+            }
+        }
+    }
+});

src/aria/widgets/action/ActionWidget.js
@@ -21,11 +21,11 @@
 Aria.classDefinition({
     $classpath : "aria.widgets.action.ActionWidget",
     $extends : "aria.widgets.Widget",
-    $dependencies : ["aria.utils.Function", "aria.utils.Dom", "aria.templates.DomEventWrapper"],
+    $dependencies : ["aria.utils.Function", "aria.utils.Dom", "aria.templates.DomEventWrapper",
+            "aria.widgets.WidgetTrait"],
     /**
      * ActionWidget constructor
      * @param {aria.widgets.CfgBeans.ActionWidgetCfg} cfg the widget configuration
-     * @param {aria.templates.TemplateCtxt} ctxt template context
      */
     $constructor : function () {
         this.$Widget.constructor.apply(this, arguments);
@@ -38,7 +38,10 @@ Aria.classDefinition({
         this._actingDom = null;
     },
     $destructor : function () {
-
+        if (this._onValidatePopup) {
+            this._onValidatePopup.$dispose();
+            this._onValidatePopup = null;
+        }
         if (this._actingDom) {
             this._actingDom = null;
         }
@@ -46,6 +49,21 @@ Aria.classDefinition({
         this.$Widget.$destructor.call(this);
     },
     $prototype : {
+        /**
+         * Prototype init method called at prototype creation time Allows to store class-level objects that are shared
+         * by all instances
+         * @param {Object} p the prototype object being built
+         */
+        $init : function (p) {
+            var src = aria.widgets.WidgetTrait.prototype;
+            for (var key in src) {
+                if (src.hasOwnProperty(key) && !p.hasOwnProperty(key)) {
+                    // copy methods which are not already on this object (this avoids copying $classpath and
+                    // $destructor)
+                    p[key] = src[key];
+                }
+            }
+        },
 
         /**
          * Called when a new instance is initialized
@@ -82,14 +100,20 @@ Aria.classDefinition({
          * Performs the action associated with the widget. Normally called for example when clicked or a key is pressed
          */
         _performAction : function (domEvent) {
-            if (this._cfg) {
+            var cfg = this._cfg;
+            if (cfg) {
                 var domEvtWrapper;
                 if (domEvent) {
                     domEvtWrapper = new aria.templates.DomEventWrapper(domEvent);
                 }
-                var returnValue = this.evalCallback(this._cfg.onclick, domEvtWrapper);
+                var returnValue = this.evalCallback(cfg.onclick, domEvtWrapper);
                 if (domEvtWrapper) {
                     domEvtWrapper.$dispose();
+                }
+                if (cfg.error && cfg.errorMessages.length) {
+                    this._validationPopupShow();
+                } else {
+                    this._validationPopupHide();
                 }
                 return returnValue;
             }

src/aria/widgets/form/Input.js
@@ -19,8 +19,8 @@
 Aria.classDefinition({
     $classpath : "aria.widgets.form.Input",
     $extends : "aria.widgets.Widget",
-    $dependencies : ["aria.utils.Dom", "aria.widgets.form.InputValidationHandler", "aria.utils.Data",
-            "aria.utils.String", "aria.widgets.environment.WidgetSettings", "aria.core.Browser"],
+    $dependencies : ["aria.utils.Dom", "aria.utils.Data", "aria.utils.String",
+            "aria.widgets.environment.WidgetSettings", "aria.core.Browser", "aria.widgets.WidgetTrait"],
     /**
      * Input constructor
      * @param {aria.widgets.CfgBeans.InputCfg} cfg the widget configuration
@@ -106,11 +106,18 @@ Aria.classDefinition({
          * @param {Object} sdef the superclass class definition
          */
         $init : function (p, def, sdef) {
+            var src = aria.widgets.WidgetTrait.prototype;
+            for (var key in src) {
+                if (src.hasOwnProperty(key) && !p.hasOwnProperty(key)) {
+                    // copy methods which are not already on this object (this avoids copying $classpath and
+                    // $destructor)
+                    p[key] = src[key];
+                }
+            }
             // we add the bindable properties to the Widget prototype
             p.automaticallyBindedProperties = ["formatError", "formatErrorMessages", "error", "errorMessages",
                     "requireFocus"];
         },
-
         /**
          * Override the Widget _init method
          * @protected
@@ -356,28 +363,6 @@ Aria.classDefinition({
         },
 
         /**
-         * Method used when a validation popup is needed for an input field
-         * @protected
-         */
-        _validationPopupShow : function () {
-            // check validation popup isn't already displayed
-            if (!this._onValidatePopup) {
-                this._onValidatePopup = new aria.widgets.form.InputValidationHandler(this);
-            }
-            this._onValidatePopup.show();
-        },
-
-        /**
-         * Method used to close the validation popup of an input field
-         * @protected
-         */
-        _validationPopupHide : function () {
-            if (this._onValidatePopup) {
-                this._onValidatePopup.hide();
-            }
-        },
-
-        /**
          * Internal method called when one of the model property that the widget is bound to has changed Must be
          * overridden by sub-classes defining bindable properties
          * @param {String} propertyName the property name
@@ -422,7 +407,7 @@ Aria.classDefinition({
 
         /**
          * Apply the automatic bindings
-         * @param {aria.widgets.CfgBeans.InputCfg} cfg
+         * @param {aria.widgets.CfgBeans.InputCfg|aria.widgets.CfgBeans.ActionWidgetCfg} cfg
          * @protected
          */
         _setAutomaticBindings : function (cfg) {

src/aria/widgets/form/InputValidationHandler.js
@@ -23,9 +23,39 @@ Aria.classDefinition({
     $constructor : function (widget) {
         this._context = widget._context;
         this._lineNumber = widget._lineNumber;
-        this._textInputField = widget.getTextInputField();
+        this._field = widget.getDom();
         this._WidgetCfg = widget._cfg;
         this._validationPopup = null; // null when the validation is closed
+        this._preferredPositions = {
+            "top right" : {
+                reference : "top right",
+                popup : "bottom left",
+                offset : {
+                    left : -30
+                }
+            },
+            "bottom right" : {
+                reference : "bottom right",
+                popup : "top left",
+                offset : {
+                    left : -30
+                }
+            },
+            "top left" : {
+                reference : "top left",
+                popup : "bottom right",
+                offset : {
+                    right : -30
+                }
+            },
+            "bottom left" : {
+                reference : "bottom left",
+                popup : "top right",
+                offset : {
+                    right : -30
+                }
+            }
+        };
 
         /**
          * Div for the error tooltip skinning. The reference is keep to change its state depending on the positioning of
@@ -38,7 +68,7 @@ Aria.classDefinition({
     $destructor : function () {
         this._closeValidation();
         this._context = null;
-        this._textInputField = null;
+        this._field = null;
         this._WidgetCfg = null;
         this._validationPopup = null;
     },
@@ -116,35 +146,27 @@ Aria.classDefinition({
             });
             this._validationPopup.open({
                 section : section,
-                domReference : this._textInputField,
-                preferredPositions : [{
-                            reference : "top right",
-                            popup : "bottom left",
-                            offset : {
-                                left : -30
-                            }
-                        }, {
-                            reference : "bottom right",
-                            popup : "top left",
-                            offset : {
-                                left : -30
-                            }
-                        }, {
-                            reference : "top left",
-                            popup : "bottom right",
-                            offset : {
-                                right : -30
-                            }
-                        }, {
-                            reference : "bottom left",
-                            popup : "top right",
-                            offset : {
-                                right : -30
-                            }
-                        }],
+                domReference : this._field,
+                preferredPositions : this._getPreferredPositions(),
                 closeOnMouseClick : true,
                 closeOnMouseScroll : false
             });
+        },
+
+        /**
+         * Creates an array of preferred positions, will first get the preferred position specified in the widgets
+         * errorTipPosition property.
+         * @return {Array} Returns an array of preferred positions.
+         */
+        _getPreferredPositions : function () {
+            var errorTipPosition = this._WidgetCfg.errorTipPosition;
+            var preferredPositions = [this._preferredPositions[errorTipPosition]];
+            for (var i in this._preferredPositions) {
+                if (this._preferredPositions.hasOwnProperty(i) && errorTipPosition != i) {
+                    preferredPositions.push(this._preferredPositions[i]);
+                }
+            }
+            return preferredPositions;
         },
 
         /**
@@ -177,10 +199,12 @@ Aria.classDefinition({
          */
         _onTooltipPositioned : function (evt) {
             var position = evt.position;
-            // if top right : no change of state
-            if (position && position.reference != "top right") {
-                // state is named after the position
+            if (position) {
+                // state is named after the position, except for topRight where it is named normal
                 var state = position.reference.replace(" right", "Right").replace(" left", "Left");
+                if (state === 'topRight') {
+                    state = 'normal';
+                }
                 var div = this._div, frame = div._frame;
                 div.initWidgetDom();
                 // this is backward compatibility for skin without errortooltip position states

test/aria/widgets/WidgetsTestSuite.js
@@ -19,6 +19,9 @@ Aria.classDefinition({
     $constructor : function () {
         this.$TestSuite.constructor.call(this);
 
+        this.addTests("test.aria.widgets.errorTip.ButtonErrorTipsTest");
+        this.addTests("test.aria.widgets.errorTip.IconButtonErrorTipsTest");
+        this.addTests("test.aria.widgets.errorTip.LinkErrorTipsTest");
         this.addTests("test.aria.widgets.AriaSkinInterfaceTest");
         this.addTests("test.aria.widgets.AriaSkinNormalizationTest");
         this.addTests("test.aria.widgets.WidgetTest");

commit 240266ff5f11fc5657cbaebb244381cfaa86c57c

fix #675 Dialog disappears after resize
When the viewport was resized significantly, so that dialogOldPosition.left
> newViewport.width, the Dialog disappeared and never appeared again.

The problem was introduced in e6405a1b79c707f2b4090875ea2e2673f29d5c71 by
the check if reference position is in viewport before displaying the
popup. However this check is valid only in reaction to scrolling events.

The check is now performed only in the onmousewheel event listener.

src/aria/popups/Popup.js
@@ -681,28 +681,22 @@ Aria.classDefinition({
             } else {
                 this.computedStyle = this._getComputedStyle();
             }
-            // Need to check that the reference point is still completely visible after a scroll
-            var referenceIsInViewSet = aria.utils.Dom.isInViewport(this.referencePosition, this.referenceSize, this.domElement);
-            if (referenceIsInViewSet) {
 
-                this.domElement.style.cssText = ['top:', this.computedStyle.top, 'px;', 'left:',
-                        this.computedStyle.left, 'px;', 'z-index:', this.computedStyle.zIndex, ';',
-                        'position:absolute;display:inline-block;'].join('');
+            this.domElement.style.cssText = ['top:', this.computedStyle.top, 'px;', 'left:', this.computedStyle.left,
+                    'px;', 'z-index:', this.computedStyle.zIndex, ';', 'position:absolute;display:inline-block;'].join('');
 
-                if (this.conf.animateIn) {
-                    this._startAnimation(this.conf.animateIn, {
-                        to : this.domElement,
-                        type : 1
-                    });
-                }
-
-                if (aria.core.Browser.isIE7 && !this.isOpen) {
-                    // Without the following line, the autocomplete does not
-                    // initially display its content on IE7:
-                    this._document.body.appendChild(this.domElement);
-                }
-
+            if (this.conf.animateIn) {
+                this._startAnimation(this.conf.animateIn, {
+                    to : this.domElement,
+                    type : 1
+                });
             }
+
+            if (aria.core.Browser.isIE7 && !this.isOpen) {
+                // Without the following line, the autocomplete does not initially display its content on IE7:
+                this._document.body.appendChild(this.domElement);
+            }
+
         },
 
         /**
@@ -851,8 +845,14 @@ Aria.classDefinition({
         _isScrolling : function () {
             var domReference = this.reference;
             if (domReference) {
+
                 var geometry = aria.utils.Dom.getGeometry(domReference);
-                if (geometry) {
+                var referenceIsInViewport = geometry && (aria.utils.Dom.isInViewport({
+                    left : geometry.x,
+                    top : geometry.y
+                }, geometry, this.domElement));
+
+                if (referenceIsInViewport) {
                     this.referencePosition = {
                         left : geometry.x,
                         top : geometry.y

test/aria/widgets/container/dialog/viewportResize/ViewportHugeShrinkTemplate.tpl
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+{Template {
+  $classpath:"test.aria.widgets.container.dialog.viewportResize.ViewportHugeShrinkTemplate"
+}}
+
+    {macro main()}
+        {@aria:Dialog {
+            id : "theDialog",
+            contentMacro: "dialogContent",
+            modal: false,
+            center : false,
+            visible : true,
+            width : 111,
+            height : 111,
+            bind : {
+              xpos : {
+                to : "xpos",
+                inside : data.dialog
+              },
+              ypos : {
+                to : "ypos",
+                inside : data.dialog
+              }
+            }
+        }}
+        {/@aria:Dialog}
+    {/macro}
+
+    {macro dialogContent()}
+        TEST
+    {/macro}
+
+{/Template}

test/aria/widgets/container/dialog/viewportResize/ViewportHugeShrinkTestCase.js
@@ -1,0 +1,119 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Test case for aria.widgets.container.Dialog
+ */
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.container.dialog.viewportResize.ViewportHugeShrinkTestCase",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $dependencies : ["aria.utils.Dom"],
+    $constructor : function () {
+
+        this.$TemplateTestCase.constructor.call(this);
+
+        this.data = {
+            dialog : {
+                width : 111,
+                height : 111,
+                xpos : 555,
+                ypos : 555
+            },
+            iframeWrapper : {
+                width : 700,
+                height : 700
+            }
+        };
+
+        var frameDim = this.data.iframeWrapper;
+        this.setTestEnv({
+            template : "test.aria.widgets.container.dialog.viewportResize.ViewportHugeShrinkTemplate",
+            data : this.data,
+            css : "overflow:hidden; width:" + frameDim.width + "px; height:" + frameDim.height + "px",
+            iframe : true
+        });
+
+        this.widgetUnderTestId = "theDialog";
+    },
+    $prototype : {
+
+        /*
+         * The aim of this test is to check what happens when the viewport is dramatically shrinked, so that the
+         * Dialog's [x,y] coords before resize were bigger than new viewport's width & height. (e.g. dialog was
+         * positioned at 555x555, and the new window size is much smaller and doesn't contain this reference point).
+         *
+         * The core of the possible issues is that if something goes wrong during the execution of popup handling logic,
+         * the popup, which is temporarily positioned in a remote place at [-15000, -15000], might not be moved back
+         * to the viewport.
+         */
+        runTemplateTest : function () {
+            // in Chrome, overflow:hidden on iframe is not enough, need to set it on <HTML> inside iframe
+            this.testWindow.document.documentElement.style.overflow = "hidden";
+            this._testIsInViewportAfterHugeViewportShrink();
+        },
+
+        _testIsInViewportAfterHugeViewportShrink : function () {
+            this.__resizeIframe(-400, this.__checkVisibility);
+        },
+        __checkVisibility : function () {
+            var geometry = this.__getDialogGeometry();
+
+            // calling AT in an iframe to use iframe's viewport in computations
+            var inViewport = this.testWindow.aria.utils.Dom.isInViewport({
+                top : geometry.y,
+                left : geometry.x
+            }, {
+                width : geometry.width,
+                height : geometry.height
+            });
+
+            this.assertTrue(inViewport, "Dialog is not in the viewport after window resize");
+            this.end();
+        },
+
+        __resizeIframe : function (delta, continueWith) {
+            var initialDimensions = this.data.iframeWrapper;
+            this.testIframe.style.width = (initialDimensions.width + delta) + "px";
+            this.testIframe.style.height = (initialDimensions.height + delta) + "px";
+            this.waitFor({
+                condition : function () {
+                    return this.__getDialog(); // wait until the dialog is re-instantiated
+                },
+                callback : {
+                    fn : continueWith,
+                    scope : this
+                }
+            });
+        },
+
+        /**
+         * @override
+         */
+        notifyTemplateTestEnd : function () {
+            Aria.$window.document.documentElement.style.backgroundColor = "#fff";
+            this.$TemplateTestCase.notifyTemplateTestEnd.call(this);
+        },
+
+        __getDialog : function () {
+            return this.getWidgetInstance(this.widgetUnderTestId);
+        },
+
+        __getDialogGeometry : function () {
+            var container = this.getWidgetInstance(this.widgetUnderTestId);
+            var geometry = aria.utils.Dom.getGeometry(container.getDom());
+            return geometry;
+        }
+    }
+});