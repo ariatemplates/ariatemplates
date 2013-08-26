e4f8181e7f0397714f937806d1e819fefb340383
jakub-g <jakub.gieryluk@amadeus.com>
2013-08-26 14:14:20 +0200

    fix #670 add ko_KR, zh_CN, tw_CN, ar_SA locales
    ko_KO, cn_CN, tw_TW, sp_AR locales are deprecated and will be removed in
    the future.
    
    Close #669.

---
+++ src/aria/resources/multiselect/FooterRes_zh_TW.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Aria resource object for multiselect footer zh_TW
+ */
+Aria.resourcesDefinition({
+    $classpath : 'aria.resources.multiselect.FooterRes',
+    $resources : {
+        selectAll : "\u5168\u90e8\u9078\u64c7",
+        deselectAll : "\u5168\u90e8\u53d6\u6d88\u9078\u64c7",
+        close : "\u95dc\u9589"
+    }
+});
