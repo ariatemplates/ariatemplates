c1d263e884fb4cc9d433496a70a869570482205a
divdavem <david-emmanuel.divernois@amadeus.com>
2013-09-16 10:29:04 +0200

    refactor Removing intermediate variable when calling Aria.classDefinition.
    Some files are using an intermediate variable before calling
    Aria.classDefinition or Aria.tplScriptDefinition.
    This is useless, and can prevent some automatic tools from working on
    those files.
    This commit removes the intermediate variable, which makes Aria Templates
    files more consistent.
    
    Close #729.

--- src/aria/tester/runner/utils/Hash.js
+++ src/aria/tester/runner/utils/Hash.js
@@ -13,84 +13,78 @@
  * limitations under the License.
  */
 
-(function () {
-    /**
-     * @class aria.tester.runner.utils.Hash
-     */
-    var classDefinition = {
-        $classpath : "aria.tester.runner.utils.Hash",
-        $singleton : true,
-        $constructor : function () {
-            this._hashParameters = {};
+Aria.classDefinition({
+    $classpath : "aria.tester.runner.utils.Hash",
+    $singleton : true,
+    $constructor : function () {
+        this._hashParameters = {};
+    },
+    $prototype : {
+        /**
+         * @private
+         */
+        _getHash : function () {
+            return Aria.$window.document.location.hash;
         },
-        $prototype : {
-            /**
-             * @private
-             */
-            _getHash : function () {
-                return Aria.$window.document.location.hash;
-            },
 
-            /**
-             * @private
-             */
-            _setHash : function (hash) {
-                Aria.$window.document.location.hash = hash;
-            },
+        /**
+         * @private
+         */
+        _setHash : function (hash) {
+            Aria.$window.document.location.hash = hash;
+        },
 
-            /**
-             *
-             */
-            getParameter : function (key) {
-                var hash = this._getHash();
+        /**
+         *
+         */
+        getParameter : function (key) {
+            var hash = this._getHash();
 
-                // remove initial #
-                hash = hash.substring(1);
+            // remove initial #
+            hash = hash.substring(1);
 
-                var keyvalues = hash.split("&");
-                for (var i = 0, l = keyvalues.length; i < l; i++) {
-                    var keyvalue = keyvalues[i];
-                    if (keyvalue.indexOf("=") != -1) {
-                        var split = keyvalue.split("=");
-                        if (key == split[0]) {
-                            return split[1];
-                        }
+            var keyvalues = hash.split("&");
+            for (var i = 0, l = keyvalues.length; i < l; i++) {
+                var keyvalue = keyvalues[i];
+                if (keyvalue.indexOf("=") != -1) {
+                    var split = keyvalue.split("=");
+                    if (key == split[0]) {
+                        return split[1];
                     }
                 }
-                return "";
-            },
-
-            setParameter : function (key, value) {
-                var hash = this._getHash();
-                var hashParam = [key, value].join("=");
-                // remove initial #
-                hash = hash.substring(1) || "";
-
-                var found = false;
-                var keyvalues = hash.split("&");
-                for (var i = 0, l = keyvalues.length; i < l; i++) {
-                    var keyvalue = keyvalues[i];
-                    if (keyvalue.indexOf("=") != -1) {
-                        var split = keyvalue.split("=");
-                        if (key == split[0]) {
-                            found = true;
-                            keyvalues[i] = hashParam;
-                        }
-                    }
-                }
-
-                if (!found) {
-                    if (keyvalues[keyvalues.length - 1] === "") {
-                        keyvalues[keyvalues.length - 1] = hashParam;
-                    } else {
-                        keyvalues.push(hashParam);
-                    }
-                }
-
-                hash = "#" + keyvalues.join("&");
-                this._setHash(hash);
             }
+            return "";
+        },
+
+        setParameter : function (key, value) {
+            var hash = this._getHash();
+            var hashParam = [key, value].join("=");
+            // remove initial #
+            hash = hash.substring(1) || "";
+
+            var found = false;
+            var keyvalues = hash.split("&");
+            for (var i = 0, l = keyvalues.length; i < l; i++) {
+                var keyvalue = keyvalues[i];
+                if (keyvalue.indexOf("=") != -1) {
+                    var split = keyvalue.split("=");
+                    if (key == split[0]) {
+                        found = true;
+                        keyvalues[i] = hashParam;
+                    }
+                }
+            }
+
+            if (!found) {
+                if (keyvalues[keyvalues.length - 1] === "") {
+                    keyvalues[keyvalues.length - 1] = hashParam;
+                } else {
+                    keyvalues.push(hashParam);
+                }
+            }
+
+            hash = "#" + keyvalues.join("&");
+            this._setHash(hash);
         }
-    };
-    Aria.classDefinition(classDefinition);
-})();
+    }
+});
