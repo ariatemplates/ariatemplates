132cece764de3f7d8e24095c53bb15b931a48afc
jakub-g <jakub.gieryluk@amadeus.com>
2013-12-06 17:11:40 +0100

    refactor remove verbosity from DOCTYPEs
    Let's just use HTML5's <!DOCTYPE html> everywhere.
    Also strip some indentation in `FrameATLoaderHTML` which is deployed
    unminified.
    
    Close #876.

--- src/aria/jsunit/TestReport.js
+++ src/aria/jsunit/TestReport.js
@@ -85,7 +85,7 @@ Aria.classDefinition({
          * @return {String}
          */
         getMailHeader : function () {
-            var header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
+            var header = '<!DOCTYPE html>'
                     + '<html>'
                     + '<head>'
                     + '<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">'
