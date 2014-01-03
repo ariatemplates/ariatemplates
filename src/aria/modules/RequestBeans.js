e40083c25eb0858d0d002ceef6cfc7f757cad94d
jakub-g <jakub.g.opensource@gmail.com>
2014-01-03 10:34:29 +0100

    feat #102 add `timeout` for `RequestMgr.sendJsonRequest`/`submitJsonRequest`
    It's been always possible to pass a `timeout` to the calls of
        `aria.core.IO.asyncRequest`
        `aria.core.IO.asyncFormSubmit`
    but not to the calls of
        `aria.modules.RequestMgr.submitJsonRequest`
        `aria.modules.RequestMgr.sendJsonRequest`
    
    The latter only had the `aria.core.IO.defaultTimeout` applied.
    
    This commit makes it possible to pass `timeout` as the key of the request
    parameter object (first parameter) of them.
    
    Close #102. Close #913.

--- src/aria/modules/RequestBeans.js
+++ src/aria/modules/RequestBeans.js
@@ -64,6 +64,10 @@ Aria.beanDefinitions({
                     $type : "env:RequestJsonSerializerCfg",
                     $description : "JSON serializer settings that have to be used for this request"
                 },
+                "timeout" : {
+                    $type : "json:Integer",
+                    $description : "Timeout in milliseconds (after which the request is canceled if no answer was received before). If this parameter is not set, the default timeout applies (specified in aria.core.IO.defaultTimeout). This property can be changed by filters."
+                },
                 /* Backward Compatibility begins here */
                 "postHeader" : {
                     $type : "json:String",
