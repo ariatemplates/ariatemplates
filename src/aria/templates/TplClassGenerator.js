afde7d4fa4c59c976f1427bc40b56a5f947e5685
flongo <francesco.longo@amadeus.com>
2014-03-21 17:23:48 +0100

    feat #1043 Removal of bindRefreshTo statement
    `bindRefreshTo` statement was deprecated in version 1.4.2 (commit 9596760c59ca923ab03c91c3b935e65a7cd5b36f). It is now removed for good.
    
    Close #1043

--- src/aria/templates/TplClassGenerator.js
+++ src/aria/templates/TplClassGenerator.js
@@ -26,8 +26,7 @@ Aria.classDefinition({
         this.$ClassGenerator.constructor.call(this);
 
         // Load the Template specific statements
-        this._loadStatements(["Template", "id", "on", "createView", "section", "@", "repeater"
-                /* BACKWARD-COMPATIBILITY-BEGIN */, "bindRefreshTo"/* BACKWARD-COMPATIBILITY-END */]);
+        this._loadStatements(["Template", "id", "on", "createView", "section", "@", "repeater"]);
 
         // Redefine the protected parser
         this._parser = aria.templates.TplParser;
