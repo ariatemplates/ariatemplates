ariatemplates/ariatemplates

commit 6584f219fad148129fa6de28cc434e1303405f9d

Now escaping is actually done automatically for expression statements.
## Code improvement

Implementing the feature didn't require a lot of code change (basically just removing the code keeping the backward compatible mode). However, the code required some cleaning. The comments/JSDoc as well have been improved.

- implementation of the feature (in the Statements class)
- implementation and tests for the modifier implementation: notably adding the documentation for the "arg" argument (corresponding to the "options" argument of the String utility, see next point)
- implementation and tests for the related String utility implementation: notably it's easier now to understand how the "options" argument works

## Internal code update regarding the change

Expression intentionally outputting HTML have been identified and changed to disable the automatic escaping.

Concerned pieces of code are:

- some tests
- some widgets' templates
- the tester application
- the debug tools (contextual menu)

## Unrelated fixes

- the aria.utils.Bridge class can now actually load the development files of the framework (used for the debug tools).
- in JSDoc comments: some parameters were documented with the @param[in] syntax, which is not understood by the JSDoc parser we use, so I removed this syntax in favor of the usual one @param

src/aria/widgets/calendar/CalendarTemplate.tpl
@@ -50,7 +50,7 @@
         {/for}
         {if settings.showShortcuts}
             <div style="text-align: center; margin: 1px;">
-                <a title="${calendar.today|dateformat:settings.completeDateLabelFormat}" tabIndex="-1" href="javascript:;" {on click {fn: "navigate", scope: moduleCtrl, args: {date:calendar.today}}/}>${res.today}</a>
+                <a title="${calendar.today|dateformat:settings.completeDateLabelFormat|escapeForHTML:{attr: true}}" tabIndex="-1" href="javascript:;" {on click {fn: "navigate", scope: moduleCtrl, args: {date:calendar.today}}/}>${res.today}</a>
                 {section {
                     id : "selectedDay",
                     macro : "selectedDay"
@@ -61,7 +61,7 @@
 
     {macro selectedDay()}
         {if settings.value}
-            &nbsp;|&nbsp; <a title="${settings.value|dateformat:settings.completeDateLabelFormat}" tabIndex="-1" href="javascript:;" {on click {fn: "navigate", scope: moduleCtrl, args: {date:settings.value}}/}>${res.selectedDate}</a>
+            &nbsp;|&nbsp; <a title="${settings.value|dateformat:settings.completeDateLabelFormat|escapeForHTML:{attr: true}}" tabIndex="-1" href="javascript:;" {on click {fn: "navigate", scope: moduleCtrl, args: {date:settings.value}}/}>${res.selectedDate}</a>
         {/if}
     {/macro}
 

src/aria/templates/Modifiers.js
@@ -62,18 +62,37 @@
             /**
              * <span style="font-weight: bold">MODIFIER</span>
              * <br/>
-             * Use the aria.utils.String.escapeForHTML utility to process the given input. If the input is null or
-             * undefined, it is returned as is.
+             *
+             * Use the <a href="http://ariatemplates.com/aria/guide/apps/apidocs/#aria.utils.String:escapeForHTML:method">aria.utils.String.escapeForHTML</li> utility to process the given input, converting the latter to a string before sending it.
+             *
+             * However this modifier skips the escaping in several cases, returning the value as is (and thus without type conversion):
+             * <ul>
+             * <li>if the input is null or undefined</li>
+             * <li>if the argument passed to control the escaping finally tells not to escape anything</li>
+             * </li>
+             *
              * @name aria.templates.Modifiers.prototype.escapeForHTML
-             * @param {String} str the input string
-             * @return {String} the processed string
-             * @see aria.utils.String.escapeForHTML
+             *
+             * @param input the input value
+             * @param arg the argument forwarded to the <a href="http://ariatemplates.com/aria/guide/apps/apidocs/#aria.utils.String:escapeForHTML:method">aria.utils.String.escapeForHTML</li> method. Please refer to its own documentation for more information.
+             *
+             * @return the processed input value. See detailed description for more information.
+             *
+             * @see <a href="http://ariatemplates.com/aria/guide/apps/apidocs/#aria.utils.String:escapeForHTML:method">aria.utils.String.escapeForHTML</li>
              */
-            fn : function (s, arg) {
-                if (s == null) {
-                    return s;
+            fn : function (input, arg) {
+                if (input == null) {
+                    return input;
                 }
-                return aria.utils.String.escapeForHTML(s + '', arg);
+
+                var infos = {};
+                var output = aria.utils.String.escapeForHTML(input + '', arg, infos);
+
+                if (!infos.escaped) {
+                    return input;
+                }
+
+                return output;
             }
         },
         "capitalize" : {
@@ -97,14 +116,20 @@
              * @name aria.templates.Modifiers.prototype.default
              * @param {String} str the entry
              * @param {String} defaultValue the default value
-             * @param {String} escape the name of the escaping modifier function to use to process the given default
+             * @param {String} escapeModifierName the name of the escaping modifier function to use to process the given default
              * value. When empty, this value is left as is.
              * @return {String}
              */
-            fn : function (str, defaultValue, escape) {
-                return str != null ? str : escape
-                        ? aria.templates.Modifiers.callModifier(escape, [defaultValue])
-                        : defaultValue;
+            fn : function (str, defaultValue, escapeModifierName) {
+                if (str != null) {
+                    return str;
+                }
+
+                if (modifierExists(escapeModifierName)) {
+                    return aria.templates.Modifiers.callModifier(escapeModifierName, [defaultValue]);
+                }
+
+                return defaultValue;
             }
         },
         "empty" : {
@@ -115,14 +140,20 @@
              * @name aria.templates.Modifiers.prototype.empty
              * @param {String} str the entry
              * @param {String} defaultValue the default value
-             * @param {String} escape the name of the escaping modifier function to use to process the given default
+             * @param {String} escapeModifierName the name of the escaping modifier function to use to process the given default
              * value. When empty, this value is left as is.
              * @return {String}
              */
-            fn : function (str, defaultValue, escape) {
-                return !!str && !/^\s*$/.test(str) ? str : escape
-                        ? aria.templates.Modifiers.callModifier(escape, [defaultValue])
-                        : defaultValue;
+            fn : function (str, defaultValue, escapeModifierName) {
+                if (!!str && !/^\s*$/.test(str)) {
+                    return str;
+                }
+
+                if (modifierExists(escapeModifierName)) {
+                    return aria.templates.Modifiers.callModifier(escapeModifierName, [defaultValue]);
+                }
+
+                return defaultValue;
             }
         },
         "pad" : {
@@ -344,6 +375,17 @@
             }
         }
     };
+
+    /**
+     * Tests whether or not the specified modifier is available.
+     *
+     * @param {String} name The name of the modifier. Should be a String but any value is handled.
+     *
+     * @return true is the modifier exists, false otherwise.
+     */
+    function modifierExists(name) {
+        return name != null && __modifiers.hasOwnProperty(('' + name).toLowerCase());
+    }
 
     /**
      * Template modifiers. Modifiers can be used inside the template syntax

src/aria/templates/Statements.js
@@ -96,33 +96,38 @@ Aria.classDefinition({
                     }
                     parts.push(param);
 
-                    // Check if the secure must be done automatically or not:
-                    // - either the modifier is present in the list and thus we leave the default behavior of modifiers
-                    // - either we are in the context of a CSS Template and we decide not to escape automatically
-                    var automaticSecure = true;
+                    // Automatic escape detection ------------------------------
 
-                    var escapeModifier = classGenerator.escapeModifier;
-                    if (!escapeModifier) {
-                        automaticSecure = false;
+                    // We automatically escape expressions if needed, using the feature of modifiers, since the escape is already handled by a modifier itself.
+                    // There are two cases where we don't do it automatically:
+                    // - in CSS templates
+                    // - if the modifier is already used, in which case the user has full control on how to apply the escape
+                    var automaticEscape = true;
+
+                    var escapeModifierName = classGenerator.escapeModifier; // Gets the actual name of the modifier in this context
+                    if (escapeModifierName == null) {
+                        automaticEscape = false;
                     } else {
                         for (var i = parts.length - 1; i >= 1; i--) {
                             var part = parts[i];
-                            if (part.indexOf(escapeModifier) === 0) {
-                                automaticSecure = false;
+                            if (part.indexOf(escapeModifierName) === 0) {
+                                automaticEscape = false;
                                 break;
                             }
                         }
                     }
 
-                    /* Begin non backward compatible change */
-                    var automaticSecure = false;
-                    /* End non backward compatible change */
+                    // end of: automatic escape detection ----------------------
 
-                    // If the secure must be done automatically, we add the modifier at the end of the list, to rely on
-                    // this available mechanism
-                    if (automaticSecure) {
-                        parts.splice(1, 0, escapeModifier);
+                    // Automatic escape application - part 1 -------------------
+
+                    // If the escape must be done automatically, we add the modifier at the beginning of the list.
+
+                    if (automaticEscape) {
+                        parts.splice(1, 0, escapeModifierName);
                     }
+
+                    // end of: automatic escape application - part 1 -----------
 
                     var beginexpr = [], endexpr = [];
                     var expr;
@@ -140,9 +145,17 @@ Aria.classDefinition({
                         expr = match[2]; // parameters of the modifier
                         if (expr) {
                             endexpr[i] = ", " + expr;
-                            if (automaticSecure) {
-                                endexpr[i] += ", '" + escapeModifier + "'";
+                            // Automatic escape application - part 2 -----------
+
+                            // We automatically pass to any modifier the name of the escaping modifier as last parameter.
+
+                            // This is done so that the other modifiers know that we are in automatic escape mode, and so that they know which modifier to use to do the escape. Useful for the implementation of modifiers receiving possibly untrusted data.
+
+                            if (automaticEscape) {
+                                endexpr[i] += ", '" + escapeModifierName + "'";
                             }
+
+                            // end of: Automatic escape application - part 2 ---
                             endexpr[i] += "])";
                         } else {
                             endexpr[i] = "])";

src/aria/tester/runner/view/config/Config.tpl
@@ -64,7 +64,7 @@
                     {var suiteName = this.getSuiteName(testWrapper)/}
                     {var isSelected =  test.isSelected()/}
                     {call displaySelect(isSelected)/}
-                    <b>${suiteName}</b>
+                    <b>${suiteName|escapeForHTML:false}</b>
                         {call displaySuite(test, 1)/}
                     </td>
                 </tr>
@@ -90,7 +90,7 @@
                     <td style="padding-left:${5 + (15*nesting)}px">
                         {var isSelected = instance && instance.isSelected()/}
                         {call displaySelect(isSelected)/}
-                        <b>${suiteName}</b>
+                        <b>${suiteName|escapeForHTML:false}</b>
                         {call displaySuite(instance, nesting+1)/}
                     </td>
                 </tr>

src/aria/tester/runner/view/header/Header.tpl
@@ -62,7 +62,7 @@
           <div
             {on click {fn:"_onStartTestsButtonClick", scope: this, args: {}}/}
             id="startTestsButton" class="${cssclass}">
-            ${this.getButtonLabel()}
+            ${this.getButtonLabel()|escapeForHTML:false}
         </div>
     {/macro}
 

src/aria/tester/runner/view/links/Links.tpl
@@ -31,7 +31,7 @@
                         href="${link.href}"
                         target="_blank"
                     >
-                        ${link.title}
+                        ${link.title|escapeForHTML:false}
                     </a>
                 </li>
             {/foreach}
@@ -44,7 +44,7 @@
                             scope : this
                         }/}
                     >
-                        Press <b>${link.key}</b> : ${link.description}
+                        Press <b>${link.key}</b> : ${link.description|escapeForHTML:false}
                     </a>
                 </li>
             {/foreach}

src/aria/tester/runner/view/popup/generic/Generic.tpl
@@ -40,7 +40,7 @@
                 scope : this
             }/}
         >
-            ${getLabelWithShortcut(label)}
+            ${getLabelWithShortcut(label)|escapeForHTML:false}
         </div>
     {/macro}
 {/Template}

src/aria/tester/runner/view/popup/report/Report.tpl
@@ -49,10 +49,10 @@
 
     {macro displayTestErrors(test)}
         <div class="classpath">
-            ${this.formatTestClasspath(test)}
+            ${this.formatTestClasspath(test)|escapeForHTML:false}
         </div>
         <div class="count">
-            (${this.formatTestErrorsCount(test)})
+            (${this.formatTestErrorsCount(test)|escapeForHTML:false})
         </div>
         {var errors = this.getTestErrors(test)/}
         {if errors.length == 0}
@@ -68,7 +68,7 @@
     {macro displayTestError(error, test)}
         <li class="error">
             <div class="message">
-                ${this.formatErrorMessage(error)}
+                ${this.formatErrorMessage(error)|escapeForHTML:false}
             </div>
         </li>
     {/macro}

commit 2c08ebb307c4821d96f1dc2f80a2e0f159270b8a

fix #1071 fixing auto-escaping issue in calendar widget
This commit fixes the issue introduced by commit
6584f219fad148129fa6de28cc434e1303405f9d in the calendar
template.

src/aria/widgets/calendar/CalendarTemplate.tpl
@@ -112,7 +112,7 @@
     {macro renderDay(day, month)}
         {var jsDate=day.jsDate/}
         {if day.monthKey==month.monthKey}
-            <td ${day.isSelectable ? "data-date=\""+jsDate.getTime()+"\"":""}
+            <td {if day.isSelectable} data-date="${jsDate.getTime()}" {/if}
                 class="${getClassForDay(day)}"
             >${day.label}</td>
         {else/}