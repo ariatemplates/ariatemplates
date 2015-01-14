ariatemplates/ariatemplates

commit 2aa83bb279c1f10d11fca7ee46e52869b3c4ee67

fix #1377 loading overlay juddering on scroll in Safari Mobile
Since overlay implementation was using fixed positioning,
the user experience was not very good in Safari Mobile
(e.g. on ipad) because the browser doesn't do repaints
of fixed-positioned elements during scrolling.

The implementation of overlay was changed to use equivalent
absolute positioning instead.

src/aria/utils/overlay/LoadingOverlay.js
@@ -95,10 +95,19 @@ module.exports = Aria.classDefinition({
             var overlayGeometry = null;
 
             if (geometry) {
-                overlayGeometry = {
-                    x : Math.max(geometry.x, 0),
-                    y : Math.max(geometry.y, 0)
-                };
+                if (overlay.style.position == "absolute") {
+                    // geometry is relative to viewport
+                    var documentScroll = aria.utils.Dom._getDocumentScroll();
+                    overlayGeometry = {
+                        x : Math.max(geometry.x + documentScroll.scrollLeft, 0),
+                        y : Math.max(geometry.y + documentScroll.scrollTop, 0)
+                    };
+                } else { // fixed
+                    overlayGeometry = {
+                        x : Math.max(geometry.x, 0),
+                        y : Math.max(geometry.y, 0)
+                    };
+                }
 
                 overlayGeometry.width = Math.min(geometry.width + Math.min(geometry.x, 0), viewportSize.width
                         - overlayGeometry.x);

src/aria/utils/overlay/Overlay.js
@@ -70,7 +70,16 @@ module.exports = Aria.classDefinition({
                 overlay.id = "xOverlay" + params.id;
             }
             overlay.className = params.className || "xOverlay";
-            overlay.style.position = params.position || "fixed";
+
+            // absolute positioning is preferred over fixed since Safari Mobile doesn't repaint fixed elements on scroll
+            var position = params.position || "absolute";
+
+            // Place it in the top-left corner of the page, otherwise if it happens to be inserted far in the DOM, it
+            // may temporarily create unnecessary scrollbars on the page - this may affect proper positioning of the
+            // overlay. We'll set left/top later.
+            overlay.style.left = 0;
+            overlay.style.top = 0;
+            overlay.style.position = position;
 
             return overlay;
         },

commit 0f640aaf11e17e59af50640951784fd39fddda94

fix #1377 ScrollableBodyTest regression
Fixed a test failure introduced in the commit
2aa83bb279c1f10d11fca7ee46e52869b3c4ee67
(PR #1377) causing the loading overlay not to be displayed
when page is scrolled too far.

The source of the bug was mixing viewport-relative
and page-relative coordinates when establishing
overlay's width and height.

Close #1386

src/aria/utils/overlay/LoadingOverlay.js
@@ -95,24 +95,36 @@ module.exports = Aria.classDefinition({
             var overlayGeometry = null;
 
             if (geometry) {
+                var width1, width2, height1, height2;
+
+                var overlayGeometryViewportRelative = {
+                    x : Math.max(geometry.x, 0),
+                    y : Math.max(geometry.y, 0)
+                };
                 if (overlay.style.position == "absolute") {
                     // geometry is relative to viewport
+                    // overlayGeometry will be relative to the page!
                     var documentScroll = aria.utils.Dom._getDocumentScroll();
                     overlayGeometry = {
                         x : Math.max(geometry.x + documentScroll.scrollLeft, 0),
                         y : Math.max(geometry.y + documentScroll.scrollTop, 0)
                     };
                 } else { // fixed
-                    overlayGeometry = {
-                        x : Math.max(geometry.x, 0),
-                        y : Math.max(geometry.y, 0)
-                    };
+                    // geometry is relative to viewport
+                    // overlayGeometry will be relative to the viewport too
+                    overlayGeometry = overlayGeometryViewportRelative;
                 }
 
-                overlayGeometry.width = Math.min(geometry.width + Math.min(geometry.x, 0), viewportSize.width
-                        - overlayGeometry.x);
-                overlayGeometry.height = Math.min(geometry.height + Math.min(geometry.y, 0), viewportSize.height
-                        - overlayGeometry.y);
+                // "cut-off" the overlay if the element stands out of the viewport to the left / top
+                width1 = geometry.width + Math.min(geometry.x, 0);
+                height1 = geometry.height + Math.min(geometry.y, 0);
+
+                // "cut-off" the overlay if the element stands out of the viewport to the right / bottom
+                width2 = viewportSize.width - overlayGeometryViewportRelative.x;
+                height2 = viewportSize.height - overlayGeometryViewportRelative.y;
+
+                overlayGeometry.width = Math.min(width1, width2);
+                overlayGeometry.height = Math.min(height1, height2);
             }
 
             var style = overlay.style;

test/aria/utils/overlay/loadingIndicator/scrollableBody/ScrollableBodyTest.js
@@ -43,6 +43,7 @@ Aria.classDefinition({
         _addLoadingIndicatorOnSpan : function () {
             this.domOverlay.create(this.spanWithLoader, "just a message");
             this.domOverlay.create(this.body, "just a message");
+
             this._testOverlay(this.spanWithLoader, "none");
             this._testBodyOverlay();
 
@@ -65,7 +66,6 @@ Aria.classDefinition({
                 scope : this,
                 delay : 300
             });
-
         },
 
         _afterSecondScroll : function () {
@@ -140,15 +140,16 @@ Aria.classDefinition({
             tolHeight = tolHeight || 0;
             var overlay = this.domOverlay.__getOverlay(element).overlay.overlay;
             if (display) {
-                this.assertEquals(overlay.style.display, display);
+                var msg = "The overlay on " + element.tagName + " had display: %1 instead of %2";
+                this.assertEquals(overlay.style.display, display, msg);
             }
             if (width) {
                 var actualWidth = parseInt(overlay.style.width, 10);
-                this.assertTrue(Math.abs(actualWidth - width) <= tolWidth, "Wrong width");
+                this.assertEqualsWithTolerance(actualWidth, width, tolWidth, "Wrong width, expected %2, got %1");
             }
             if (height) {
                 var actualHeight = parseInt(overlay.style.height, 10);
-                this.assertTrue(Math.abs(actualHeight - height) <= tolHeight, "Wrong height");
+                this.assertEqualsWithTolerance(actualHeight, height, tolHeight, "Wrong height, expected %2, got %1");
             }
         },
 