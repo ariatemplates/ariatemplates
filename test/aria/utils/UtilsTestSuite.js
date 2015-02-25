df611023ccf5f615b7dd8e8cffbdfea1ce76c1cb
ymeine <yannick.meine@amadeus.com>
2015-02-25 16:51:11 +0100

    feat Refactoring of Browser and Device classes: deletion of deprecated Orientation class.
    There is another commit dealing with the deletion of deprecated code, but this one completes it by removing some code which has been missed.

--- test/aria/utils/UtilsTestSuite.js
+++ test/aria/utils/UtilsTestSuite.js
@@ -66,7 +66,6 @@ Aria.classDefinition({
         this.addTests("test.aria.utils.TypeTest");
         this.addTests("test.aria.utils.Xml");
         this.addTests("test.aria.utils.DeviceTest");
-        this.addTests("test.aria.utils.OrientationTest");
         this.addTests("test.aria.utils.dragdrop.DragTestSuite");
         this.addTests("test.aria.utils.events.EventsTestSuite");
     }
