ee03589e95c3b0871f67e4c900a4b24da65786dc
fbasso <fabrice.basso@amadeus.com>
2015-06-11 17:17:11 +0200

    [WAI-ARIA] implementation for simple textinput widgets
    close #1489

--- test/aria/widgets/wai/textInputBased/WaiTextInputBasedTestSuite.js
+++ test/aria/widgets/wai/textInputBased/WaiTextInputBasedTestSuite.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Amadeus s.a.s.
+ * Copyright 2012 Amadeus s.a.s.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,12 +14,12 @@
  */
 
 Aria.classDefinition({
-    $classpath : "test.aria.widgets.wai.WaiTestSuite",
+    $classpath : "test.aria.widgets.wai.textInputBased.WaiTextInputBasedTestSuite",
     $extends : "aria.jsunit.TestSuite",
     $constructor : function () {
         this.$TestSuite.constructor.call(this);
 
-        this.addTests("test.aria.widgets.wai.autoComplete.WaiAutoCompleteTestSuite");
-        this.addTests("test.aria.widgets.wai.textInputBased.WaiTextInputBasedTestSuite");
+        this.addTests("test.aria.widgets.wai.textInputBased.TextInputTestCase");
+        this.addTests("test.aria.widgets.wai.textInputBased.TextInputNonWaiTestCase");
     }
 });
