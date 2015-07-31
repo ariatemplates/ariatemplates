ariatemplates/ariatemplates

commit c3e296e18ae8f02ae7cfb0f0a10ec80b5be17a1f

fix #1487 easypackage grunt task modified binary files
Binary files (such as images) were modified by the easypackage grunt
task when copied to the destination folder.

tasks/easypackage/configuration.js
@@ -111,10 +111,7 @@ module.exports = function (grunt, args) {
                 }, {
                     type : 'CopyUnpackaged',
                     cfg : {
-                        files : ['**/*', '!**/*.js'],
-                        builder : {
-                            type : 'ATMultipart'
-                        }
+                        files : ['**/*']
                     }
                 }, args.checkPackaged ? 'CheckPackaged' : null],
         packages : args.packages

test/node/gruntTasks.js
@@ -42,7 +42,7 @@ var testBuild = function (index, callback) {
 
 };
 
-var veryfyIOswfFile = function () {
+var verifyIOswfFile = function () {
     var originalFile = fs.readFileSync(srcPath + "aria/resources/handlers/IO.swf", "hex");
     var outputFile, outputFolderPath = testProjectPath + "target/fwk/aria/resources/handlers";
     var containerDir = fs.readdirSync(outputFolderPath);
@@ -54,6 +54,19 @@ var veryfyIOswfFile = function () {
 
     return originalFile == outputFile;
 
+};
+
+var verifyImgFiles = function (outputFolder) {
+    var outputFolderPath = testProjectPath + "target/" + outputFolder + "/app/css/img";
+    var containerDir = fs.readdirSync(outputFolderPath);
+    var checks = 0;
+    containerDir.forEach(function (fileName) {
+        checks++;
+        var outputFile = fs.readFileSync(outputFolderPath + "/" + fileName, "hex");
+        var originalFile = fs.readFileSync(testProjectPath + "src/app/css/img/" + fileName, "hex");
+        assert.strictEqual(outputFile, originalFile, fileName + " was modified by the build.");
+    });
+    assert.strictEqual(checks, 2, "Wrong number of files in app/css/img");
 };
 
 describe("easypackage grunt task", function () {
@@ -72,7 +85,7 @@ describe("easypackage grunt task", function () {
             }
 
             try {
-                assert.ok(veryfyIOswfFile(), "IO.swf file has been modified by the build");
+                assert.ok(verifyIOswfFile(), "IO.swf file has been modified by the build");
             } catch (ex) {
                 assert.ok(false, "IO.swf file has been modified by the build");
             }
@@ -97,6 +110,9 @@ describe("easypackage grunt task", function () {
             assert.ok(/abcdefg/.test(packageContent), "License has not been added");
             assert.ok(/require\(\\"ariatemplates\/Aria\\"\)/.test(packageContent), "Conversion to noderJS syntax missing");
             assert.ok(!/\{Template/.test(packageContent), "Template compilation missing");
+
+            verifyImgFiles("one");
+
             callback();
         });
     });
@@ -125,6 +141,9 @@ describe("easypackage grunt task", function () {
                 flag = false;
             }
             assert.ok(flag, "The second target of the task was not executed correctly");
+
+            verifyImgFiles("two");
+
             callback();
         });
     });

commit 93ade97a5742bbc375bd4a1d016e4c35cd86f819

fix #1515 easypackage failing when templates are not packaged
This commit fixes a regression introduced by commit c3e296e18ae8f02ae7cfb0f0a10ec80b5be17a1f.
It also makes sure dependencies declared in templates are correctly
detected during packaging.

tasks/easypackage/configuration.js
@@ -41,16 +41,17 @@ module.exports = function (grunt, args) {
             }
         },
         outputDirectory : args.outputDirectory,
-        visitors : [args.includeDependencies ? {
-                    type : 'NoderDependencies',
-                    cfg : {
-                        externalDependencies : ['noder-js/**'],
-                        files : ['**/*.js']
-                    }
-                } : null, args.includeDependencies ? {
+        visitors : [
+                args.includeDependencies ? {
                     type : 'ATDependencies',
                     cfg : {
                         externalDependencies : ['aria/**'],
+                        files : atExtensions
+                    }
+                } : null, args.includeDependencies ? {
+                    type : 'NoderDependencies',
+                    cfg : {
+                        externalDependencies : ['noder-js/**', 'ariatemplates/**'],
                         files : atExtensions
                     }
                 } : null, args.includeDependencies ? {
@@ -100,7 +101,7 @@ module.exports = function (grunt, args) {
                 } : null, {
                     type : 'CopyUnpackaged',
                     cfg : {
-                        files : ['**/*.js'],
+                        files : atExtensions,
                         builder : {
                             type : 'ATMultipart',
                             cfg : {

test/node/gruntTasks.js
@@ -147,5 +147,27 @@ describe("easypackage grunt task", function () {
             callback();
         });
     });
+    it("should build the application correctly - third config", function (callback) {
+        testBuild(3, function () {
+            // Checks that the .tpl.css file is present and was compiled
+            var AutocompleteStyleTplCss = fs.readFileSync(testProjectPath + "target/three/app/css/AutocompleteStyle.tpl.css", 'utf8');
+            assert.ok(/abcdefg-license/.test(AutocompleteStyleTplCss), "License has not been added");
+            assert.ok(!/\{Template/.test(AutocompleteStyleTplCss), "Template compilation has not been done");
 
+            var flag;
+            try {
+                fs.readFileSync(testProjectPath + "target/three/app/css/CalendarSkin.js", 'utf8');
+                flag = false;
+            } catch (ex) {
+                flag = true;
+            }
+            assert.ok(flag, "Automatic dependency inclusion did not work");
+
+            var calendarPkg = fs.readFileSync(testProjectPath + "target/three/app/css/calendar.js", 'utf8');
+            assert.ok(/loadFileContent\("app\/css\/CalendarSkin\.js","/.test(calendarPkg), "Package app/css/calendar.js does not include CalendarSkin.js");
+            assert.ok(/loadFileContent\("app\/css\/CalendarStyle\.tpl\.css","/.test(calendarPkg), "Package app/css/calendar.js does not include CalendarStyle.tpl.css");
+            verifyImgFiles("three");
+            callback();
+        });
+    });
 });

test/nodeTestResources/testProject/gruntfiles/Gruntfile3.js
@@ -1,0 +1,45 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var path = require('path');
+module.exports = function (grunt) {
+    grunt.registerTask('default', ['easypackage:one']);
+    grunt.loadTasks('../../../../tasks');
+    grunt.config.set('easypackage.one', {
+        options : {
+            // Compile all templates, packages just 2 files
+            outputDirectory : path.join(__dirname, "../target/three"),
+            sourceDirectories : [path.join(__dirname, '../src')],
+            sourceFiles : ['**/*'],
+            license : "/* abcdefg-license */",
+            ATAppEnvironment : {
+                defaultWidgetLibs : {
+                    "light" : "atplugins.lightWidgets.LightWidgetLib"
+                }
+            },
+            includeDependencies : true,
+            packages : [{
+                name : "app/css/calendar.js",
+                // includeDependencies is true so atpackager should automatically
+                // add CalendarSkin.js to this package
+                files : ["app/css/CalendarStyle.tpl.css"]
+            }],
+            clean : true,
+            minify : false,
+            gzipStats : true,
+            hash : false
+        }
+    });
+
+};