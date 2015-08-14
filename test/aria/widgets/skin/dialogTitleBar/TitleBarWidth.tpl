ariatemplates/ariatemplates

commit bf282635ed67e89b33bb4c4e0f8b3629ba4cd182

fix #1474 Better dialog width management depending on the title length

src/aria/widgets/container/Dialog.js
@@ -405,6 +405,7 @@ module.exports = Aria.classDefinition({
                 this._cfg.title = newValue;
                 if (this._titleDomElt) {
                     this._titleDomElt.innerHTML = newValue;
+                    this._onDimensionsChanged();
                 }
             } else if (propertyName === "macro") {
                 this._cfg.macro = newValue;
@@ -580,9 +581,12 @@ module.exports = Aria.classDefinition({
             var cfg = this._cfg;
             var getDomElementChild = ariaUtilsDom.getDomElementChild;
             this._domElt = this._popup.domElement;
-            this._titleBarDomElt = getDomElementChild(this._domElt, 0, true);
-            this._titleDomElt = getDomElementChild(this._titleBarDomElt, cfg.icon ? 1 : 0);
+            var titleBarDomElt = this._titleBarDomElt = getDomElementChild(this._domElt, 0, true);
+            this._titleDomElt = getDomElementChild(titleBarDomElt, cfg.icon ? 1 : 0);
+            this._onDimensionsChanged();
+
             this._calculatePosition();
+
             if (cfg.modal) {
                 ariaTemplatesNavigationManager.focusFirst(this._domElt);
             }
@@ -709,6 +713,7 @@ module.exports = Aria.classDefinition({
          * @protected
          */
         _updateDivSize : function (viewport) {
+            var cfg = this._cfg;
             var math = ariaUtilsMath;
 
             var maxHeight, maxWidth;
@@ -720,16 +725,51 @@ module.exports = Aria.classDefinition({
                 maxWidth = math.min(this._cfg.maxWidth, viewport.width);
             }
 
+            var titleBarDomElt = this._titleBarDomElt;
+            var titleDomElt = this._titleDomElt;
+
+            var isIE7 = aria.core.Browser.isIE7;
+            if (isIE7) {
+                // without this, IE 7 gives wrong inner sizes
+                titleDomElt.style.overflow = "visible";
+            }
+
+            titleDomElt.style.width = "";
+            var titleWidth = ariaUtilsDom.getGeometry(titleDomElt).width;
+            var childNodes = titleBarDomElt.childNodes;
+            var iconsWidth = 0;
+            for (var i = 1, ii = childNodes.length; i < ii; i++) {
+                iconsWidth += childNodes[i].offsetWidth;
+            }
+
+            // First manage the width of the modal depending on the title bar and the max width
+            var shadows = this._shadows;
+            var minWidth = ariaUtilsMath.max(cfg.width, cfg.minWidth);
+            if (cfg.width == -1) {
+                minWidth = ariaUtilsMath.max(titleWidth + iconsWidth + shadows.left + shadows.right, minWidth);
+            }
+
             // if maximized == true, then height|widthMaximized will be used; otherwise normal width and height
             this._div.updateSize({
                 maxHeight : maxHeight,
                 maxWidth : maxWidth,
-                height : this._cfg.height,
-                width : this._cfg.width,
-                heightMaximized : this._cfg.heightMaximized,
-                widthMaximized : this._cfg.widthMaximized,
-                maximized : this._cfg.maximized
+                minWidth : minWidth,
+                height : cfg.height,
+                width : cfg.width,
+                heightMaximized : cfg.heightMaximized,
+                widthMaximized : cfg.widthMaximized,
+                maximized : cfg.maximized
             });
+
+            // The manage the title length to manage the text-overflow
+            var titleBarWidth = ariaUtilsDom.getGeometry(titleBarDomElt).width;
+            titleDomElt.style.width = (titleBarWidth - iconsWidth - shadows.left - shadows.right) + "px";
+
+            if (isIE7) {
+                // Back to overflow hidden mode
+                titleDomElt.style.overflow = "hidden";
+            }
+
         },
 
         /**

src/aria/widgets/container/DialogStyle.tpl.css
@@ -132,6 +132,9 @@
             padding-top: 6px;
             color:${skinClass.titleColor};
             font-weight:bold;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
         }
         .xDialog_${skinClassName}_icon {
             padding-left: 6px;

src/aria/widgets/container/Div.js
@@ -106,7 +106,7 @@ module.exports = Aria.classDefinition({
          */
         updateSize : function (cfg) {
             var hasChanged = false, prefName, newVal;
-            var prefs = ['maxWidth', 'maxHeight', 'width', 'height'];
+            var prefs = ['maxWidth', 'maxHeight', 'minWidth', 'width', 'height'];
 
             for (var i = 0, len = prefs.length; i < len; i++) {
                 prefName = prefs[i];

test/aria/widgets/container/dialog/sizes/DialogMaxWidthTestCase.js
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Test for issue #10
+ */
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.container.dialog.sizes.DialogMaxWidthTestCase",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $dependencies : ["aria.utils.Json"],
+    $constructor : function () {
+        this.$TemplateTestCase.constructor.call(this);
+
+        this.$json = aria.utils.Json;
+
+        this.data = {
+            title: "Short",
+            width: 150
+        };
+
+        this.setTestEnv({
+            template : "test.aria.widgets.container.dialog.sizes.DialogMaxWidthTestCaseTpl",
+            data : this.data
+        });
+    },
+    $prototype : {
+
+        checkSizes : function(dialogId, mainWidth) {
+            var dialog = this.dialogs[dialogId];
+
+            var dom = dialog._domElt;
+            var children = dom.children;
+            var dlgBody = children[0];
+            var dlgTitle = children[1];
+            var dlgTitleText = aria.utils.Dom.getElementsByClassName(dlgTitle, "sDialog_title");
+            var dlgCloseIcon = aria.utils.Dom.getElementsByClassName(dlgTitle, "sDialog_close");
+
+            this.assertEqualsWithTolerance(dlgBody.offsetWidth, mainWidth, 8, dialogId + " width should be %2 instead of %1");
+            this.assertEqualsWithTolerance(dlgTitle.offsetWidth, mainWidth - 10, 8, dialogId + " title width should be %2 instead of %1");
+            this.assertEquals(dlgTitleText.offsetTop, dlgCloseIcon.offsetTop, "The close icon should be on teh first line");
+        },
+
+
+        runTemplateTest : function () {
+            // The main template is Ready even if it contains a template inside the Dialog, wait
+            var dialogIds = ["dialog1", "dialog2", "dialog3", "dialog4", "dialog5"];
+            this.waitFor({
+                condition : function() {
+                    // Wait for every dialog ready and open
+                    var dialogs = this.dialogs = {};
+                    for (var i = 0; i < dialogIds.length; i += 1) {
+                        var id = dialogIds[i];
+                        var currentInstance = dialogs[id] = this.getWidgetInstance(id);
+                        if (!currentInstance || !currentInstance._popup.isOpen) {
+                            return false;
+                        }
+                    }
+                    return true;
+                },
+                callback : {
+                    fn : this.checkWidths,
+                    scope : this
+                }
+            });
+        },
+
+        checkWidths : function () {
+            this.checkSizes("dialog1", 400);
+            this.checkSizes("dialog2", 400);
+            this.checkSizes("dialog3", 238);
+            this.checkSizes("dialog4", 400);
+
+            this.changeContainerSize();
+        },
+
+        changeContainerSize : function () {
+
+            var data = this.data;
+            this.checkSizes("dialog5", 200);
+            this.$json.setValue(data, "title", "This is now a very long title");
+            this.checkSizes("dialog5", 251);
+
+            this.$json.setValue(data, "title", "This is now a very very very very very very very long title");
+            this.checkSizes("dialog5", 400);
+
+            this.$json.setValue(data, "title", "Short again");
+            this.checkSizes("dialog5", 200);
+
+            this.$json.setValue(data, "width", 500);
+            this.checkSizes("dialog5", 400);
+
+            this.end();
+        }
+    }
+});

test/aria/widgets/container/dialog/sizes/DialogMaxWidthTestCaseTpl.tpl
@@ -1,0 +1,110 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+{Template {
+    $classpath : "test.aria.widgets.container.dialog.sizes.DialogMaxWidthTestCaseTpl"
+}}
+
+    {macro main()}
+
+        {@aria:Dialog {
+            visible : true,
+            macro : "largeContent",
+            title: "Very very very very very very very very very very very very long title",
+            id : "dialog1",
+            maximizable : true,
+            center : false,
+            xpos : 20,
+            ypos : 20,
+            maxHeight: 350,
+            maxWidth: 400
+        }/}
+
+        {@aria:Dialog {
+            visible : true,
+            macro : "largeContent",
+            title: "Normal title",
+            id : "dialog2",
+            center : false,
+            xpos : 450,
+            ypos : 20,
+            maxHeight: 400,
+            maxWidth: 400
+        }/}
+
+        {@aria:Dialog {
+            visible : true,
+            macro : "smallContent",
+            title: "Title larger than the content",
+            id : "dialog3",
+            center : false,
+            xpos : 900,
+            ypos : 20,
+            maxHeight: 400,
+            maxWidth: 400
+        }/}
+
+        {@aria:Dialog {
+            visible : true,
+            macro : "smallContent",
+            title: "Very very very very very very very very very very very very long title",
+            id : "dialog4",
+            center : false,
+            xpos : 1150,
+            ypos : 20,
+            maxHeight: 400,
+            maxWidth: 400
+        }/}
+
+        {@aria:Dialog {
+            visible : true,
+            macro : "dynamicWidth",
+            title: "Normal title",
+            bind: {
+                title: {
+                    to : "title",
+                    inside : data
+                }
+            },
+            id : "dialog5",
+            center : false,
+            xpos : 900,
+            ypos : 200,
+            maxHeight: 400,
+            maxWidth: 400
+        }/}
+
+    {/macro}
+
+    {macro largeContent ()}
+        <div style="width:700px; height: 3000px">Large dialog content</div>
+    {/macro}
+    {macro dynamicWidth ()}
+        {section {
+            macro: "dynamicWidthContent",
+            bindRefreshTo: [{
+                inside: data,
+                to: "width"
+            }]
+        }/}
+    {/macro}
+    {macro dynamicWidthContent ()}
+        <div style="width:${data.width}px; height: 3000px">Large dialog content</div>
+    {/macro}
+    {macro smallContent ()}
+        <div style="padding: 20px">Dialog content</div>
+    {/macro}
+
+{/Template}

commit 7831c3ce0404f885e7e3d8cb7828a4c1dc5dbcf0

fix #1517 the calculation of the Dialog widget title bar. The issue was introduced in commit: bf282635ed67e89b33bb4c4e0f8b3629ba4cd182 released in version 1.7.8 When padding is added to either the title bar element or the title text element, then the width is calculated incorrectly. This fix takes into account the paddings for those elements and calculates the correct width accordingly.
close #1519

src/aria/utils/Dom.js
@@ -31,7 +31,7 @@ module.exports = Aria.classDefinition({
         DIV_NOT_FOUND : "Missing div '%1' in DOM.",
         INSERT_ADJACENT_INVALID_POSITION : "Invalid position %1. Expected one of: beforeBegin, afterBegin, beforeEnd or afterEnd.",
 
-        pxRegExp : /^[0-9]+px$/
+        pxRegExp : /^[0-9]+(\.[0-9]+)?px$/
 
     },
     $prototype : {
@@ -1332,7 +1332,7 @@ module.exports = Aria.classDefinition({
         getStylePx : function (element, property, defaultValue) {
             var value = this.getStyle(element, property);
             if (this.pxRegExp.test(value)) {
-                return parseInt(value, 10);
+                return Math.round(parseFloat(value));
             }
             return defaultValue;
         },

src/aria/widgets/container/Dialog.js
@@ -756,6 +756,7 @@ module.exports = Aria.classDefinition({
 
             titleDomElt.style.width = "";
             var titleWidth = titleDomElt.offsetWidth;
+            var titleBarPaddings = ariaUtilsDom.getStylePx(titleBarDomElt, "paddingLeft", 0) + ariaUtilsDom.getStylePx(titleBarDomElt, "paddingRight",0);
             var childNodes = titleBarDomElt.childNodes;
             var iconsWidth = 0;
             for (var i = 0, ii = childNodes.length; i < ii; i++) {
@@ -767,7 +768,7 @@ module.exports = Aria.classDefinition({
             var shadows = this._shadows;
             var minWidth = ariaUtilsMath.max(cfg.width, cfg.minWidth);
             if (cfg.width == -1) {
-                minWidth = ariaUtilsMath.max(titleWidth + iconsWidth + shadows.left + shadows.right, minWidth);
+                minWidth = ariaUtilsMath.max(titleBarPaddings + titleWidth + iconsWidth + shadows.left + shadows.right, minWidth);
             }
 
             // if maximized == true, then height|widthMaximized will be used; otherwise normal width and height
@@ -783,8 +784,9 @@ module.exports = Aria.classDefinition({
             });
 
             // The manage the title length to manage the text-overflow
-            var titleBarWidth = titleBarDomElt.offsetWidth;
-            titleDomElt.style.width = ariaUtilsMath.max(titleBarWidth - iconsWidth - shadows.left - shadows.right, 0) + "px";
+            var titleBarInnerWidth = titleBarDomElt.clientWidth - titleBarPaddings;
+            var titlePaddings = ariaUtilsDom.getStylePx(titleDomElt, "paddingLeft", 0) + ariaUtilsDom.getStylePx(titleDomElt, "paddingRight", 0);
+            titleDomElt.style.width = ariaUtilsMath.max(titleBarInnerWidth - titlePaddings - iconsWidth - shadows.left - shadows.right - 1, 0) + "px";
 
             if (isIE7) {
                 // Back to overflow hidden mode

test/aria/widgets/skin/AllSkinTestSuite.js
@@ -21,5 +21,6 @@ Aria.classDefinition({
 
         this.addTests("test.aria.widgets.skin.dropdownwidth.DropdownWidthTestCase");
         this.addTests("test.aria.widgets.skin.button.verticalAlign.ButtonVerticalAlignTestSuite");
+        this.addTests("test.aria.widgets.skin.dialogTitleBar.TitleBarWidthTestCase");
     }
 });

test/aria/widgets/skin/dialogTitleBar/TitleBarWidth.tpl
@@ -1,0 +1,90 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+{Template {
+  $classpath:"test.aria.widgets.skin.dialogTitleBar.TitleBarWidth"
+}}
+
+    {macro main()}
+        {@aria:Dialog {
+          id : "theDialog",
+          maxWidth: 800,
+          modal: true,
+          center: true,
+          visible: true,
+          closeOnMouseClick: true,
+          closable: true,
+          sclass: "std",
+          title: "Hotel - 1 Night",
+          macro: "dialogContent"
+        }/}
+    {/macro}
+
+    {macro dialogContent()}
+        <div class="hotelDetails">
+          <div class="hotel">
+            <div class="providerHeader">
+              <div class="providerName">
+                <div class="mainTitle">GRANGER</div>
+                <div class="subTitle">Global Hospitality</div>
+              </div>
+            </div>
+          <div class="address">
+            <span class="addressType">Location: </span>2-5 LETS BE AVENUE, LONDON WC1Z 6BT United Kingdom
+            <span class="distance"> (1.2 kilometers from airport)</span>
+            <br>
+            <span class="addressType">Voice: </span>44-207-0000000
+            <br>
+            <span class="addressType">Fax: </span>44-207-0000001
+            <br>
+            <span class="addressType">Telex: </span>N/A
+          </div>
+
+          <div class="description">With a spectacular private garden overlooking and adjoining the world renowned British Museum, the Grange White Hall is in a unique private location, just minutes away from the myriad theatre and fashion attractions of London’s west end.</div>
+
+          <div class="separator"></div>
+          <div class="roomDescription">
+            <span class="addressType">Room: </span>Moderate with bath/shower
+            <br>BEST AVAILABLE RATE EX VAT
+            <br>SUPERIOR SINGLE FRENCH QUEEN BED MIN 18 SQM
+            <br>DEDICATED WORK DESK LAMP AND HI SPEED INTERNET
+          </div>
+
+        </div>
+
+        <div class="hotelFacilitiesContainer">
+          <ul class="hotelFacilities">
+
+            <li class="hotelFacility">Restaurants</li>
+
+            <li class="hotelFacility">Bar</li>
+
+            <li class="hotelFacility">Laundry service</li>
+
+            <li class="hotelFacility">Room service</li>
+
+            <li class="hotelFacility">Safe dep box</li>
+
+            <li class="hotelFacility">PC hookup in room</li>
+
+            <li class="hotelFacility">Meeting rooms</li>
+
+          </ul>
+        </div>
+
+        </div>
+    {/macro}
+
+{/Template}

test/aria/widgets/skin/dialogTitleBar/TitleBarWidthTestCase.js
@@ -1,0 +1,55 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Test case for test.aria.widgets.skin.dialogTitleBar.DialogTitleBarTest
+ */
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.skin.dialogTitleBar.TitleBarWidthTestCase",
+    $extends : "aria.jsunit.TemplateTestCase",
+    $dependencies : ["aria.utils.Dom"],
+    $constructor : function () {
+        this.$TemplateTestCase.constructor.call(this);
+        this.setTestEnv({
+            template : "test.aria.widgets.skin.dialogTitleBar.TitleBarWidth"
+        });
+    },
+    $prototype : {
+        /**
+         * Test base layout
+         */
+        runTemplateTest : function () {
+            this._checkCloseButtonPosition("theDialog");
+            this.end();
+        },
+
+        getDialogCloseButton : function (titleBar) {
+            var child = titleBar.lastChild;
+            while (child && ! (/close/.test(child.className))) {
+                child = child.previousSibling;
+            }
+            return child;
+        },
+
+        _checkCloseButtonPosition : function (id) {
+            var dialog = this.getWidgetInstance(id);
+            var titleBar = dialog._titleBarDomElt;
+            var closeButton = this.getDialogCloseButton(titleBar);
+            var titleBarGeometry = aria.utils.Dom.getGeometry(titleBar);
+            var closeButtonGeometry = aria.utils.Dom.getGeometry(closeButton);
+            this.assertEquals(titleBarGeometry.y, closeButtonGeometry.y);
+        }
+    }
+});