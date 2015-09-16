3f0ce123780f7950bcaad343a38c9214bd040101
fbasso <fabrice.basso@amadeus.com>
2015-09-16 09:56:29 +0200

    Addition of resources for Vietnamese language
    close #1534, AT-1156, PTR-10344117

---
+++ src/aria/resources/DateRes_vi_VN.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2012 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.resourcesDefinition({
+    $classpath : 'aria.resources.DateRes',
+    $resources : {
+        day : [
+            "Chủ nhật",
+            "Thứ hai",
+            "Thứ ba",
+            "Thứ tư",
+            "Thứ năm",
+            "Thứ sáu",
+            "Thứ bẩy"
+        ],
+        // a false value for the following items mean: use substring
+        // to generate the short versions of days or months
+        dayShort : false,
+        monthShort : false,
+        month : [
+            "Tháng một",
+            "Tháng hai",
+            "Tháng ba",
+            "Tháng tư",
+            "Tháng năm",
+            "Tháng sáu",
+            "Tháng bẩy",
+            "Tháng tám",
+            "Tháng chín",
+            "Tháng mười",
+            "Tháng mười một",
+            "Tháng mười hai"
+        ]
+    }
+});
