ariatemplates/ariatemplates

commit f9d30b69f19e3d472f6b4c93ddbbb210776507d8

fix #1572 Texttfield autoselect doesn't work with JAWS and the arrow keys
AT-1242
CR 08528144

src/aria/widgets/form/TextInput.js
@@ -911,7 +911,6 @@ module.exports = Aria.classDefinition({
          * @protected
          */
         _dom_onclick : function () {
-            this._autoselect();
             if (!!this._cfg.onclick) {
                 this.evalCallback(this._cfg.onclick);
             }
@@ -965,6 +964,8 @@ module.exports = Aria.classDefinition({
             if (!!this._cfg.onfocus && !avoidCallback) {
                 this.evalCallback(this._cfg.onfocus);
             }
+
+            this._autoselect();
         },
 
         /**
@@ -1263,16 +1264,6 @@ module.exports = Aria.classDefinition({
               textInputField.value = textInputField.value;
             }
 
-            if (!fromSelf) {
-                // IE FIX: the focus() can be asynchronous, so let's add a timeout to manage the autoselect
-                ariaCoreTimer.addCallback({
-                    fn : function () {
-                        this._autoselect();
-                    },
-                    scope : this,
-                    delay : 25
-                });
-            }
         },
 
         /**

test/aria/widgets/form/TextInputTest.js
@@ -213,6 +213,7 @@ Aria.classDefinition({
             var instance1 = tf1.instance;
             instance1._hasFocus = true;
             instance1._dom_onclick();
+            instance1._dom_onfocus();
             if (!aria.core.Browser.isOldIE) {
                 this.assertTrue(instance1._textInputField.selectionStart === 0);
                 this.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
@@ -222,6 +223,7 @@ Aria.classDefinition({
             instance1._textInputField.selectionEnd = 0;
             instance1._cfg.autoselect = false;
             instance1._dom_onclick();
+            instance1._dom_onfocus();
             if (!aria.core.Browser.isOldIE) {
                 this.assertTrue(instance1._textInputField.selectionEnd === 0);
             }

test/aria/widgets/form/TextareaTest.js
@@ -219,6 +219,7 @@ Aria.classDefinition({
             var instance1 = tf1.instance;
             instance1._hasFocus = true;
             instance1._dom_onclick();
+            instance1._dom_onfocus();
             if (!aria.core.Browser.isOldIE) {
                 this.assertTrue(instance1._textInputField.selectionStart === 0);
                 this.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
@@ -228,6 +229,7 @@ Aria.classDefinition({
             instance1._textInputField.selectionEnd = 0;
             instance1._cfg.autoselect = false;
             instance1._dom_onclick();
+            instance1._dom_onfocus();
             if (!aria.core.Browser.isOldIE) {
                 this.assertTrue(instance1._textInputField.selectionEnd === 0);
             }

commit cbbfb2ddc1b232a9964fd0f7756fc5caf24e6ccd

fix #1582 Fixing test.aria.widgets.autoselect.programmatic.AutoSelect on IE9 and IE10
This commit fixes a regression introduced by commit f9d30b69f19e3d472f6b4c93ddbbb210776507d8.
On IE9 and IE10, setting the selection on the focus event handler itself
does not work, it has to be delayed.

src/aria/widgets/form/TextInput.js
@@ -965,7 +965,12 @@ module.exports = Aria.classDefinition({
                 this.evalCallback(this._cfg.onfocus);
             }
 
-            this._autoselect();
+            // on IE9 and IE10, it is necessary to add some delay before being able to set the selection
+            ariaCoreTimer.addCallback({
+                fn : this._autoselect,
+                scope : this,
+                delay : 1
+            });
         },
 
         /**

test/aria/widgets/form/TextInputTest.js
@@ -201,9 +201,10 @@ Aria.classDefinition({
             });
         },
 
-        testAutoSelect : function () {
+        testAsyncAutoSelect : function () {
+            var self = this;
             // test autoselect is turned on
-            var tf1 = this._createTextInput({
+            var tf1 = self._createTextInput({
                 label : "TESTLABEL",
                 value : "Selected?",
                 autoselect : true
@@ -211,23 +212,40 @@ Aria.classDefinition({
 
             // autoselect on
             var instance1 = tf1.instance;
-            instance1._hasFocus = true;
-            instance1._dom_onclick();
-            instance1._dom_onfocus();
-            if (!aria.core.Browser.isOldIE) {
-                this.assertTrue(instance1._textInputField.selectionStart === 0);
-                this.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
+
+            function step0() {
+                instance1._dom_onclick();
+                instance1._dom_onfocus();
+                setTimeout(step1, 10);
             }
 
-            // autoselect off
-            instance1._textInputField.selectionEnd = 0;
-            instance1._cfg.autoselect = false;
-            instance1._dom_onclick();
-            instance1._dom_onfocus();
-            if (!aria.core.Browser.isOldIE) {
-                this.assertTrue(instance1._textInputField.selectionEnd === 0);
+            function step1() {
+                if (!aria.core.Browser.isOldIE) {
+                    self.assertTrue(instance1._textInputField.selectionStart === 0);
+                    self.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
+                }
+                instance1._textInputField.selectionEnd = 0;
+                instance1._dom_onblur();
+                setTimeout(step2, 10);
             }
-            this._destroyTextInput(instance1);
+
+            function step2() {
+                // autoselect off
+                instance1._cfg.autoselect = false;
+                instance1._dom_onclick();
+                instance1._dom_onfocus();
+                setTimeout(step3, 10);
+            }
+
+            function step3() {
+                if (!aria.core.Browser.isOldIE) {
+                    self.assertTrue(instance1._textInputField.selectionEnd === 0);
+                }
+                self._destroyTextInput(instance1);
+                self.notifyTestEnd("testAsyncAutoSelect");
+            }
+
+            step0();
         },
 
         /**

test/aria/widgets/form/TextareaTest.js
@@ -207,9 +207,10 @@ Aria.classDefinition({
             });
         },
 
-        testAutoSelect : function () {
+        testAsyncAutoSelect : function () {
+            var self = this;
             // test autoselect is turned on
-            var tf1 = this._createTextarea({
+            var tf1 = self._createTextarea({
                 label : "TESTLABEL",
                 value : "Selected?",
                 autoselect : true
@@ -217,23 +218,41 @@ Aria.classDefinition({
 
             // autoselect on
             var instance1 = tf1.instance;
-            instance1._hasFocus = true;
-            instance1._dom_onclick();
-            instance1._dom_onfocus();
-            if (!aria.core.Browser.isOldIE) {
-                this.assertTrue(instance1._textInputField.selectionStart === 0);
-                this.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
+
+            function step0() {
+                instance1._dom_onclick();
+                instance1._dom_onfocus();
+                setTimeout(step1, 10);
             }
 
-            // autoselect off
-            instance1._textInputField.selectionEnd = 0;
-            instance1._cfg.autoselect = false;
-            instance1._dom_onclick();
-            instance1._dom_onfocus();
-            if (!aria.core.Browser.isOldIE) {
-                this.assertTrue(instance1._textInputField.selectionEnd === 0);
+            function step1() {
+                if (!aria.core.Browser.isOldIE) {
+                    self.assertTrue(instance1._textInputField.selectionStart === 0);
+                    self.assertTrue(instance1._textInputField.selectionEnd === instance1._textInputField.value.length);
+                }
+
+                instance1._textInputField.selectionEnd = 0;
+                instance1._dom_onblur();
+                setTimeout(step2, 10);
             }
-            this._destroyTextarea(instance1);
+
+            function step2() {
+                // autoselect off
+                instance1._cfg.autoselect = false;
+                instance1._dom_onclick();
+                instance1._dom_onfocus();
+                setTimeout(step3, 10);
+            }
+
+            function step3() {
+                if (!aria.core.Browser.isOldIE) {
+                    self.assertTrue(instance1._textInputField.selectionEnd === 0);
+                }
+                self._destroyTextarea(instance1);
+                self.notifyTestEnd("testAsyncAutoSelect");
+            }
+
+            step0();
         },
 
         /**