ariatemplates/ariatemplates

commit f123c7a173b77fcd627f68089da702c8b57fc2bf

fix #1558 trying to close the autocomplete popup by clicking on an iframe
PTR 10584795

src/aria/widgets/form/AutoComplete.js
@@ -46,7 +46,13 @@ module.exports = Aria.classDefinition({
 
         this.$DropDownTextInput.constructor.call(this, cfg, ctxt, lineNumber, controllerInstance);
 
-        if (!cfg.expandButton) {
+        if (cfg.expandButton) {
+            this._iconsAttributes = {
+                // unselectable is necessary on IE so that, on mouse down, there is no blur of the active element
+                // (preventing the default action on mouse down does not help on IE)
+                "dropdown" : 'unselectable="on"'
+            };
+        } else {
             /**
              * Array of icon names which need to be hidden.
              * @type Array
@@ -169,6 +175,35 @@ module.exports = Aria.classDefinition({
                 this._keepFocus = false;
             }
             this.$DropDownTextInput._reactToControllerReport.call(this, report, arg);
+        },
+
+        /**
+         * DOM callback function called when the focus is taken off the input. The onBlur event is available on the
+         * input that sits inside a span. In this function we always close the popup.
+         * @param {aria.DomEvent} event Blur event
+         * @protected
+         */
+        _dom_onblur : function (event, avoidCallback) {
+            this._keepFocus = false;
+            this.$DropDownTextInput._dom_onblur.call(this, event, avoidCallback);
+            if (this._dropdownPopup) {
+                this._dropdownPopup.close(event);
+            }
+
+        },
+
+        /**
+         * Handle events raised by the frame
+         * @param {Object} evt
+         * @override
+         */
+        _frame_events : function (evt) {
+            if (evt.name == "iconMouseDown" && evt.iconName == "dropdown" && !this._cfg.disabled) {
+                evt.event.preventDefault();
+            } else if (evt.name == "iconClick" && evt.iconName == "dropdown" && !this._cfg.disabled) {
+                this._toggleDropdown();
+                evt.event.preventDefault();
+            }
         },
 
         /**

src/aria/widgets/form/DropDownListTrait.js
@@ -49,7 +49,7 @@ module.exports = Aria.classDefinition({
          */
         _clickOnItem : function (evt) {
             if (this._updateFocusNoKeyboard) {
-                this._updateFocusNoKeyboard();
+                this._updateFocusNoKeyboard(true);
             }
             this._closeDropdown();
             var report = this.controller.checkDropdownValue(evt.value);
@@ -99,6 +99,16 @@ module.exports = Aria.classDefinition({
                 var dm = this.controller.getDataModel();
                 ariaUtilsJson.setValue(dm, "selectedIdx", evt.index);
             }
+        },
+
+        /**
+         * Callback called when the user called when the user clicks the mouse in the list.
+         * @param {Object} evt object containing information about the element on which the mouse was clicked.
+         * @protected
+         */
+        _listMouseDown : function (evt) {
+            evt.target.setProperty("unselectable", "on");
+            evt.preventDefault(); // prevent the blur when clicking the popup inside the widget
         },
 
         /**
@@ -169,6 +179,10 @@ module.exports = Aria.classDefinition({
                 },
                 onclose : {
                     fn : this._closeDropdown,
+                    scope : this
+                },
+                onmousedown : {
+                    fn : this._listMouseDown,
                     scope : this
                 },
                 onchange : options.onchange,

src/aria/widgets/form/list/templates/ListTemplateScript.js
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 var Aria = require("../../../../Aria");
-var ariaCoreBrowser = require("../../../../core/Browser");
 var ariaCoreTimer = require("../../../../core/Timer");
 
 
@@ -126,11 +125,6 @@ module.exports = Aria.tplScriptDefinition({
             if (!this.data.disabled) {
                 var itemIdx = evt.target.getData("itemIdx", true);
                 if (itemIdx) {
-                    if (ariaCoreBrowser.isWebkit) {
-                        // webkit-based browsers explicitly need this
-                        // (http://www.quirksmode.org/dom/events/blurfocus.html)
-                        evt.target.focus();
-                    }
                     this.moduleCtrl.itemClick(itemIdx);
                 }
             }

test/aria/widgets/form/autocomplete/AutoCompleteTestSuite.js
@@ -56,5 +56,6 @@ Aria.classDefinition({
         this.addTests("test.aria.widgets.form.autocomplete.preselectAutofill.PreselectAutofillTestSuite");
         this.addTests("test.aria.widgets.form.autocomplete.popupWidth.AdaptToContentWidthTest");
         this.addTests("test.aria.widgets.form.autocomplete.defaultErrorMessages.DefaultErrorMessagesTest");
+        this.addTests("test.aria.widgets.form.autocomplete.iframe.AutoComplete");
     }
 });

test/aria/widgets/form/autocomplete/iframe/AutoComplete.js
@@ -1,0 +1,72 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.classDefinition({
+    $classpath : 'test.aria.widgets.form.autocomplete.iframe.AutoComplete',
+    $extends : 'aria.jsunit.RobotTestCase',
+    $constructor : function () {
+        this.$RobotTestCase.constructor.call(this);
+        this._autoCompleteTestCaseEnv = {
+            template : "test.aria.widgets.form.autocomplete.iframe.AutoCompleteTpl",
+            data : null
+        };
+        this.setTestEnv(this._autoCompleteTestCaseEnv);
+    },
+    $prototype : {
+        _helperGetPopupTable : function () {
+            var popup = this.getWidgetDropDownPopup('ac1');
+
+            if (popup) {
+                var table = popup.getElementsByTagName("table")[0];
+                if (table) {
+                    return table.parentNode;
+                }
+            }
+        },
+
+        runTemplateTest : function () {
+            this.assertEquals(typeof(this._helperGetPopupTable()), "undefined", "Dropdown for the Autocomplete is open where as it was expected to be closed");
+            this.assertFalse(!!this.getWidgetDropDownPopup("ac1"), "Dropdown for the Autocomplete is open where as it was expected to be closed");
+            var expandButton = this.getExpandButton("ac1");
+            this.synEvent.click(expandButton, {
+                fn : function () {
+                    this.waitFor({
+                        condition : function () {
+                            return (!!this._helperGetPopupTable());
+                        },
+                        callback : {
+                            fn : this._blurField,
+                            scope : this
+                        }
+                    });
+                },
+                scope : this
+            });
+        },
+
+        _blurField : function () {
+            this.assertTrue(!!this._helperGetPopupTable(), "Dropdown for the Autocomplete is closed where it was expected to be opened");
+            this.synEvent.click(this.getElementById('display'), {
+                fn : this.finishTest,
+                scope : this
+            });
+        },
+
+        finishTest : function () {
+            this.assertFalse(!!this._helperGetPopupTable(), "Dropdown for the Autocomplete is open where as it was expected to be closed after clicking on an iframe");
+            this.end();
+        }
+    }
+});

test/aria/widgets/form/autocomplete/iframe/AutoCompleteTpl.tpl
@@ -1,0 +1,39 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+{Template {
+  $classpath:"test.aria.widgets.form.autocomplete.iframe.AutoCompleteTpl",
+  $hasScript:true,
+  $width: {min: 500}
+}}
+
+{macro main()}
+  <h1>Closing the AutoComplete when clicking on an iframe</h1>
+  <iframe {id "display" /}></iframe>
+  {@aria:AutoComplete {
+      label : "Enter a country beginning by A:",
+      resourcesHandler : getHandler(),
+      expandButton : true,
+      id: "ac1"
+  }/}
+
+  <select name="select">
+    <option value="value1">Valeur 1</option>
+    <option value="value2" >Valeur 2</option>
+    <option value="value3">Valeur 3</option>
+  </select>
+{/macro}
+
+{/Template}

test/aria/widgets/form/autocomplete/iframe/AutoCompleteTplScript.js
@@ -1,0 +1,41 @@
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.tplScriptDefinition({
+    $classpath : "test.aria.widgets.form.autocomplete.iframe.AutoCompleteTplScript",
+    $dependencies : ["aria.resources.handlers.LCResourcesHandler"],
+    $constructor : function () {
+        this._lcHandler = new aria.resources.handlers.LCResourcesHandler();
+        this._lcHandler.setSuggestions([{
+                    label : "Allemagne",
+                    code : "ALL"
+                }, {
+                    label : "Albanie",
+                    code : "ALB"
+                }, {
+                    label : "Argentine",
+                    code : "ARG"
+                }]);
+    },
+    $destructor : function () {
+        this._lcHandler.$dispose();
+        this._lcHandler = null;
+    },
+    $prototype : {
+        getHandler : function () {
+            return this._lcHandler;
+        }
+    }
+});

src/aria/widgets/CfgBeans.js
@@ -1849,6 +1849,10 @@ module.exports = Aria.beanDefinitions({
                     $type : "common:Callback",
                     $description : "Function to be called when the user moves the mouse over an item in the list."
                 },
+                "onmousedown" : {
+                    $type : "common:Callback",
+                    $description : "Function to be called when the user clicks the mouse in the list."
+                },
                 "bind" : {
                     $type : "WidgetCfg.bind",
                     $properties : {

commit 95a1b2bea287cc570e23edc5c054593df42ad6e5

fix #1643 The AutoComplete's dropdown was sometimes closed too early because of unexpected blur events preceding others
The issue was introduced in commit: f123c7a173b77fcd627f68089da702c8b57fc2bf released in version 1.7.15

Under some execution environment, it happened that some blur events were fired on the AutoComplete's input element before any other event were. The initial commit was making the widget rely on blur to close the its dropdown, which in this unfortunate case prevented other events to occur on this same dropdown, that way preventing item selection from the mouse for instance.

Also added an alias for the "back_space" key to "backspace" for tests using Robot under PhantomJS.

PTR 11565758

src/aria/jsunit/RobotPhantomJS.js
@@ -81,6 +81,7 @@ module.exports = Aria.classDefinition({
         }
         keys.VK_CTRL = robotKeys.Control;
         keys.VK_BACK_SPACE = robotKeys.Backspace;
+        keys.VK_BACKSPACE = robotKeys.Backspace;
     },
     $statics : {
         MOUSEWHEEL_NOT_IMPLEMENTED : "mouseWheel is not implemented",

src/aria/widgets/form/AutoComplete.js
@@ -174,21 +174,6 @@ module.exports = Aria.classDefinition({
             this.$DropDownTextInput._reactToControllerReport.call(this, report, arg);
         },
 
-        /**
-         * DOM callback function called when the focus is taken off the input. The onBlur event is available on the
-         * input that sits inside a span. In this function we always close the popup.
-         * @param {aria.DomEvent} event Blur event
-         * @protected
-         */
-        _dom_onblur : function (event, avoidCallback) {
-            this._keepFocus = false;
-            this.$DropDownTextInput._dom_onblur.call(this, event, avoidCallback);
-            if (this._dropdownPopup) {
-                this._dropdownPopup.close(event);
-            }
-
-        },
-
         _dom_onkeydown : function (event) {
             // On Shift+F10, when AutoComplete has no expand button enabled, we don't open the dropdown, we only close it
             if (this._isShiftF10Pressed(event) && !this._cfg.expandButton && this._dropdownPopup == null) {
@@ -207,20 +192,6 @@ module.exports = Aria.classDefinition({
             this.$DropDownTextInput._dom_onkeyup.call(this, event);
             if (event.keyCode == event.KC_ENTER) {
                 this.checkValue();
-            }
-        },
-
-        /**
-         * Handle events raised by the frame
-         * @param {Object} evt
-         * @override
-         */
-        _frame_events : function (evt) {
-            if (evt.name == "iconMouseDown" && evt.iconName == "dropdown" && !this._cfg.disabled) {
-                evt.event.preventDefault();
-            } else if (evt.name == "iconClick" && evt.iconName == "dropdown" && !this._cfg.disabled) {
-                this._toggleDropdown();
-                evt.event.preventDefault();
             }
         },
 

src/aria/widgets/form/DropDownListTrait.js
@@ -102,16 +102,6 @@ module.exports = Aria.classDefinition({
         },
 
         /**
-         * Callback called when the user called when the user clicks the mouse in the list.
-         * @param {Object} evt object containing information about the element on which the mouse was clicked.
-         * @protected
-         */
-        _listMouseDown : function (evt) {
-            evt.target.setProperty("unselectable", "on");
-            evt.preventDefault(); // prevent the blur when clicking the popup inside the widget
-        },
-
-        /**
          * Callback for the keyevent on List widget. <br />
          * restore the focus on the right item if it does not have the focus, and propagate the key
          * @param {Object} evt object containing keyboard event information (charCode and keyCode). This is not an
@@ -179,10 +169,6 @@ module.exports = Aria.classDefinition({
                 },
                 onclose : {
                     fn : this._closeDropdown,
-                    scope : this
-                },
-                onmousedown : {
-                    fn : this._listMouseDown,
                     scope : this
                 },
                 onchange : options.onchange,

src/aria/widgets/form/list/templates/ListTemplateScript.js
@@ -13,6 +13,7 @@
  * limitations under the License.
  */
 var Aria = require("../../../../Aria");
+var ariaCoreBrowser = require("../../../../core/Browser");
 var ariaCoreTimer = require("../../../../core/Timer");
 
 
@@ -125,6 +126,11 @@ module.exports = Aria.tplScriptDefinition({
             if (!this.data.disabled) {
                 var itemIdx = evt.target.getData("itemIdx", true);
                 if (itemIdx) {
+                    if (ariaCoreBrowser.isWebkit) {
+                        // webkit-based browsers explicitly need this
+                        // (http://www.quirksmode.org/dom/events/blurfocus.html)
+                        evt.target.focus();
+                    }
                     this.moduleCtrl.itemClick(itemIdx);
                 }
             }

test/aria/widgets/form/autocomplete/AutoCompleteTestSuite.js
@@ -56,6 +56,5 @@ Aria.classDefinition({
         this.addTests("test.aria.widgets.form.autocomplete.preselectAutofill.PreselectAutofillTestSuite");
         this.addTests("test.aria.widgets.form.autocomplete.popupWidth.AdaptToContentWidthTest");
         this.addTests("test.aria.widgets.form.autocomplete.defaultErrorMessages.DefaultErrorMessagesTest");
-        this.addTests("test.aria.widgets.form.autocomplete.iframe.AutoComplete");
     }
 });

test/aria/widgets/form/autocomplete/iframe/AutoComplete.js
@@ -1,72 +1,0 @@
-/*
- * Copyright 2013 Amadeus s.a.s.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-Aria.classDefinition({
-    $classpath : 'test.aria.widgets.form.autocomplete.iframe.AutoComplete',
-    $extends : 'aria.jsunit.RobotTestCase',
-    $constructor : function () {
-        this.$RobotTestCase.constructor.call(this);
-        this._autoCompleteTestCaseEnv = {
-            template : "test.aria.widgets.form.autocomplete.iframe.AutoCompleteTpl",
-            data : null
-        };
-        this.setTestEnv(this._autoCompleteTestCaseEnv);
-    },
-    $prototype : {
-        _helperGetPopupTable : function () {
-            var popup = this.getWidgetDropDownPopup('ac1');
-
-            if (popup) {
-                var table = popup.getElementsByTagName("table")[0];
-                if (table) {
-                    return table.parentNode;
-                }
-            }
-        },
-
-        runTemplateTest : function () {
-            this.assertEquals(typeof(this._helperGetPopupTable()), "undefined", "Dropdown for the Autocomplete is open where as it was expected to be closed");
-            this.assertFalse(!!this.getWidgetDropDownPopup("ac1"), "Dropdown for the Autocomplete is open where as it was expected to be closed");
-            var expandButton = this.getExpandButton("ac1");
-            this.synEvent.click(expandButton, {
-                fn : function () {
-                    this.waitFor({
-                        condition : function () {
-                            return (!!this._helperGetPopupTable());
-                        },
-                        callback : {
-                            fn : this._blurField,
-                            scope : this
-                        }
-                    });
-                },
-                scope : this
-            });
-        },
-
-        _blurField : function () {
-            this.assertTrue(!!this._helperGetPopupTable(), "Dropdown for the Autocomplete is closed where it was expected to be opened");
-            this.synEvent.click(this.getElementById('display'), {
-                fn : this.finishTest,
-                scope : this
-            });
-        },
-
-        finishTest : function () {
-            this.assertFalse(!!this._helperGetPopupTable(), "Dropdown for the Autocomplete is open where as it was expected to be closed after clicking on an iframe");
-            this.end();
-        }
-    }
-});

test/aria/widgets/form/autocomplete/iframe/AutoCompleteTpl.tpl
@@ -1,39 +1,0 @@
-/*
- * Copyright 2013 Amadeus s.a.s.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-{Template {
-  $classpath:"test.aria.widgets.form.autocomplete.iframe.AutoCompleteTpl",
-  $hasScript:true,
-  $width: {min: 500}
-}}
-
-{macro main()}
-  <h1>Closing the AutoComplete when clicking on an iframe</h1>
-  <iframe {id "display" /}></iframe>
-  {@aria:AutoComplete {
-      label : "Enter a country beginning by A:",
-      resourcesHandler : getHandler(),
-      expandButton : true,
-      id: "ac1"
-  }/}
-
-  <select name="select">
-    <option value="value1">Valeur 1</option>
-    <option value="value2" >Valeur 2</option>
-    <option value="value3">Valeur 3</option>
-  </select>
-{/macro}
-
-{/Template}

test/aria/widgets/form/autocomplete/iframe/AutoCompleteTplScript.js
@@ -1,41 +1,0 @@
-/*
- * Copyright 2013 Amadeus s.a.s.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-Aria.tplScriptDefinition({
-    $classpath : "test.aria.widgets.form.autocomplete.iframe.AutoCompleteTplScript",
-    $dependencies : ["aria.resources.handlers.LCResourcesHandler"],
-    $constructor : function () {
-        this._lcHandler = new aria.resources.handlers.LCResourcesHandler();
-        this._lcHandler.setSuggestions([{
-                    label : "Allemagne",
-                    code : "ALL"
-                }, {
-                    label : "Albanie",
-                    code : "ALB"
-                }, {
-                    label : "Argentine",
-                    code : "ARG"
-                }]);
-    },
-    $destructor : function () {
-        this._lcHandler.$dispose();
-        this._lcHandler = null;
-    },
-    $prototype : {
-        getHandler : function () {
-            return this._lcHandler;
-        }
-    }
-});