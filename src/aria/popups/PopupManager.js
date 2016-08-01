ariatemplates/ariatemplates

commit 6f45f150ff92f89ca8023232c299d96611c17888

feat #1654 WAI-ARIA - Modal dialog - Focus cycling for accessible modal popups.
Now the focus is maintained inside the popup when navigating with the tab key.

This will loop through the elements of the popup, taking into account forward and backward navigation.

This will also handle properly the focus and the navigation direction when navigating from the user agent GUI.

PTR 11592232
AT-1425
AT-1442
AT-1455
close #1654

src/aria/popups/PopupManager.js
@@ -21,6 +21,7 @@ var ariaTemplatesNavigationManager = require("../templates/NavigationManager");
 var ariaUtilsAriaWindow = require("../utils/AriaWindow");
 var ariaCoreBrowser = require("../core/Browser");
 var ariaCoreTimer = require("../core/Timer");
+var ViewportNavigationInterceptor = require('./PopupNavigationManager').ViewportNavigationInterceptor;
 
 (function () {
 
@@ -112,6 +113,8 @@ var ariaCoreTimer = require("../core/Timer");
              * @type Number
              */
             this.currentZIndex = this.baseZIndex;
+
+            this._viewportNavigationInterceptor = ViewportNavigationInterceptor();
 
             ariaUtilsAriaWindow.$on({
                 "unloadWindow" : this._reset,
@@ -379,15 +382,31 @@ var ariaCoreTimer = require("../core/Timer");
                 var target = domEvent.target;
                 var self = this;
                 var popup = this.findParentPopup(target, function (popup) {
+                    var navigation;
+                    navigation = self._viewportNavigationInterceptor.getNavigationInformation(target);
+                    if (navigation == null) {
+                        navigation = popup.getNavigationInformation(target);
+                    }
+
                     var eventObject = {
                         name: "beforePreventingFocus",
                         event: domEvent,
                         modalPopup: popup,
-                        cancel: false
+                        cancel: false,
+                        navigation: navigation
                     };
                     self.$raiseEvent(eventObject);
                     if (!eventObject.cancel) {
-                        ariaTemplatesNavigationManager.focusFirst(popup.domElement);
+                        var direction;
+                        if (navigation != null) {
+                            direction = navigation.direction;
+                        }
+                        if (direction == null) {
+                            direction = 'forward';
+                        }
+
+                        var reverse = !!(direction === 'backward');
+                        ariaTemplatesNavigationManager.focusFirst(popup.domElement, reverse);
                         return popup;
                     }
                     // if the focus is allowed, don't return any popup to bring to the front
@@ -583,6 +602,7 @@ var ariaCoreTimer = require("../core/Timer");
                     popup : popup
                 });
 
+                this._viewportNavigationInterceptor.ensureElements();
             },
 
             /**
@@ -597,6 +617,7 @@ var ariaCoreTimer = require("../core/Timer");
                     if (this.modalPopups === 0) {
                         this.disconnectModalEvents();
                         this.$raiseEvent("modalPopupAbsent");
+                        this._viewportNavigationInterceptor.destroyElements();
                     }
                 }
                 if (utilsArray.isEmpty(openedPopups)) {

src/aria/popups/Popup.js
@@ -27,6 +27,7 @@ var ariaCoreTimer = require("../core/Timer");
 var ariaCoreJsonValidator = require("../core/JsonValidator");
 var ViewportPopupContainer = require("./container/Viewport");
 var environment = require("../core/environment/Environment");
+var DialogNavigationInterceptor = require('./PopupNavigationManager').DialogNavigationInterceptor;
 
 /**
  * Popup instance
@@ -75,6 +76,8 @@ module.exports = Aria.classDefinition({
         WRONG_MODALMASK_ZINDEX : "The z-index of the modal mask (%1) is higher than the z-index of the corresponding dialog (%2)."
     },
     $constructor : function () {
+        // ---------------------------------------------------------- properties
+
         /**
          * The array of positions (couples of anchors) used for positionning the popup
          * @type Array
@@ -152,6 +155,8 @@ module.exports = Aria.classDefinition({
          */
         this.ANCHOR_KEYS = [this.ANCHOR_BOTTOM, this.ANCHOR_TOP, this.ANCHOR_LEFT, this.ANCHOR_RIGHT];
 
+        // ------------------------------------------------ protected properties
+
         /**
          * A list of HTMLElements which, when clicked upon, do not cause the popup to close
          * @protected
@@ -183,12 +188,18 @@ module.exports = Aria.classDefinition({
          * @type HTMLElement
          */
         this._document = Aria.$window.document;
-        ariaPopupsPopupManager.registerPopup(this);
 
+        var self = this;
+        this._dialogNavigationInterceptor = DialogNavigationInterceptor(function() {
+            return self.domElement;
+        });
+
+        // ---------------------------------------------------------- processing
+
+        ariaPopupsPopupManager.registerPopup(this);
     },
 
     $destructor : function () {
-
         this.close();
         this.reference = null;
         if (this._delegateId) {
@@ -230,7 +241,6 @@ module.exports = Aria.classDefinition({
     },
 
     $prototype : {
-
         /**
          * Save the configuration information for this popup.
          * @param {aria.popups.Beans:PopupCfg} conf
@@ -706,18 +716,22 @@ module.exports = Aria.classDefinition({
 
             var conf = this.conf;
 
+            var waiAria = conf.waiAria;
+            var animateIn = conf.animateIn;
+
             var domElement = this.domElement;
             var modalMaskDomElement = this.modalMaskDomElement;
+            var popupContainer = this.popupContainer;
 
             // ------------------------------------------------------ processing
 
             // Ensure that the top left corner is visible
             if (modalMaskDomElement) {
                 if (this._containerOverflow == -1) {
-                    this._containerOverflow = this.popupContainer.changeContainerOverflow("hidden");
+                    this._containerOverflow = popupContainer.changeContainerOverflow("hidden");
                 }
 
-                var containerSize = this.popupContainer.getScrollSize();
+                var containerSize = popupContainer.getScrollSize();
 
                 // Compute the style after scrollbars are removed from the
                 // container. Thus the dialog can be properly centered.
@@ -742,11 +756,11 @@ module.exports = Aria.classDefinition({
                     'display:block;'
                 ].join('');
 
-                if (conf.waiAria) {
+                if (waiAria) {
                     modalMaskDomElement.setAttribute('aria-hidden', 'true');
                 }
 
-                if (conf.animateIn) {
+                if (animateIn) {
                     this._getMaskAnimator().start("fade", {
                         to : modalMaskDomElement,
                         type : 1
@@ -773,8 +787,8 @@ module.exports = Aria.classDefinition({
             domElement.style.cssText = popupPosition.concat(['z-index:', this.computedStyle.zIndex, ';',
                     'position:absolute;display:inline-block;']).join('');
 
-            if (conf.animateIn) {
-                this._startAnimation(conf.animateIn, {
+            if (animateIn) {
+                this._startAnimation(animateIn, {
                     to : domElement,
                     type : 1
                 }, false);
@@ -782,9 +796,16 @@ module.exports = Aria.classDefinition({
 
             if (ariaCoreBrowser.isIE7 && !this.isOpen) {
                 // Without the following line, the autocomplete does not initially display its content on IE7:
-                this.popupContainer.getContainerElt().appendChild(domElement);
+                popupContainer.getContainerElt().appendChild(domElement);
             }
 
+            if (modalMaskDomElement && waiAria && popupContainer.getContainerRef() === ariaUtilsDom.VIEWPORT) {
+                this._dialogNavigationInterceptor.ensureElements();
+            }
+        },
+
+        getNavigationInformation : function(focusedElement) {
+            return this._dialogNavigationInterceptor.getNavigationInformation(focusedElement);
         },
 
         /**
@@ -840,6 +861,9 @@ module.exports = Aria.classDefinition({
                 this.$raiseEvent(event);
                 if (!event.cancelClose) {
                     this._hide();
+
+                    this._dialogNavigationInterceptor.destroyElements();
+
                     this.isOpen = false;
                     // Notify the popup manager this popup was closed
                     ariaPopupsPopupManager.onPopupClose(this);

src/aria/popups/PopupNavigationManager.js
@@ -1,0 +1,375 @@
+// -----------------------------------------------------------------------------
+
+var Aria = require("../Aria");
+var ariaUtilsArray = require("../utils/Array");
+
+var document = Aria.$window.document;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Library
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Creates a DOM element that is not visible.
+ *
+ * <p>
+ *   It uses a technique that doesn't prevent the element from being focused.
+ * </p>
+ * <p>
+ *   It also respects accessibility by setting aria-hidden to true.
+ * </p>
+ *
+ * @return {HTMLElement} The created element.
+ */
+function createHiddenElement() {
+    // -------------------------------------------------------------- properties
+
+    var element = document.createElement('div');
+
+    var style = element.style;
+    style.width = 0;
+    style.height = 0;
+
+    element.setAttribute('aria-hidden', 'true');
+
+    // ------------------------------------------------------------------ return
+
+    return element;
+}
+exports.createHiddenElement = createHiddenElement;
+
+/**
+ * Inserts a DOM element before another one: the reference.
+ *
+ * <p>
+ *   This requires the reference element to have a parent node.
+ * </p>
+ *
+ * @param {HTMLElement} element The element to insert
+ * @param {HTMLElement} reference The reference element
+ *
+ * @return {HTMLElement} the given element
+ */
+function insertBefore(element, reference) {
+    reference.parentNode.insertBefore(element, reference);
+    return element;
+}
+exports.insertBefore = insertBefore;
+
+/**
+ * Inserts a DOM element after another one: the reference.
+ *
+ * <p>
+ *   This requires the reference element to have a parent node.
+ * </p>
+ *
+ * @param {HTMLElement} element The element to insert
+ * @param {HTMLElement} reference The reference element
+ *
+ * @return {HTMLElement} the given element
+ */
+function insertAfter(element, reference) {
+    reference.parentNode.insertBefore(element, reference.nextSibling);
+    return element;
+}
+exports.insertAfter = insertAfter;
+
+/**
+ * Inserts a DOM element from the rendered DOM.
+ *
+ * <p>
+ *   This requires the element to have a parent node.
+ * </p>
+ *
+ * @param {HTMLElement} element The element to remove
+ *
+ * @return {HTMLElement} the given element
+ */
+function removeElement(element) {
+    element.parentNode.removeChild(element);
+    return element;
+}
+exports.removeElement = removeElement;
+
+/**
+ * Inserts a DOM element as the first child of the given parent.
+ *
+ * @param {HTMLElement} element The element to insert
+ * @param {HTMLElement} parent The parent element
+ *
+ * @return {HTMLElement} the given element
+ */
+function insertFirst(element, parent) {
+    insertBefore(element, parent.childNodes[0]);
+    return element;
+}
+exports.insertFirst = insertFirst;
+
+/**
+ * Inserts a DOM element as the last child of the given parent.
+ *
+ * @param {HTMLElement} element The element to insert
+ * @param {HTMLElement} parent The parent element
+ *
+ * @return {HTMLElement} the given element
+ */
+function insertLast(element, parent) {
+    parent.appendChild(element);
+    return element;
+}
+exports.insertLast = insertLast;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Local library
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Creates a hidden DOM element that can be focused through tab navigation.
+ *
+ * <p>
+ *   The goal of this element, when inserted at a proper place, is to intercept the navigation, in order to infer various information: direction, origin, etc. One important thing is that it won't alter the position of the scroll when being focused.
+ * </p>
+ *
+ * @return {HTMLElement} The created element.
+ */
+function createInterceptorElement() {
+    // -------------------------------------------------------------- processing
+
+    var element = createHiddenElement();
+
+    element.style.position = 'fixed';
+
+    element.tabIndex = 0;
+
+    // ------------------------------------------------------------------ return
+
+    return element;
+}
+exports.createInterceptorElement = createInterceptorElement;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A navigation interception manager.
+ *
+ * <p>
+ *   This manages a set of elements to be inserted relatively to a reference element. It can ensure those elements are created and properly inserted, and when so, it can check if an element corresponds to one of its interceptors. The manager has an origin, and the direction is inferred from the interceptor.
+ * </p>
+ *
+ * <p>
+ *   Interceptors are defined with these properties:
+ *   <ul>
+ *     <li>direction: the name of the direction of the tab navigation when intercepted by this manager. Usually "backward" or "forward", but you can choose any convention.</li>
+ *     <li>insert: a callback to insert the element. It will be passed the following arguments: the element to insert, the reference element.</li>
+ *   </ul>
+ *   During execution, this object will have a new property when elements are actually created: element. This holds the created HTMLElement.
+ * </p>
+ *
+ * @param {Object} spec An object:
+ * <ul>
+ *   <li>origin: the name of the origin of the tab navigation when intercepted by this manager (can be the reference element, but any other depending on the use cases)</li>
+ *   <li>getReferenceElement: a callback to get the reference element</li>
+ *   <li>interceptors: a list of interceptors (see detailed description)</li>
+ * </ul>
+ */
+function NavigationInterceptorClass(spec) {
+    this.origin = spec.origin;
+    this.getReferenceElement = spec.getReferenceElement;
+    this.interceptors = spec.interceptors;
+}
+exports.NavigationInterceptorClass = NavigationInterceptorClass;
+
+/**
+ * A factory to instantiate the class NavigationInterceptorClass. All parameters are forwarded.
+ *
+ * @return {NavigationInterceptorClass} an instance of NavigationInterceptorClass.
+ */
+function NavigationInterceptor(spec) {
+    return new NavigationInterceptorClass(spec);
+}
+exports.NavigationInterceptor = NavigationInterceptor;
+
+var prototype = NavigationInterceptorClass.prototype;
+
+/**
+ * Ensures interceptor element are actually created, and inserted at the right position in the rendered DOM.
+ *
+ * <p>
+ *   Created elements are put under property "element" inside the interceptor spec object. This is how their existence is checked.
+ * </p>
+ */
+prototype.ensureElements = function () {
+    // ----------------------------------------------------------- destructuring
+
+    var getReferenceElement = this.getReferenceElement;
+
+    // -------------------------------------------------------------- processing
+
+    var referenceElement = getReferenceElement();
+
+    ariaUtilsArray.forEach(this.interceptors, function (interceptor) {
+        var insert = interceptor.insert;
+        var element = interceptor.element;
+
+        if (element == null) {
+            element = createInterceptorElement();
+            interceptor.element = element;
+        }
+
+        insert(element, referenceElement);
+    }, this);
+};
+
+/**
+ * Removes and forgets about the interceptor elements, if ever they had been created.
+ *
+ * <p>
+ *   This sets to "null" the "element" property inside the interceptor spec object.
+ * </p>
+ */
+prototype.destroyElements = function () {
+    ariaUtilsArray.forEach(this.interceptors, function (interceptor) {
+        var element = interceptor.element;
+
+        if (element != null) {
+            removeElement(element);
+            interceptor.element = null;
+        }
+    }, this);
+};
+
+/**
+ * Returns information about the navigation that can be inferred from the knowledge of the focused element.
+ *
+ * <p>
+ *   It will compare the given element with the interceptor objects to infer a "direction" of navigation (the one specified in the interceptor spec).
+ * </p>
+ * <p>
+ *   It will add the "origin" piece of information, as specified when instantiating the class.
+ * </p>
+ * <p>
+ *   If the element doesn't match any interceptor, no information will be returned.
+ * </p>
+ *
+ * @param {HTMLElement} focusedElement The element that has focus (or any other if you want), with which interceptors will be compared
+ *
+ * @return {Object} Information navigation in the form of {origin, direction} or "null".
+ */
+prototype.getNavigationInformation = function(focusedElement) {
+    // ----------------------------------------------------------- destructuring
+
+    var interceptors = this.interceptors;
+    var origin = this.origin;
+
+    // -------------------------------------------------------------- processing
+
+    // direction ---------------------------------------------------------------
+
+    var direction = null;
+
+    for (var index = 0, length = interceptors.length; index < length; index++) {
+        var interceptor = interceptors[index];
+
+        if (focusedElement === interceptor.element) {
+            direction = interceptor.direction;
+            break;
+        }
+    }
+
+    // result ------------------------------------------------------------------
+
+    var result;
+
+    if (direction == null) {
+        result = null;
+    } else {
+        result = {
+            direction: direction,
+            origin: origin
+        };
+    }
+
+    // ------------------------------------------------------------------ return
+
+    return result;
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Creates a NavigationInterceptor for a Popup.
+ *
+ * <p>
+ *   Interceptor elements will be inserted right before and right after the Popup element, corresponding respectively to a navigation direction "backward" or "forward".
+ * </p>
+ *
+ * @param {Function} getReferenceElement A callback to get the Popup element
+ *
+ * @return {NavigationInterceptorClass} an instance of NavigationInterceptorClass
+ */
+function DialogNavigationInterceptor(getReferenceElement) {
+    return NavigationInterceptor({
+        origin: 'popup',
+
+        getReferenceElement: getReferenceElement,
+
+        interceptors: [
+            // before
+            {
+                direction: 'backward',
+                insert: insertBefore
+            },
+            // after
+            {
+                direction: 'forward',
+                insert: insertAfter
+            }
+        ]
+    });
+}
+exports.DialogNavigationInterceptor = DialogNavigationInterceptor;
+
+/**
+ * Creates a NavigationInterceptor for the browser/viewport.
+ *
+ * <p>
+ *   Interceptor elements will be inserted at the very beginning and at the very end of the viewport ("document.body"), corresponding respectively to a navigation direction "forward" or "backward".
+ * </p>
+ *
+ * @return {NavigationInterceptorClass} an instance of NavigationInterceptorClass
+ */
+function ViewportNavigationInterceptor() {
+    return NavigationInterceptor({
+        origin: 'browser',
+
+        getReferenceElement: function () {
+            return document.body;
+        },
+
+        interceptors: [
+            // first
+            {
+                direction: 'forward',
+                insert: insertFirst
+            },
+            // last
+            {
+                direction: 'backward',
+                insert: insertLast
+            }
+        ]
+    });
+}
+exports.ViewportNavigationInterceptor = ViewportNavigationInterceptor;

src/aria/templates/NavigationManager.js
@@ -222,34 +222,72 @@ module.exports = Aria.classDefinition({
                 return false;
             }
             // case existing widget
-            if (target.__widget) {
-                if (target.__widget.focus) {
-                    var focusSuccess = target.__widget.focus(null, cb);
-                    // PTR 04634723 - the focus method of the template widget returns a boolean value that represents
-                    // the success/failure of the focus.
-                    // Note 30.10.2012: I refactored from return (focusSuccess==false) ? false:true
-                    // It seems that false is returned from 'focus' method in case of failure, and undefined (*not*
-                    // true, as stated above!) otherwise. So logically, seems there should be === false comparison here,
-                    // but the code seems to be working fine...
-                    return !!focusSuccess;
-                } else {
-                    return false;
-                }
+            var widget = target.__widget;
+            if (widget && widget.focus) {
+                var focusSuccess = widget.focus(null, cb);
+                // PTR 04634723 - the focus method of the template widget returns a boolean value that represents
+                // the success/failure of the focus.
+                // Note 30.10.2012: I refactored from return (focusSuccess==false) ? false:true
+                // It seems that false is returned from 'focus' method in case of failure, and undefined (*not*
+                // true, as stated above!) otherwise. So logically, seems there should be === false comparison here,
+                // but the code seems to be working fine...
+                return !!focusSuccess;
             } else {
-                var nodeName = target.nodeName, tabIndex = target.getAttributeNode("tabindex");
-                if ((nodeName == "INPUT" || nodeName == "TEXTAREA" || nodeName == "A" || nodeName == "BUTTON" || (tabIndex
-                        && tabIndex.specified && tabIndex.nodeValue != -1))
-                        && !target.disabled) {
-                    try {
-                        target.focus();
-                        return true;
-                    } catch (e) {
-                        // this happens in IE if the element is disable or hidden
-                        return false;
-                    }
+                return this.focusDomElement(target);
+            }
+        },
+
+        focusDomElement : function (domElement) {
+            // ------------------------------------------------------ processing
+
+            var focused = false;
+
+            if (this.canBeFocused(domElement)) {
+                try {
+                    domElement.focus();
+                    focused = true;
+                } catch (exception) {
+                    // this happens in IE if the element is disable or hidden
                 }
             }
-            return false;
+
+            // ---------------------------------------------------------- return
+
+            return focused;
+        },
+
+        canBeFocused : function (domElement) {
+            // --------------------------------------------------- destructuring
+
+            var nodeName = domElement.nodeName;
+            var tabIndex = domElement.getAttributeNode('tabindex');
+            var disabled = domElement.disabled;
+
+            // ------------------------------------------------------ processing
+
+            var result = false;
+
+            // TODO Check visibility
+            if (!disabled) {
+                // TODO Add "AREA" (with checking of "href")
+                if (nodeName == 'INPUT') {
+                    result = true;
+                } else if (nodeName == 'TEXTAREA') {
+                    result = true;
+                } else if (nodeName == 'A') {
+                    // FIXME Check "href"
+                    result = true;
+                } else if (nodeName == 'BUTTON') {
+                    result = true;
+                } else if (tabIndex != null && tabIndex.specified && tabIndex.nodeValue != -1) {
+                    // XXX Check that it's the correct way to test the tabIndex (getAttributeNode vs tabIndex property, using !=-1 instead of >=0)
+                    result = true;
+                }
+            }
+
+            // ---------------------------------------------------------- return
+
+            return result;
         },
 
         // TABLE NAVIGATION

src/aria/widgets/Icon.js
@@ -17,6 +17,7 @@ var ariaUtilsString = require("../utils/String");
 var ariaWidgetsIconStyle = require("./IconStyle.tpl.css");
 var ariaWidgetsWidget = require("./Widget");
 var ariaCoreTplClassLoader = require("../core/TplClassLoader");
+var ariaTemplatesNavigationManager = require('../templates/NavigationManager');
 
 /**
  * Aria Icon Widget
@@ -164,6 +165,11 @@ module.exports = Aria.classDefinition({
             // ---------------------------------------------------------- output
 
             out.write(markup);
+        },
+
+        focus : function () {
+            var element = this.getDom();
+            return ariaTemplatesNavigationManager.focusDomElement(element);
         },
 
         /**

test/EnhancedRobotTestCase.js
@@ -77,6 +77,13 @@ var prototype = {
         this._log.apply(this, args);
     },
 
+    _debugger : function (next) {
+        /* jshint debug: true */
+        debugger;
+        next();
+    },
+
+
 
     ////////////////////////////////////////////////////////////////////////////
     // DOM

commit 3c01e0842afb0685fde0abaf8e3166e69964e399

fix focus issue on IE 7 caused by navigation elements introduced in #1654
The new navigation elements introduced in commit 6f45f150ff92f89ca8023232c299d96611c17888 (#1654)
make the following tests fail on IE 7:
 - test.aria.widgets.wai.popup.dialog.modal.FirstTest
 - test.aria.widgets.wai.popup.dialog.modal.FourthTest

Those tests are blocked in _testFocusRestoration while waiting for the
opening element to be focused again. It is not obvious why adding the
navigation elements prevents the opening element from being correctly
focused again when the dialog is closed. It must be just another IE 7 bug!

The issue seems not to happen if we do not remove the navigation elements
from the DOM when the dialog is closed. However, instead of keeping those
elements in the DOM a little longer (with a non-deterministic setTimeout),
this commit simply avoids adding those elements on IE 7 which results in
the following slightly behavior on IE 7: when pressing shift-tab from
outside the viewport when a modal dialog is open, the first item of the
dialog gets focused instead of the last one.

src/aria/popups/PopupManager.js
@@ -602,7 +602,18 @@ var ViewportNavigationInterceptor = require('./PopupNavigationManager').Viewport
                     popup : popup
                 });
 
-                this._viewportNavigationInterceptor.ensureElements();
+                if (!ariaCoreBrowser.isIE7) {
+                    // The navigation elements inserted by ensureElements make the following tests fail on IE 7:
+                    // - test.aria.widgets.wai.popup.dialog.modal.FirstTest
+                    // - test.aria.widgets.wai.popup.dialog.modal.FourthTest
+                    // Those tests are blocked in _testFocusRestoration while waiting for the opening element to
+                    // be focused again. It is not obvious why adding the navigation elements prevents the opening
+                    // element from being correctly focused again when the dialog is closed... just an IE 7 bug!
+                    // The issue seems not to happen if we do not remove the navigation elements from the DOM when
+                    // the dialog is closed. However, instead of keeping those elements in the DOM a little longer
+                    // (with a non-deterministic setTimeout), we simply avoid adding those elements on IE 7:
+                    this._viewportNavigationInterceptor.ensureElements();
+                }
             },
 
             /**