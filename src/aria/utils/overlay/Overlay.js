ariatemplates/ariatemplates

commit 45f686eaa6e018b6eaa4b91db74a774b20b65028

fix #1769 New algorithm to compute minimal (or no) z-index value for loading overlays
The lesser the better for interoperability with other's code.

AT-1428
PTR 11674386

src/aria/utils/overlay/Overlay.js
@@ -146,29 +146,117 @@ module.exports = Aria.classDefinition({
 
         /**
          * Apply a zIndex to the overlay
+         *
+         * <p>Resources: </p>
+         * <ul>
+         *   <li><a href="http://www.cssmojo.com/everything_you_always_wanted_to_know_about_z-index_but_were_afraid_to_ask/">Find out how elements stack and start using low z-index values</a></li>
+         *   <li><a href="https://philipwalton.com/articles/what-no-one-told-you-about-z-index/">What No One Told You About Z-Index — Philip Walton</a></li>
+         * </ul>
          * @param {HTMLElement} element Reference zIndex
          * @param {HTMLElement} overlay DOM element of the overlay
          * @protected
          */
         _computeZIndex : function (element, overlay) {
-            var document = Aria.$window.document;
-            var zIndex = 20000, stopper = document.body;
+            // --------------------------------------------------- destructuring
 
-            // inspect parent z-Indexes
-            while (element && element != stopper) {
-                var style = element.style, elementZIndex = style.zIndex;
-                if (elementZIndex) {
-                    var intZIndex = parseInt(elementZIndex, 10);
-                    if (intZIndex > zIndex) {
-                        zIndex = intZIndex + 1;
-                    }
-                }
-                element = element.parentNode;
+            var window = Aria.$window;
+            var document = window.document;
+            var body = document.body;
+
+            // ------------------------------------------------- local functions
+
+            function getStyle(element, property) {
+                return ariaUtilsDom.getStyle(element, property);
             }
 
-            // set zindex to maximum value from parents
-            overlay.style.zIndex = "" + zIndex;
+            function getZIndex(element) {
+                return getStyle(element, 'zIndex');
+            }
 
+            function createsANewStackingContext(element) {
+                var zIndex = getZIndex(element);
+                var position = getStyle(element, 'position');
+                var opacity = getStyle(element, 'opacity');
+
+                return (opacity < 1) || ((zIndex !== 'auto') && (position !== 'static'));
+            }
+
+            // ------------------------------------------------------ processing
+
+            var zIndexToBeat = null;
+
+            // building parent branch ------------------------------------------
+
+            var ancestors = [];
+            var currentElement = element;
+            while (currentElement !== body && currentElement != null) {
+                ancestors.unshift(currentElement);
+                currentElement = currentElement.parentNode;
+            }
+
+            // checking parent branch ------------------------------------------
+            // only the first (top-most) parent has to be taken into account, further children then being contained in its new stacking context
+
+            for (var index = 0, length = ancestors.length; index < length; index++) {
+                var potentialStackingContextRoot = ancestors[index];
+
+                if (createsANewStackingContext(potentialStackingContextRoot)) {
+                    zIndexToBeat = getZIndex(potentialStackingContextRoot);
+                    break;
+                }
+            }
+
+            // checking children if necessary ----------------------------------
+
+            // NB 2017-07-10T12:12:54+02:00
+            // This section has been commented since I (https://github.com/ymeine) wrote the algorithm to also check all descendants of the covered element. However, the feature itself can be discussed: it can sometimes be the desired behavior, sometimes not. So we had to choose, and considering the higher complexity of this algorithm compared to the one above (in the former we are potentially analyzing ALL the descendants instead of a single upper portion of a branch in the latter) we decided not to tun this feature. However, in case it's wanted one day, I prefer leaving the algorithm written here at least for now.
+
+            // if (zIndexToBeat == null) {
+            //     var getMax = function(values) {
+            //         return values.length === 0 ? null : Math.max.apply(Math, values);
+            //     }
+
+            //     var isANumber = function(value) {
+            //         return !isNaN(value);
+            //     }
+
+            //     var stackingContexts = [];
+
+            //     var findChildrenCreatingANewStackingContext = function(root) {
+            //         var children = root.children;
+
+            //         for (var index = 0, length = children.length; index < length; index++) {
+            //             var child = children[index];
+
+            //             if (createsANewStackingContext(child)) {
+            //                 stackingContexts.push(child);
+            //             } else {
+            //                 findChildrenCreatingANewStackingContext(child);
+            //             }
+            //         }
+            //     };
+
+            //     findChildrenCreatingANewStackingContext(element);
+
+            //     var zIndexes = [];
+            //     for (var index = 0, length = stackingContexts.length; index < length; index++) {
+            //         var stackingContext = stackingContexts[index];
+
+            //         var currentZIndex = getZIndex(stackingContext);
+            //         if (isANumber(currentZIndex)) {
+            //             zIndexes.push(currentZIndex);
+            //         }
+            //     }
+            //     zIndexToBeat = getMax(zIndexes);
+            // }
+
+            // final zindex application ----------------------------------------
+            // Our overlay element is put at last in the DOM, so if there is no other stacking context found, there is no need to put a zIndex ourselves since the natural stacking order will apply.
+            // However, still due to the natural stacking order applied within a same stacking context, there's no need to put a higher zIndex than the top-most stacking context's root, putting the same is sufficient
+
+            if (zIndexToBeat !== null) {
+                overlay.style.zIndex = '' + zIndexToBeat;
+            }
         },
 
         /**

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/newStackingContext/asDescendant/IndexTestCase.js
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2017 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Aria = require('ariatemplates/Aria');
+
+var Base = require('../../ZIndexNewAlgorithmTestBase');
+
+module.exports = Aria.classDefinition({
+    $classpath: 'test.aria.utils.overlay.loadingIndicator.zindex.newAlgorithm.newStackingContext.asDescendant.IndexTestCase',
+    $extends: Base,
+
+    $prototype : {
+        generateTestData: function () {
+            return {
+                expectedZIndex: null,
+                overlayShouldBeOnTop: false
+            };
+        }
+    }
+});

test/aria/utils/overlay/loadingIndicator/zindex/IndexTestCase.js
@@ -16,9 +16,18 @@
 Aria.classDefinition({
     $classpath : "test.aria.utils.overlay.loadingIndicator.zindex.IndexTestCase",
     $extends : "aria.jsunit.TemplateTestCase",
+
+    $constructor: function () {
+        this.$TemplateTestCase.constructor.apply(this, arguments);
+
+        this.setTestEnv({
+            iframe : true
+        });
+    },
+
     $prototype : {
         runTemplateTest : function () {
-            var document = Aria.$window.document;
+            var document = this.testDocument;
             // overlay is last element inserted in DOM
             var bodyChilds = document.body.childNodes;
             var overlay = bodyChilds[bodyChilds.length - 1];
@@ -26,7 +35,8 @@ Aria.classDefinition({
             if (overlay.nodeType != 1) {
                 overlay = overlay.previousSibling;
             }
-            this.assertTrue(parseInt(overlay.style.zIndex, 10) > 40000, "overlay zIndex makes it non visible");
+            overlay.style.backgroundColor = '#000'; // to make it visible for manual testing
+            this.assertTrue(parseInt(overlay.style.zIndex, 10) >= 40000, "overlay zIndex is not high enough and thus not visible, should be more than 40000 but has " + overlay.style.zIndex + " instead");
 
             this.notifyTemplateTestEnd();
         }

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/newStackingContext/asNestedParent/IndexTestCase.js
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2017 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Aria = require('ariatemplates/Aria');
+
+var Base = require('../../ZIndexNewAlgorithmTestBase');
+
+module.exports = Aria.classDefinition({
+    $classpath: 'test.aria.utils.overlay.loadingIndicator.zindex.newAlgorithm.newStackingContext.asNestedParent.IndexTestCase',
+    $extends: Base,
+
+    $prototype : {
+        generateTestData: function () {
+            return {
+                expectedZIndex: 10000
+            };
+        }
+    }
+});

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/newStackingContext/opacity/IndexTestCase.js
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2017 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Aria = require('ariatemplates/Aria');
+var Browser = require('ariatemplates/core/Browser');
+
+var Base = require('../../ZIndexNewAlgorithmTestBase');
+
+module.exports = Aria.classDefinition({
+    $classpath: 'test.aria.utils.overlay.loadingIndicator.zindex.newAlgorithm.newStackingContext.opacity.IndexTestCase',
+    $extends: Base,
+
+    $prototype : {
+        generateTestData: function () {
+            return {
+                expectedZIndex: Browser.isIE8 ? 10000 : null // "opacity" CSS property is not supported in IE8 (it uses an alternative instead)
+            };
+        }
+    }
+});

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/noStackingContext/bare/IndexTestCase.js
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2017 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Aria = require('ariatemplates/Aria');
+
+var Base = require('../../ZIndexNewAlgorithmTestBase');
+
+module.exports = Aria.classDefinition({
+    $classpath: 'test.aria.utils.overlay.loadingIndicator.zindex.newAlgorithm.noStackingContext.bare.IndexTestCase',
+    $extends: Base,
+
+    $prototype : {
+        generateTestData: function () {
+            return {
+                expectedZIndex: null
+            };
+        }
+    }
+});

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/noStackingContext/zIndexOnly/IndexTestCase.js
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2017 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Aria = require('ariatemplates/Aria');
+
+var Base = require('../../ZIndexNewAlgorithmTestBase');
+
+module.exports = Aria.classDefinition({
+    $classpath: 'test.aria.utils.overlay.loadingIndicator.zindex.newAlgorithm.noStackingContext.zIndexOnly.IndexTestCase',
+    $extends: Base,
+
+    $prototype : {
+        generateTestData: function () {
+            return {
+                expectedZIndex: null
+            };
+        }
+    }
+});

test/aria/templates/section/processingIndicator/ProcessingIndicatorOnSectionTestCase.js
@@ -106,7 +106,7 @@ Aria.classDefinition({
             var overlayZIndex = this.computeZIndex(overlay);
             this.assertJsonEquals(sectionGeo, overlayGeo, "The overlay was not positioned correctly on section "
                     + sectionId + ".");
-            this.assertTrue(overlayZIndex > sectionZIndex, "The overlay is behind the corresponding element.");
+            this.assertTrue(overlayZIndex >= sectionZIndex, "The overlay is behind the corresponding element.");
 
         }
 

commit 5bebc1478658530d477d862e0cf630bc12b25a60

fix #1785 overlay z-index computation (now takes child elements into account)
This commit fixes a regression caused by commit 45f686e (#1769), by
uncommenting the code to take into account child elements when computing
the z-index to apply on an overlay for an element.

PTR 14158720
AT-1807

src/aria/utils/overlay/Overlay.js
@@ -208,47 +208,44 @@ module.exports = Aria.classDefinition({
 
             // checking children if necessary ----------------------------------
 
-            // NB 2017-07-10T12:12:54+02:00
-            // This section has been commented since I (https://github.com/ymeine) wrote the algorithm to also check all descendants of the covered element. However, the feature itself can be discussed: it can sometimes be the desired behavior, sometimes not. So we had to choose, and considering the higher complexity of this algorithm compared to the one above (in the former we are potentially analyzing ALL the descendants instead of a single upper portion of a branch in the latter) we decided not to tun this feature. However, in case it's wanted one day, I prefer leaving the algorithm written here at least for now.
+            if (zIndexToBeat == null) {
+                var getMax = function(values) {
+                    return values.length === 0 ? null : Math.max.apply(Math, values);
+                };
 
-            // if (zIndexToBeat == null) {
-            //     var getMax = function(values) {
-            //         return values.length === 0 ? null : Math.max.apply(Math, values);
-            //     }
+                var isANumber = function(value) {
+                    return !isNaN(value);
+                };
 
-            //     var isANumber = function(value) {
-            //         return !isNaN(value);
-            //     }
+                var stackingContexts = [];
 
-            //     var stackingContexts = [];
+                var findChildrenCreatingANewStackingContext = function(root) {
+                    var children = root.children;
 
-            //     var findChildrenCreatingANewStackingContext = function(root) {
-            //         var children = root.children;
+                    for (var index = 0, length = children.length; index < length; index++) {
+                        var child = children[index];
 
-            //         for (var index = 0, length = children.length; index < length; index++) {
-            //             var child = children[index];
+                        if (createsANewStackingContext(child)) {
+                            stackingContexts.push(child);
+                        } else {
+                            findChildrenCreatingANewStackingContext(child);
+                        }
+                    }
+                };
 
-            //             if (createsANewStackingContext(child)) {
-            //                 stackingContexts.push(child);
-            //             } else {
-            //                 findChildrenCreatingANewStackingContext(child);
-            //             }
-            //         }
-            //     };
+                findChildrenCreatingANewStackingContext(element);
 
-            //     findChildrenCreatingANewStackingContext(element);
+                var zIndexes = [];
+                for (var index = 0, length = stackingContexts.length; index < length; index++) {
+                    var stackingContext = stackingContexts[index];
 
-            //     var zIndexes = [];
-            //     for (var index = 0, length = stackingContexts.length; index < length; index++) {
-            //         var stackingContext = stackingContexts[index];
-
-            //         var currentZIndex = getZIndex(stackingContext);
-            //         if (isANumber(currentZIndex)) {
-            //             zIndexes.push(currentZIndex);
-            //         }
-            //     }
-            //     zIndexToBeat = getMax(zIndexes);
-            // }
+                    var currentZIndex = getZIndex(stackingContext);
+                    if (isANumber(currentZIndex)) {
+                        zIndexes.push(currentZIndex);
+                    }
+                }
+                zIndexToBeat = getMax(zIndexes);
+            }
 
             // final zindex application ----------------------------------------
             // Our overlay element is put at last in the DOM, so if there is no other stacking context found, there is no need to put a zIndex ourselves since the natural stacking order will apply.

test/aria/utils/overlay/loadingIndicator/zindex/newAlgorithm/newStackingContext/asDescendant/IndexTestCase.js
@@ -24,8 +24,8 @@ module.exports = Aria.classDefinition({
     $prototype : {
         generateTestData: function () {
             return {
-                expectedZIndex: null,
-                overlayShouldBeOnTop: false
+                expectedZIndex: 10000,
+                overlayShouldBeOnTop: true
             };
         }
     }