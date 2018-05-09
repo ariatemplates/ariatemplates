2f8fcfd9595b54bb0a674b9975c49ae21b1da5c4
divdavem <david-emmanuel.divernois@amadeus.com>
2018-05-09 11:22:52 +0200

    fix #1797 JAWS reads dialog title when closing dropdown widget
    This commit contains a workaround for an issue with the JAWS screen reader:
    JAWS reads the dialog title when closing a dropdown widget that is inside
    the dialog, whereas it should not (as the user did not leave the dialog).
    
    As investigated by @ymeine:
    
    > - the issue comes from the fact the dropdown's element is physically
    > outside the dialog, the focus is moved to it, and when closing the
    > dropdown the focus is put back to the dialog, making JAWS announce it
    > again
    > - we can't put the dropdown's element inside the dialog because of other
    > limitations (possible overflow styling on parent elements)
    > - the right way to do is to use the WAI-ARIA attribute aria-owns to
    > restore the logical structure of elements but JAWS doesn't support it
    > - the workaround is to remove the role=dialog attribute on the Dialog's
    > root element as long as a dropdown logically located inside it is open,
    > and restore it short after closing this dropdown
    
    The workaround implemented in this commit unfortunately only delays the
    issue because when the user moves the focus to another item inside the
    dialog, JAWS will detect the role=dialog attribute that was restored on
    the dialog and read the dialog title again!
    
    PTR 14068536

---
+++ test/aria/widgets/wai/dropdown/dialogTitle/DropDownDialogTitleJawsTestCase.js
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2018 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.classDefinition({
+    $classpath : "test.aria.widgets.wai.dropdown.dialogTitle.DropDownDialogTitleJawsTestCase",
+    $extends : "aria.jsunit.JawsTestCase",
+    $dependencies : ["aria.utils.Json"],
+    $constructor : function () {
+        this.$JawsTestCase.constructor.call(this);
+        this.data = {};
+        this.setTestEnv({
+            template : "test.aria.widgets.wai.dropdown.dialogTitle.Tpl",
+            data: this.data
+        });
+    },
+    $prototype : {
+        runTemplateTest : function () {
+            aria.utils.Json.setValue(this.data, "dialogVisible", true);
+            this.waitFor({
+                condition : function () {
+                    return !!this.getElementById("firstItem");
+                },
+                callback : this.afterDialogDisplayed
+            });
+        },
+
+        afterDialogDisplayed : function () {
+            this.noiseRegExps.push(/type|thisisadate/i, /^MyDialogTitle Edit$/);
+            var actions = [
+                ["click", this.getElementById("firstItem")], ["pause", 500],
+                ["type", null, "[tab]"], ["pause", 500],
+                
+                // DatePicker:
+                ["type", null, "[down]"], ["pause", 100],
+                ["type", null, "[space]"], ["pause", 2000],
+                ["type", null, "[down]"], ["pause", 1000],
+                ["type", null, "[enter]"], ["pause", 2000],
+                ["type", null, "[tab]"], ["pause", 100],
+                ["type", null, "[tab]"], ["pause", 100],
+
+                // AutoComplete:
+                ["type", null, "d"], ["pause", 100],
+                ["type", null, "[down]"], ["pause", 1000],
+                ["type", null, "[enter]"], ["pause", 2000],
+                ["type", null, "[tab]"], ["pause", 100],
+                ["type", null, "[tab]"], ["pause", 100],
+
+                // MultiSelect:
+                ["type", null, "[down]"], ["pause", 100],
+                ["type", null, "[space]"], ["pause", 2000],
+                ["type", null, "[down]"], ["pause", 1000],
+                ["type", null, "[space]"], ["pause", 2000],
+                ["type", null, "[escape]"], ["pause", 2000],
+                ["type", null, "[tab]"], ["pause", 100],
+                ["type", null, "[tab]"], ["pause", 100],
+
+                // closes the dialog:
+                ["type", null, "[escape]"], ["pause", 1000]
+            ];
+            this.synEvent.execute(actions, {
+                fn: function () {
+                    this.assertJawsHistoryEquals([
+                        "MyDialogTitle dialog",
+                        "FirstFieldLabel Edit",
+                        "DatePickerLabel Edit",
+                        "DropDownLabelForDatePicker",
+                        "Calendar table. Use arrow keys to navigate and space to validate.",
+                        "DatePickerLabel Edit",
+                        "MyDialogTitle dialog", // must be after "DatePickerLabel Edit"
+                        "DropDownLabelForDatePicker",
+                        "AutoCompleteLabel Edit",
+                        "List view Desktop device",
+                        "AutoCompleteLabel Edit",
+                        "Desktop device",
+                        "MyDialogTitle dialog", // must be after "AutoCompleteLabel Edit"
+                        "DropDownLabelForAutoComplete",
+                        "MultiSelectLabel Edit",
+                        "DropDownLabelForMultiSelect",
+                        "Touch device check box not checked",
+                        "Desktop device check box not checked",
+                        "Desktop device check box checked",
+                        "MultiSelectLabel Edit",
+                        "Desktop device",
+                        "MyDialogTitle dialog", // must be after "MultiSelectLabel Edit"
+                        "DropDownLabelForMultiSelect",
+                        "LastFieldLabel Edit"
+                    ].join("\n"), this.end, function (response) {
+                        return response.replace(/\n(check box)/g, " $1").replace(/not checked\n(checked)/g, "$1");
+                    });
+                },
+                scope: this
+            });
+        }
+    }
+});
---
+++ test/aria/widgets/wai/dropdown/dialogTitle/TplScript.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Aria.tplScriptDefinition({
+    $classpath : "test.aria.widgets.wai.dropdown.dialogTitle.TplScript",
+    $dependencies : ["aria.resources.handlers.LCResourcesHandler"],
+    $constructor : function () {
+        this.items = [{
+                    label : "Touch device",
+                    code : "Touch device",
+                    value : "Touch device"
+                }, {
+                    label : "Desktop device",
+                    code : "Desktop device",
+                    value : "Desktop device"
+                }];
+    },
+    $destructor : function () {
+        if (this.acHandler) {
+            this.acHandler.$dispose();
+            this.acHandler = null;
+        }
+    },
+    $prototype : {
+        openDialog : function () {
+            this.$json.setValue(this.data, "dialogVisible", true);
+        },
+
+        getAutoCompleteHandler : function () {
+            if (!this.acHandler) {
+                var acHandler = new aria.resources.handlers.LCResourcesHandler();
+                acHandler.setSuggestions(this.items);
+                this.acHandler = acHandler;
+            }
+            return this.acHandler;
+        }
+    }
+});
