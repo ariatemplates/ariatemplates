a94037e5c8a80b7b1aa4ec5d35b1615b2b40ced5
divdavem <david-emmanuel.divernois@amadeus.com>
2019-01-16 14:48:26 +0100

    feat #1807 Adding autocomplete and name properties to text input widgets
    PTR 16220515
    
    (cherry picked from commit 111c3c1fc65376be88dc5e76cd200c0167d76350)

--- src/aria/widgets/form/TextInput.js
+++ src/aria/widgets/form/TextInput.js
@@ -305,6 +305,14 @@ Aria.classDefinition({
                 // corresponding attribute in the HTML
                 spellCheck = ' spellcheck="' + (cfg.spellCheck ? "true" : "false") + '"';
             }
+            var autocomplete = "";
+            if (cfg.autocomplete) {
+                autocomplete = ' autocomplete="' + stringUtils.escapeHTML(cfg.autocomplete) + '"';
+            }
+            var name = "";
+            if (cfg.name) {
+                name = ' name="' + stringUtils.escapeHTML(cfg.name) + '"';
+            }
 
             if (this._isTextarea) {
                 out.write(['<textarea', Aria.testMode ? ' id="' + this._domId + '_textarea"' : '',
@@ -313,6 +321,7 @@ Aria.classDefinition({
                         ';overflow:auto;resize:none;height: ' + this._frame.innerHeight + 'px; width:', inputWidth,
                         'px;"', 'value=""', (cfg.maxlength > -1 ? 'maxlength="' + cfg.maxlength + '" ' : ' '),
                         (cfg.tabIndex != null ? 'tabindex="' + this._calculateTabIndex() + '" ' : ' '), spellCheck,
+                        autocomplete, name,
                         '>', stringUtils.encodeForQuotedHTMLAttribute((this._helpTextSet) ? cfg.helptext : text),
                         '</textarea>'
 
@@ -324,7 +333,7 @@ Aria.classDefinition({
                         'value="', stringUtils.encodeForQuotedHTMLAttribute((this._helpTextSet) ? cfg.helptext : text),
                         '" ', (cfg.maxlength > -1 ? 'maxlength="' + cfg.maxlength + '" ' : ' '),
                         (cfg.tabIndex != null ? 'tabindex="' + this._calculateTabIndex() + '" ' : ' '), spellCheck,
-                        ' _ariaInput="1"/>'
+                        autocomplete, name, ' _ariaInput="1"/>'
                 // the _ariaInput attribute is present so that pressing
                 // ENTER on this widget raises the onSubmit event of
                 // the fieldset:
