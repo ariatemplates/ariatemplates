e18610e841bbef008cd05195b0f2bf4bf6f7ba1d
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2020-04-28 16:58:48 +0200

    fix/test #1826 test.aria.map.MapManagerTwoTestCase
    This commit splits the MapManagerTwoTestCase in two and increases the timeouts
    in the part that uses Microsoft Maps to avoid receiving "Script error".

--- test/aria/map/MapManagerTwoTestCase.js
+++ test/aria/map/MapManagerTwoTestCase.js
@@ -70,7 +70,7 @@ Aria.classDefinition({
             var that = this;
             setTimeout(function() {
                 that._createCallbackOne(map, args);
-            }, 250);
+            }, 1000);
         },
 
         _createCallbackOne : function (map, args) {
@@ -118,104 +118,7 @@ Aria.classDefinition({
                 that.assertTrue(that.mapMgr.getMap("anotherMs7MapId") === null);
 
                 that.notifyTestEnd("testAsyncValidCreateMap");
-            }, 250);
-        },
-        testAsyncValidCreateMapTwo : function () {
-            var that = this;
-            this.mapMgr.addProvider("anotherProvider", {
-                load : function (cb) {
-                    that.$callback(cb);
-                },
-                getMap : function () {
-                    return {
-                        mapText : "message"
-                    };
-                },
-                disposeMap : function () {}
-            });
-            var validCfg = {
-                id : "anotherMap",
-                provider : "anotherProvider",
-                domElement : this._testDomElement,
-                afterCreate : {
-                    fn : this._timeout2,
-                    scope : this,
-                    args : {
-                        testArgs : "test"
-                    }
-                }
-            };
-
-            this.mapMgr.createMap(validCfg);
-
-        },
-
-        _timeout2 : function(map, args) {
-            var that = this;
-            setTimeout(function() {
-                that._createCallbackOneTwo(map, args);
-            }, 250);
-        },
-
-        _createCallbackOneTwo : function (map, args) {
-            this.assertTrue(map !== null, "The map was not created.");
-            this.assertTrue(args.testArgs === "test");
-            this.assertTrue(this.mapMgr.getMapStatus("anotherMap") == this.mapMgr.READY);
-
-            this.assertTrue(this.mapMgr.getMap("anotherMap") == map);
-            this.assertTrue(map.mapText == "message");
-            this._testDestroyMapTwo();
-        },
-
-        _testDestroyMapTwo : function () {
-            this.mapMgr.destroyMap("anotherMap");
-            this.assertTrue(this.mapMgr.getMap("anotherMap") === null);
-            this.assertTrue(this.mapMgr.getMapStatus("anotherMap") === null);
-            this.assertTrue(this.mapMgr.getMapDom("anotherMap") == null);
-            this.mapMgr.destroyMap("anotherMap");
-
-            this.assertTrue(this.mapMgr.hasProvider("anotherProvider"));
-            this.mapMgr.removeProvider("anotherProvider");
-            this.assertFalse(this.mapMgr.hasProvider("anotherProvider"));
-
-            this.notifyTestEnd("testAsyncValidCreateMapTwo");
-        },
-        testAsyncValidCreateMapThree : function () {
-            var that = this;
-
-            var validCfg = {
-                id : "anotherAnotherMap",
-                provider : "anotherAnotherProvider",
-                domElement : this._testDomElement,
-                afterCreate : {
-                    fn : this._timeout3,
-                    scope : this,
-                    args : {
-                        testArgs : "test"
-                    }
-                }
-            };
-
-            this.mapMgr.createMap(validCfg);
-
-        },
-
-        _timeout3 : function(map, args) {
-            var that = this;
-            setTimeout(function() {
-                that._createCallbackOneThree(map, args);
-            }, 250);
-        },
-
-        _createCallbackOneThree : function (map, args) {
-            this.assertTrue(map !== null, "The map was not created.");
-            this.assertTrue(args.testArgs === "test");
-            this.assertTrue(this.mapMgr.getMapStatus("anotherAnotherMap") == this.mapMgr.READY);
-
-            this.assertTrue(this.mapMgr.getMap("anotherAnotherMap") == map);
-
-            this.notifyTestEnd("testAsyncValidCreateMapThree");
+            }, 1000);
         }
-
     }
 });
