2a1432ba9b1f3ea979295fd0a80518095d539db6
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2021-09-16 11:49:53 +0200

    fix/test: no longer click outside the viewport

--- test/aria/widgets/form/autocomplete/helptext/test1/AutoCompleteHelptextRobotTestCase.js
+++ test/aria/widgets/form/autocomplete/helptext/test1/AutoCompleteHelptextRobotTestCase.js
@@ -33,52 +33,19 @@ Aria.classDefinition({
 
         runTemplateTest : function () {
             var field = this.getInputField("ac");
-            // This test is intentionally causing a click outside of the viewport in order to reproduce the issue that
-            // triggered the code change
-            var position = this._getPositionOutOfViewport();
             this.templateCtxt.$focus("ac");
-            this.synEvent.execute([["click", position]], {
+            this.synEvent.execute([
+                ["waitFocus", field],
+                ["click", {x: 1, y: 1}],
+                ["pause", 1000]
+            ], {
                 fn : this._afterBlur,
                 scope : this
             });
         },
         _afterBlur : function () {
             this.assertFalse(this.dataModel.getSuggestionsCalled, "The getSuggestions method of the resource handler has been called with the helptext as argument");
-
-            this.synEvent.execute([["click", {
-                        x : 1,
-                        y : 1
-                    }]], {
-                fn : this._afterClickInViewport,
-                scope : this
-            });
-        },
-
-        _afterClickInViewport : function () {
-            aria.core.Timer.addCallback({
-                fn : this._finalizeTest,
-                scope : this,
-                delay : 1000
-            });
-        },
-
-        _finalizeTest : function () {
             this.end();
-        },
-
-        _getPositionOutOfViewport : function () {
-            if (aria.core.Browser.isFirefox) {
-                var viewport = aria.utils.Dom.getViewportSize();
-                return {
-                    x : 0,
-                    y : viewport.height + 1
-                };
-            }
-            return {
-                x : 0,
-                y : -1
-            };
         }
-
     }
 });
